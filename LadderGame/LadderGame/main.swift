2e82f712b570f8b6d6421fa290e8c41db279e246
ChocOZerO <nayalth@gmail.com>
2017-10-20 14:31:40 +0900

    Init Ladder Game
    Can get people count
    Can get ladder count
    
    print ladder's main line for poeple count

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,16 @@
 
 import Foundation
 
+print("참여할 사람은 몇 명 인가요?")
+let peopleCount : Int = Int(readLine() ?? "0") ?? 0
+print("최대 사다리의 높이는 몇 개인가요?")
+let ladderCount : Int = Int(readLine() ?? "0") ?? 0
+
+var result : String = ""
+for _ in 0..<ladderCount {
+    for _ in 0..<peopleCount {
+        result += "| "
+    }
+    result += "\n"
+}
+print(result)
