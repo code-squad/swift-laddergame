c76473f473bcb5ca261dce7902e469ee299a537a
ChocOZerO <nayalth@gmail.com>
2017-10-23 10:36:53 +0900

    Update data type from String to Array

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -13,18 +13,31 @@ let peopleCount : Int = Int(readLine() ?? "0") ?? 0
 print("최대 사다리의 높이는 몇 개인가요?")
 let ladderCount : Int = Int(readLine() ?? "0") ?? 0
 
-var result : String = ""
-for _ in 0..<ladderCount {
+var result : Array<Array<String>> = Array<Array<String>>()
+for i in 0..<ladderCount {
+    result.append(Array<String>())
     for _ in 1..<peopleCount {
-        result += "|"
+        result[i].append("|")
         let ladderLine : Int = Int(arc4random_uniform(2))
         if ladderLine == 0 {
-            result += "-"
+            result[i].append("-")
         } else {
-            result += " "
+            result[i].append(" ")
         }
     }
-    result += "|\n"
+    result[i].append("|")
 }
-print(result)
+
+func printResult(result: Array<Array<String>>) {
+    var tmp : String = ""
+    for i in 0..<result.count {
+        for j in 0..<result[i].count {
+            tmp += result[i][j]
+        }
+        tmp += "\n"
+    }
+    print(tmp)
+}
+
+printResult(result: result)
 
