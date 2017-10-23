e998d17be76ef2affc2c337d8511faa9865e8c6f
ChocOZerO <nayalth@gmail.com>
2017-10-23 20:20:18 +0900

    [Refactoring]구조체를 활용하여 사다리게임 구현
    기존에 함수로만 되어있던 로직개선
    LadderPlayer와 LadderGame을 활용해 로직구현
    InputView와 ResultView를 활용해 입출력 구분

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,76 +8,105 @@
 
 import Foundation
 
-// 사다리 구성 요소
-enum LadderPrint : Character {
-    case bar = "|"
-    case step = "-"
-    case empty = " "
+struct LadderPlayer {
+    let name : String
 }
 
-// 사다리 구현을 위한 이중 배열
-var result : Array<Array<Character>> = Array<Array<Character>>()
+struct LadderGame {
+    
+    var height : Int = 0
+    var names : [LadderPlayer]
+    var ladder : Array<Array<String>>
+    
+    // 사다리 구성 요소
+    enum LadderPrint : String {
+        case bar = "|"
+        case step = "-----"
+        case empty = "     "
+    }
+    
+    // 사다리 틀 만들기
+    mutating func makeLadderLayer(ladderHeight: Int, playerNames: [String.SubSequence]) {
+        for i in 0..<ladderHeight {
+            self.ladder.append(Array<String>())
+            setLadderLine(lineNum: i, playerCount: playerNames.count)
+            self.ladder[i].append(LadderPrint.bar.rawValue)
+        }
+        for i in 0..<playerNames.count {
+            self.names.append(LadderPlayer.init(name: String(playerNames[i])))
+        }
+    }
+    // 사다리 각 라인 만들기
+    mutating func setLadderLine(lineNum: Int, playerCount: Int) {
+        for _ in 1..<playerCount {
+            self.ladder[lineNum].append(LadderPrint.bar.rawValue)
+            setStep(lineNum: lineNum)
+        }
+    }
+    // 발판 유무
+    mutating func setStep(lineNum: Int) {
+        guard Int(arc4random_uniform(2)) > 0 else {
+            self.ladder[lineNum].append(LadderPrint.empty.rawValue)
+            return
+        }
+        self.ladder[lineNum].append(LadderPrint.step.rawValue)
+    }
+}
+
+struct InputView {
+    
+    // 참여인원 입력
+    static func getPlayerCount() -> String {
+        print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)")
+        return readLine() ?? ""
+    }
+    // 최대 사다리 높이 입력
+    static func getLadderHeight() -> Int {
+        print("최대 사다리의 높이는 몇 개인가요?")
+        return Int(readLine() ?? "0") ?? 0
+    }
+    
+}
+
+struct ResultView {
+    
+    // 사다리 출력
+    static func printResult(result: Array<Array<String>>) {
+        for i in 0..<result.count {
+            printLadderLineResult(lineResult: result[i])
+        }
+    }
+    // 사다리 각 라인출력
+    static func printLadderLineResult(lineResult: Array<String>) {
+        let playerCount : Int = lineResult.count
+        for j in 0..<(playerCount-1) {
+            print(lineResult[j], terminator: "")
+        }
+        print(lineResult[playerCount-1])
+    }
+    
+    // player 출력
+    static func printPlayers(players: Array<LadderPlayer>) {
+        for player in players {
+            print(player.name, terminator:" ")
+        }
+    }
+}
 
 // 사다리 게임 세팅
-func setLadderGame() {
-    var playerCount : Int = 0
+func setLadderGame() -> LadderGame {
+    var playerNames : [String.SubSequence]
     var ladderHeight : Int = 0
     repeat {
-        playerCount = getPlayerCount()
-        ladderHeight = getLadderHeight()
-    } while(!(playerCount > 0) || !(ladderHeight > 0))
-    makeLadderLayer(ladderHeight: ladderHeight, playerCount: playerCount)
-}
-// 참여인원 입력
-func getPlayerCount() -> Int {
-    print("참여할 사람은 몇 명 인가요?")
-    return Int(readLine() ?? "0") ?? 0
-}
-// 최대 사다리 높이 입력
-func getLadderHeight() -> Int {
-    print("최대 사다리의 높이는 몇 개인가요?")
-    return Int(readLine() ?? "0") ?? 0
+        playerNames = InputView.getPlayerCount().split(separator: ",")
+        ladderHeight = InputView.getLadderHeight()
+    } while(!(playerNames.count > 0) || !(ladderHeight > 0))
+    var ladderGame : LadderGame = LadderGame(height: ladderHeight, names: [], ladder: [])
+    ladderGame.makeLadderLayer(ladderHeight: ladderHeight, playerNames: playerNames)
+    return ladderGame
 }
 
-// 사다리 틀 만들기
-func makeLadderLayer(ladderHeight: Int, playerCount: Int) {
-    for i in 0..<ladderHeight {
-        result.append(Array<Character>())
-        setLadderLine(lineNum: i, playerCount: playerCount)
-        result[i].append(LadderPrint.bar.rawValue)
-    }
-}
-// 사다리 각 라인 만들기
-func setLadderLine(lineNum: Int, playerCount: Int) {
-    for _ in 1..<playerCount {
-        result[lineNum].append(LadderPrint.bar.rawValue)
-        setStep(lineNum: lineNum)
-    }
-}
-// 발판 유무
-func setStep(lineNum: Int) {
-    guard Int(arc4random_uniform(2)) > 0 else {
-        result[lineNum].append(LadderPrint.empty.rawValue)
-        return
-    }
-    result[lineNum].append(LadderPrint.step.rawValue)
-}
-
-// 사다리 출력
-func printResult(result: Array<Array<Character>>) {
-    for i in 0..<result.count {
-        printLadderLineResult(lineResult: result[i])
-    }
-}
-// 사다리 각 라인출력
-func printLadderLineResult(lineResult: Array<Character>) {
-    let playerCount : Int = lineResult.count
-    for j in 0..<(playerCount-1) {
-        print(lineResult[j], terminator: "")
-    }
-    print(lineResult[playerCount-1])
-}
-
-setLadderGame()
-printResult(result: result)
+let ladderGame = setLadderGame()
+ResultView.printResult(result: ladderGame.ladder)
+ResultView.printPlayers(players: ladderGame.names)
 
