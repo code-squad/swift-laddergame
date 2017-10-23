d1bfcc39f4ee2abcbe0f29ff983ee1c2474c1fb8
ChocOZerO <nayalth@gmail.com>
2017-10-23 11:32:52 +0900

    Refactoring the Ladder to using enum character

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -13,30 +13,39 @@ let peopleCount : Int = Int(readLine() ?? "0") ?? 0
 print("최대 사다리의 높이는 몇 개인가요?")
 let ladderCount : Int = Int(readLine() ?? "0") ?? 0
 
-var result : Array<Array<String>> = Array<Array<String>>()
-for i in 0..<ladderCount {
-    result.append(Array<String>())
-    for _ in 1..<peopleCount {
-        result[i].append("|")
-        let ladderLine : Int = Int(arc4random_uniform(2))
-        if ladderLine == 0 {
-            result[i].append("-")
-        } else {
-            result[i].append(" ")
-        }
-    }
-    result[i].append("|")
+enum LadderPrint : Character {
+    case bar = "|"
+    case step = "-"
+    case empty = " "
 }
 
-func printResult(result: Array<Array<String>>) {
-    var tmp : String = ""
-    for i in 0..<result.count {
-        for j in 0..<result[i].count {
-            tmp += result[i][j]
+var result : Array<Array<Character>> = Array<Array<Character>>()
+for i in 0..<ladderCount {
+    result.append(Array<Character>())
+    for _ in 1..<peopleCount {
+        result[i].append(LadderPrint.bar.rawValue)
+        let ladderLine : Int = Int(arc4random_uniform(2))
+        switch ladderLine {
+        case 0:
+            result[i].append(LadderPrint.step.rawValue)
+        default:
+            result[i].append(LadderPrint.empty.rawValue)
         }
-        tmp += "\n"
     }
-    print(tmp)
+    result[i].append(LadderPrint.bar.rawValue)
+}
+
+func printResult(result: Array<Array<Character>>) {
+    for i in 0..<result.count {
+        let peopleCount : Int = result[i].count
+        for j in 0..<peopleCount {
+            if j == peopleCount - 1 {
+                print(result[i][j])
+            } else {
+                print(result[i][j], terminator: "")
+            }
+        }
+    }
 }
 
 printResult(result: result)
