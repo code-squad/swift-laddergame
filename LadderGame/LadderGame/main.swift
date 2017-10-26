8fe0cc3f0ee3806345de156d353caeed23503976
Elly <lhyal2p@gmail.com>
2017-10-26 15:32:10 +0900

    ladder-step1

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 
+print("참여할 사람은 몇 명 인가요?")
+var personNum = readLine()
+
+print("최대 사다리 높이는 몇 개인가요?")
+var ladderHeight = readLine()
+
+for _ in 0..<Int(ladderHeight!)! {
+    for j in 0..<Int(personNum!)! {
+        print("|", terminator: "")
+
+        let rndNum = arc4random_uniform(2)
+        if rndNum == 0 && j < 2 {
+            print(" ", terminator: "")
+        }else if rndNum == 1 && j < 2 {
+            print("-", terminator: "")
+        }
+    }
+    print()
+}
+
