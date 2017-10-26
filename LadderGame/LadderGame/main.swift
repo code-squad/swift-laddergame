33601ed6cc4f3068340d9cabb4cb039fcbbf9206
Jung Kim <godrm77@gmail.com>
2017-10-26 14:41:27 +0900

    Revert "Edit global Val, add input guard method"
    This reverts commit a7f37c3020c9f8fdca63f0e1be6b4c6d46c21ec9.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,43 +7,53 @@
 //
 import Foundation
 
-func makeLadder(col: String?, row: String?){
-    guard let col = Int(col!), let row = Int(row!) else {
-        print("입력오류")
-        return
-    }
-    drawLadder(col, row)
+var innerArr: [String] = [""]
+var outerArr: [[String]] = [[""]]
+var countNum: Int = 0
+var flag: Bool = true
+
+func makeBaseLadder(peopleNum: Int, ladderNum: Int){
+    countNum = (peopleNum - 1) + peopleNum
+    innerArr = [String](repeating: "|", count: countNum)
+    outerArr = [[String]](repeating: innerArr, count: ladderNum)
 }
 
-func drawLadder(_ peopleNum: Int, _ ladderNum: Int){
-    let countNum = (peopleNum - 1) + peopleNum
-    let innerArr = [String](repeating: "|", count: countNum)
-    var outerArr = [[String]](repeating: innerArr, count: ladderNum)
-    var flag: Bool = true
-    for i in 0..<ladderNum{
-        //true 면 사다리 존재
-        for j in 0..<countNum{
-            if j % 2 == 0{
-                print(outerArr[i][j], terminator: "")
-            } else {
-                let random: Bool = arc4random_uniform(2) % 2 == 0 ? true : false
-                if random && flag {
-                    outerArr[i][j] = "-"
-                    print(outerArr[i][j], terminator: "")
-                    flag = false
-                } else {
-                    outerArr[i][j] = " "
-                    print(outerArr[i][j], terminator: "")
-                    flag = true
-                }
-            }
+func makeRandomLine(row i: Int, col j: Int){
+    let random: Bool = arc4random_uniform(2) % 2 == 0 ? true : false
+    if random && flag {
+        outerArr[i][j] = "-"
+        print(outerArr[i][j], terminator: "")
+        flag = false
+    } else {
+        outerArr[i][j] = " "
+        print(outerArr[i][j], terminator: "")
+        flag = true
+    }
+}
+
+func selectLine(row i: Int) -> Bool{
+    //true 면 사다리 존재
+    for j in 0..<countNum{
+        if j % 2 == 0{
+            print(outerArr[i][j], terminator: "")
+        } else {
+            makeRandomLine(row: i, col: j)
         }
+    }
+    return flag
+}
+
+func drawLadder(peopleNum col: Int, ladderNum row: Int){
+    makeBaseLadder(peopleNum: col, ladderNum: row)
+    for i in 0..<row{
+        flag = selectLine(row: i)
         print()
     }
 }
 
 print("참여할 사람은 몇 명인가요?")
-let peopleNum = readLine()
+var peopleNum = readLine()
 print("최대 사다리 높이는 몇 개인가요?")
-let ladderNum = readLine()
-makeLadder(col: peopleNum, row: ladderNum)
+var ladderNum = readLine()
+drawLadder(peopleNum: Int(peopleNum!)!, ladderNum: Int(ladderNum!)!)
+
