348928cb538f33b2fa27fc4967d09f834537f004
conalls <comets_195@naver.com>
2017-10-26 14:53:08 +0900

    사다리게임 step1

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -6,46 +6,63 @@
 //  Copyright © 2017 Codesquad Inc. All rights reserved.
 //
 import Foundation
+let horizLine: String = "-"
+let vertiLine: String = "|"
+let spaceLine: String = " "
 
 struct Ladder {
     var innerArr: [String] = [""]
     var outerArr: [[String]] = [[""]]
     var realColNum: Int = 0
     var realRowNum: Int = 0
+    var flag: Bool = true
     
     mutating func makeLadder(_ col: Int, _ row: Int){
         realColNum = (col - 1) + col
         realRowNum = row
-        innerArr = [String](repeating: "|", count: realColNum)
+        innerArr = [String](repeating: vertiLine, count: realColNum)
         outerArr = [[String]](repeating: innerArr, count: realRowNum)
     }
     
     mutating func makeRandomLine(){
-        var flag: Bool = true
         for seqOfOutArr in 0..<realRowNum {
-            for seqOfInArr in 0..<realColNum {
-                let randomVal: Bool = arc4random_uniform(2) % 2 == 0 ? true : false
-                if seqOfInArr % 2 != 0 { (flag && randomVal) ? (flag = makeHrozLine(seqOfOutArr, seqOfInArr)) : (flag = makeSpaceLine(seqOfOutArr, seqOfInArr)) }
-            }
+            makeRandomLineInner(seqOfOutArr)
         }
     }
     
+    mutating func makeRandomLineInner(_ seqOfOutArr: Int){
+        for seqOfInArr in 0..<realColNum {
+            let randomVal: Bool = arc4random_uniform(2) % 2 == 0 ? true : false
+            chooseToMakeLine(seqOfOutArr ,seqOfInArr, randomVal)
+        }
+    }
+    
+    mutating func chooseToMakeLine(_ seqOfOutArr: Int ,_ seqOfInArr: Int, _ randomVal: Bool){
+        if seqOfInArr % 2 != 0 { (flag && randomVal) ?
+            (flag = makeHrozLine(seqOfOutArr, seqOfInArr)) :
+            (flag = makeSpaceLine(seqOfOutArr, seqOfInArr)) }
+    }
+    
     mutating func makeHrozLine(_ seqOfOutArr: Int, _ seqOfInArr: Int) -> Bool{
-        outerArr[seqOfOutArr][seqOfInArr] = "-"
+        outerArr[seqOfOutArr][seqOfInArr] = horizLine
         return false
     }
-
+    
     mutating func makeSpaceLine(_ seqOfOutArr: Int, _ seqOfInArr: Int) -> Bool{
-        outerArr[seqOfOutArr][seqOfInArr] = " "
+        outerArr[seqOfOutArr][seqOfInArr] = spaceLine
         return true
     }
     
     func printLadder(){
         for seqOfOutArr in 0..<realRowNum {
-            for seqOfInArr in 0..<realColNum{
-                print("\(outerArr[seqOfOutArr][seqOfInArr])", terminator: "")
-            }
+            printLadderInner(seqOfOutArr)
             print()
+        }
+    }
+    
+    func printLadderInner(_ seqOfOutArr: Int){
+        for seqOfInArr in 0..<realColNum {
+            print("\(outerArr[seqOfOutArr][seqOfInArr])", terminator: "")
         }
     }
 }
@@ -66,3 +83,4 @@ let peopleNum = readLine()
 print("최대 사다리 높이는 몇 개인가요?")
 let ladderNum = readLine()
 inputExecute(peopleNum, ladderNum)
+
