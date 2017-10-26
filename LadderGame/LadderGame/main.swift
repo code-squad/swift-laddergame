1496538b0eda12de8094512c1feba49b3765c62b
Elly <lhyal2p@gmail.com>
2017-10-26 18:11:06 +0900

    create ladder-step2

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,22 +8,48 @@
 
 import Foundation
 
-print("참여할 사람은 몇 명 인가요?")
-var playerNum = readLine()
-
-print("최대 사다리 높이는 몇 개인가요?")
-var ladderHeight = readLine()
-
-for _ in 0..<Int(ladderHeight!)! {
-    for cnt in 0..<Int(playerNum!)! {
-        print("|", terminator: "")
-        let needStep = arc4random_uniform(2)
-        if needStep == 0 && cnt < 2 {
-            print(" ", terminator: "")
-        }else if needStep == 1 && cnt < 2 {
-            print("-", terminator: "")
-        }
-    }
-    print()
+func prompt(){
+    print("참여할 사람은 몇 명 인가요?")
+    let numberOfPlayer = readLine()
+    
+    print("최대 사다리 높이는 몇 개인가요?")
+    let heightForLadder = readLine()
+    
+    printLadder(numberOfPlayer: numberOfPlayer!, heightForLadder: heightForLadder!)
 }
+
+func printLadder(numberOfPlayer: String, heightForLadder: String){
+    for _ in 0..<Int(heightForLadder)! {
+        printLadderBar(numberOfPlayer: numberOfPlayer)
+        print()
+    }
+}
+
+func printLadderBar(numberOfPlayer: String){
+    for index in 0..<Int(numberOfPlayer)! {
+        print("|", terminator: "")
+        printLadderStep(index: index)
+    }
+}
+
+func printLadderStep(index: Int){
+    let isPrintStep = makeRandomValue()
+    if isPrintStep && index > 0 {
+        print(" ", terminator: "")
+    }else if !isPrintStep && index > 0 {
+        print("-", terminator: "")
+    }
+}
+
+func makeRandomValue() -> Bool {
+    let randomValue = arc4random_uniform(2)
+    var isPrintStep: Bool = false
+    if randomValue == 0 {
+        isPrintStep = false
+    }else{
+        isPrintStep = true
+    }
+    return isPrintStep
+}
+prompt()
 
