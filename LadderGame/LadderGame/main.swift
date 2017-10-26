0cfd76bce4d56d827647ccf51447f1807b90c9ae
Elly <lhyal2p@gmail.com>
2017-10-26 16:22:15 +0900

    change variable name

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -9,19 +9,18 @@
 import Foundation
 
 print("참여할 사람은 몇 명 인가요?")
-var personNum = readLine()
+var playerNum = readLine()
 
 print("최대 사다리 높이는 몇 개인가요?")
 var ladderHeight = readLine()
 
 for _ in 0..<Int(ladderHeight!)! {
-    for j in 0..<Int(personNum!)! {
+    for cnt in 0..<Int(playerNum!)! {
         print("|", terminator: "")
-
-        let rndNum = arc4random_uniform(2)
-        if rndNum == 0 && j < 2 {
+        let isStep = arc4random_uniform(2)
+        if isStep == 0 && cnt < 2 {
             print(" ", terminator: "")
-        }else if rndNum == 1 && j < 2 {
+        }else if isStep == 1 && cnt < 2 {
             print("-", terminator: "")
         }
     }
