a139b01daaf7e01464ed2e957f7e5ec0daa96949
yuaming <miennes3@gmail.com>
2017-10-26 14:45:39 +0900

    사다리게임 1단계 구현 완료

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,36 @@
 
 import Foundation
 
+func makeLadderBar() -> String {
+    let isMake: Bool = arc4random_uniform(2) == 0 ? false : true
+    
+    if isMake {
+        return "-"
+    }
+    
+    return " "
+}
+
+func drawLadder(colNum col: Int, rowNum row: Int) {
+    for _ in 0..<col {
+        for _ in 0..<row-1 {
+            print("ㅣ", terminator: makeLadderBar())
+        }
+        
+        print("ㅣ", terminator: "\n")
+    }
+}
+
+while (true) {
+    print("참여할 사람은 몇 명 인가요?", terminator: " ")
+    let inputUserCount: String = readLine()!
+    
+    if inputUserCount.isEmpty { break }
+    
+    print("최대 사다리 높이는 몇 개인가요?", terminator: " ")
+    let inputLadderHeight: String = readLine()!
+    
+    if inputLadderHeight.isEmpty { break }
+    
+    drawLadder(colNum: Int(inputLadderHeight) ?? 0, rowNum: Int(inputUserCount) ?? 0)
+}
