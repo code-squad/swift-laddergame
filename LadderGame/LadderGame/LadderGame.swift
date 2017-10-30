311cf5e3555f4c0e039c6cd295aa2d43b6fe485f
DongJin Lee <comets195@gmail.com>
2017-10-30 14:41:47 +0900

    변수이름 변경 및 readLine()호출 함수 추가

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -14,34 +14,34 @@ struct LadderGame {
     let spaceLine: String = " "
     var ladderComponents: [String] = [""]
     var ladderFrames: [[String]] = [[""]]
-    var ladderColNumber: Int = 0
-    var ladderRowNumber: Int = 0
-    var ladderHorizflag: Bool = true
+    var columnNumber: Int = 0
+    var rowNumber: Int = 0
+    var horizontalFlag: Bool = true
     
     mutating func makeLadder(_ col: Int, _ row: Int){
-        ladderColNumber = (col - 1) + col
-        ladderRowNumber = row
-        ladderComponents = [String](repeating: vertiLine, count: ladderColNumber)
-        ladderFrames = [[String]](repeating: ladderComponents, count: ladderRowNumber)
+        columnNumber = (col - 1) + col
+        rowNumber = row
+        ladderComponents = [String](repeating: vertiLine, count: columnNumber)
+        ladderFrames = [[String]](repeating: ladderComponents, count: rowNumber)
     }
     
     mutating func makeRandomLine(){
-        for seqOfOutArr in 0..<ladderRowNumber {
+        for seqOfOutArr in 0..<rowNumber {
             makeRandomInLine(seqOfOutArr)
         }
     }
     
     private mutating func makeRandomInLine(_ seqOfOutArr: Int){
-        for seqOfInArr in 0..<ladderColNumber {
+        for seqOfInArr in 0..<columnNumber {
             let randomVal: Bool = arc4random_uniform(2) % 2 == 0 ? true : false
             chooseToMakeLine(seqOfOutArr ,seqOfInArr, randomVal)
         }
     }
     
     private mutating func chooseToMakeLine(_ seqOfOutArr: Int ,_ seqOfInArr: Int, _ randomVal: Bool){
-        if seqOfInArr % 2 != 0 { (ladderHorizflag && randomVal) ?
-            (ladderHorizflag = makeHrozLine(seqOfOutArr, seqOfInArr)) :
-            (ladderHorizflag = makeSpaceLine(seqOfOutArr, seqOfInArr)) }
+        if seqOfInArr % 2 != 0 { (horizontalFlag && randomVal) ?
+            (horizontalFlag = makeHrozLine(seqOfOutArr, seqOfInArr)) :
+            (horizontalFlag = makeSpaceLine(seqOfOutArr, seqOfInArr)) }
     }
     
     private mutating func makeHrozLine(_ seqOfOutArr: Int, _ seqOfInArr: Int) -> Bool{
@@ -55,14 +55,14 @@ struct LadderGame {
     }
     
     func printLadder(){
-        for seqOfOutArr in 0..<ladderRowNumber {
+        for seqOfOutArr in 0..<rowNumber {
             printLadderInner(seqOfOutArr)
             print()
         }
     }
     
     private func printLadderInner(_ seqOfOutArr: Int){
-        for seqOfInArr in 0..<ladderColNumber {
+        for seqOfInArr in 0..<columnNumber {
             print("\(ladderFrames[seqOfOutArr][seqOfInArr])", terminator: "")
         }
     }
--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,6 +7,14 @@
 //
 import Foundation
 
+func executeLadderGame() -> (peopleNumber: String?, ladderNumber: String?){
+    print("참여할 사람은 몇 명인가요?")
+    let peopleNumber = readLine()
+    print("최대 사다리 높이는 몇 개인가요?")
+    let ladderNumber = readLine()
+    return (peopleNumber, ladderNumber)
+}
+
 func executeLadderGame(people col: String?, ladder row: String?){
     guard let col = Int(col!), let row = Int(row!) else {
         print("입력오류")
@@ -18,9 +26,5 @@ func executeLadderGame(people col: String?, ladder row: String?){
     ladder.printLadder()
 }
 
-print("참여할 사람은 몇 명인가요?")
-let peopleNumber = readLine()
-print("최대 사다리 높이는 몇 개인가요?")
-let ladderNumber = readLine()
-executeLadderGame(people: peopleNumber, ladder: ladderNumber)
-
+let ladderGameInfo = executeLadderGame()
+executeLadderGame(people:ladderGameInfo.peopleNumber , ladder: ladderGameInfo.ladderNumber)
