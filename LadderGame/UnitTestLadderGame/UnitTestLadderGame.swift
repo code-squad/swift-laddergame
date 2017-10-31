2d09963fa9e3ce746897185e9c120a1f6cb5ee8d
DongJin Lee <comets195@gmail.com>
2017-10-31 18:03:37 +0900

    Add TestUnit

---
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -0,0 +1,27 @@
+//
+//  UnitTestLadderGame.swift
+//  UnitTestLadderGame
+//
+//  Created by Mrlee on 2017. 10. 31..
+//  Copyright © 2017년 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestLadderGame: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
