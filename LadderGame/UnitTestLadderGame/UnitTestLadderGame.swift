1be8661db173c9be7b509c2d9c0c5744709cd793
yuaming <miennes3@gmail.com>
2017-11-02 14:37:49 +0900

    [수정] 사다리 게임 테스트 내용 추가

--- LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -11,7 +11,7 @@ import XCTest
 @testable import LadderGame
 
 class UnitTestLadderGame: XCTestCase {
-    var userNames: String  = "hj,sj,gamja,woong"
+    var userNames: String = "hj,sj,gamja,woong"
     var ladderHeight: String = "5"
     var gameResult: LadderGameInformation?
     
@@ -41,7 +41,20 @@ class UnitTestLadderGame: XCTestCase {
         XCTAssertNotEqual(0, gameResult!.result.row)
     }
     
-    func testGameResultLadderIsNotValue() {
-        XCTAssertFalse(gameResult!.result.ladder == "")
+    func testGameResultLadderIsNotNil() {
+        XCTAssertTrue(gameResult!.result.ladder != "", "사다리은 빈 값이 아닙니다.")
+    }
+    
+    func testLadderPlayerExceedLength() {
+        XCTAssertFalse(LadderPlayer(name: "yuaming").isExceedLength, "유저 이름이 5자리 넘습니다.")
+    }
+    
+    func testLadderIsNotNil() {
+        let ladder: Ladder = Ladder(column: userNames.split(separator: ",").count, row: Int(ladderHeight) ?? 0).makeLadder()
+        XCTAssertTrue(ladder.ladder != "", "Ladder객체의 ladder는 빈 값이 아닙니다.")
+    }
+    
+    func testLadderGameCheckGettingPlayerNames() {
+        XCTAssertTrue(gameResult!.playerNames.count > 0, "플레이어 이름을 가져옵니다.")
     }
 }
