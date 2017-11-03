01055ab9bfd995f907e74b250e74ad273e08db9e
waitNseE <yellow.cjh@gmail.com>
2017-11-03 16:12:17 +0900

    use twoDimentional Array

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,37 +8,52 @@
 
 import Foundation
 
-func makeARow() -> String {
-    // 랜덤으로 가로줄 생성
-    let isExistent: Bool = arc4random_uniform(2) == 0 ? true : false
-    guard  isExistent == true else{ return  " " }
-    return "-"
+
+// 랜덤 불값 생성
+func makeRandomBooleanValue () -> Bool {
+    let randomNum = Int(arc4random_uniform(10))
+    guard randomNum % 2 == 0 else { return false }
+    return true
 }
 
-func printLadder (_ numberOfParticipant: Int, _ heightOfLadder: Int) {
-    var (numberOfParticipant, heightOfLadder) = (numberOfParticipant, heightOfLadder)
-    //전체줄 수 생성루프 (사다리 갯수만큼 생성)
-    while 0 < heightOfLadder {
-        // 안쪽 가로선 및 가로선 우측선 랜덤생성 <첫째줄은 이미 생성되기 떄문에 사람 수 -1)
-        for _ in 0..<numberOfParticipant-1 {
-            print("ㅣ", terminator: makeARow())
+// <랜덤 불값의 한줄 배열 만들기>
+func makeRandomInnerArray (numberOfPeople col: Int) -> Array<Bool> {
+    var firstArray = Array<Bool>()
+    for _ in 0 ..< col-1 {
+        firstArray.append(makeRandomBooleanValue())
+    }
+    return firstArray
+}
+
+// <랜덤 불값의 전체 배열 만들기>
+func makeTwoDimentionofBool (numberOf col : Int, countOf row: Int) -> Array<Array<Bool>> {
+    var result = Array<Array<Bool>>()
+    for _ in 0 ..< row {
+        result.append(makeRandomInnerArray(numberOfPeople: col))
+    }
+    return result
+}
+
+// 프린트 함수
+func printLadder (_ input: Array<Array<Bool>>) {
+    for outer in 0 ..< input.count {
+        for inner in 0 ..< input[outer].count {
+            if input[outer][inner] == true {
+                print("|", terminator: "-")
+            } else {
+                print("|", terminator: " ")
+            }
         }
-        // 외곽선(전체 줄) 생성
-        print("ㅣ")
-        heightOfLadder -= 1
+        print ("|")
     }
 }
 
+// 프로그램 루프
 ladderLoop : while (true) {
     print("참여할 사람은 몇 명 인가요?")
-    // 사람수 입력값 바인딩 / 입력값 없거나 Int타입이 아닐경우 종료
-    guard let numberOfPeople = readLine(),
-                 let numberOfParticipant = Int(numberOfPeople) else {break}
+    guard let numberOfPeople = readLine(), let participant = Int(numberOfPeople) else {break}
     print("최대 사다리 높이는 몇 개인가요?")
-    // 사다리
-    // 입력값 바인딩 / 입력값 없거나 Int타입이 아닐경우 종료
-    guard let countOfLadder = readLine(),
-                 let numberOfLadder = Int(countOfLadder) else {break}
-    // 사다리 출력함수 호출
-    printLadder(numberOfParticipant, numberOfLadder)
+    guard let countOfLadder = readLine(), let ladder = Int(countOfLadder) else {break}
+    printLadder(makeTwoDimentionofBool(numberOf: participant, countOf: ladder))
 }
+
