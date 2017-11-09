ae8ec1491e3c1f5407f4cb8f0154236bff1fe151
waitNseE <yellow.cjh@gmail.com>
2017-11-09 14:22:34 +0900

    Change the order of ending condition within a function

--- ladderGameStep3/ladderGameStep3/main.swift
+++ ladderGameStep3/ladderGameStep3/main.swift
@@ -12,14 +12,12 @@ while (true) {
     var inputView = InputView()
     // 1-1. 인풋뷰의 입력값 받는 메소드를 호출하여 튜플 상수에 대입
     let (names, heightOfLadder) = inputView.getUserInput()
+    if names.count == 0 {
+        break
+    }
     // 2. ResultView인스턴스 생성
     let resultView = ResultView()
     // 2-2. ResultView의 사다리정보를 출력하는 메소드 호출
     resultView.printFullLadder(height: heightOfLadder, names: names)
-    if names.count == 0 {
-        break
-    }
 }
-
-
 
