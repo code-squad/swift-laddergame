601eb6f24487db92b949ec16db967480bea68918
waitNseE <yellow.cjh@gmail.com>
2017-11-16 14:31:22 +0900

    Separate test-method and delete unnecessary test-method

--- LadderGame/LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -9,29 +9,34 @@
 import XCTest
 
 class UnitTestLadderGame: XCTestCase {
-    func testCheckValues () {
+    func testIsCheckingValueCanThrowError () {
         let inputNames = Array<String>()
         let inputHeight = 0
         let valueData = ValueData(inputNames, inputHeight)
         XCTAssertThrowsError(try CheckingValue().returnResultOfChecking(valueData.names , valueData.heightOfLadder))
     }
     
-    func testLadderOfGame() {
+    func testLadderOfGameisNotNil() {
         let inputNames = ["hoon","pobi","jake","mine"]
         let inputHeight = 5
         let valueData = ValueData(inputNames,inputHeight)
         let ladderGame = LadderGame(inputValues: (valueData.names,valueData.heightOfLadder))
         XCTAssertNotNil(ladderGame)
-        XCTAssertEqual(ladderGame.makeTwoDimentionalArray(ladderGame.participant.count, ladderGame.height).count, 5)
-        XCTAssertNotEqual(ladderGame.makeTwoDimentionalArray(ladderGame.participant.count, ladderGame.height).count, 6)
     }
     
-    func testParticipantsOfLadderGame() {
-        let inputNames = ["honux","pobi","jake","mine"]
+    func testHeightOfLadderIsEqual () {
+        let inputNames = ["hoon","pobi","jake","mine"]
         let inputHeight = 5
         let valueData = ValueData(inputNames,inputHeight)
         let ladderGame = LadderGame(inputValues: (valueData.names,valueData.heightOfLadder))
-        XCTAssertEqual(ladderGame.participant[0].name , "honux")
-        XCTAssertNotNil(ladderGame.participant[3].name)
+        XCTAssertEqual(ladderGame.makeTwoDimentionalArray(ladderGame.participant.count, ladderGame.height).count, 5)
+    }
+    
+    func testHeightOfLadderisNotEqual () {
+        let inputNames = ["hoon","pobi","jake","mine"]
+        let inputHeight = 5
+        let valueData = ValueData(inputNames,inputHeight)
+        let ladderGame = LadderGame(inputValues: (valueData.names,valueData.heightOfLadder))
+        XCTAssertNotEqual(ladderGame.makeTwoDimentionalArray(ladderGame.participant.count, ladderGame.height).count, 6)
     }
 }
