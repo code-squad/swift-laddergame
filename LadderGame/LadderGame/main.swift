079edaeb02d0cbbc8af5f3bf7175ff181e2fb3eb
cocomilktea <coco_milktea@naver.com>
2017-12-22 16:43:13 +0900

    Edit nameOfFunction, delete unnecessary parameter, Use array.count, Edit guard

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -22,45 +22,44 @@ func getHeightOfLadder() -> Int {
     return heightOfLadder
 }
 
-func generateArrOfLadder(_ player: Int,_ heightOfLadder: Int) -> [[String]]{
-    var ladderArr = [[String]](repeating: Array(repeating: " ", count: player-1), count: heightOfLadder)
-    return ladderArr
+func generateLadders(_ player: Int,_ heightOfLadder: Int) -> [[String]]{
+    var ladders = [[String]](repeating: Array(repeating: " ", count: player-1), count: heightOfLadder)
+    return ladders
 }
 
-func editArrHeightOfLadder(_ array: [[String]],_ player: Int,_ height: Int) {
-    for heightIdx in 0..<height {
-        editArrNumOfPlayer(array, player, heightIdx)
+func drawHorizontalLadder(_ array: [[String]]) {
+    for heightIdx in 0..<array.count {
+        drawVerticalLadder(array[heightIdx])
     }
 }
 
-func editArrNumOfPlayer(_ array: [[String]],_ player: Int,_ height: Int) {
-    for playerIdx in 0..<player-1 {
-        getLadderOfArray(array, height, playerIdx)
+func drawVerticalLadder(_ array: [String]) {
+    for playerIdx in 0..<array.count {
+        drawNumberOfLadder()
     }
     print("|")
 }
 
-func getNumOfLadder() -> String {
+func getNumberOfLadder() -> String {
     let randomNum: UInt32 = arc4random_uniform(2)
     let numOfLadder = Int(randomNum)
     guard numOfLadder == 1 else { return " " }
     return "-"
 }
 
-func getLadderOfArray(_ array:[[String]],_ heightIdx: Int,_ playerIdx: Int) {
-    var tempArr = array
-    tempArr[heightIdx][playerIdx] = getNumOfLadder()
-    printLadder(tempArr, heightIdx, playerIdx)
-}
-
-func printLadder(_ arr: [[String]],_ h: Int,_ p: Int) {
-    print("|" + arr[h][p], terminator:"")
+func drawNumberOfLadder() {
+    print("|" + getNumberOfLadder(), terminator:"")
 }
 
 while true {
     let player = getNumOfPlayer()
+    guard player > 1 else { print("1명 이상 참여가능 \n"); continue }
     let height = getHeightOfLadder()
-    let generateArr = generateArrOfLadder(player, height)
-    guard player < 2 else { editArrHeightOfLadder(generateArr, player, height); break }
-    print("1명 이상 참여가능 \n")
+    let generateArr = generateLadders(player, height)
+    drawHorizontalLadder(generateArr)
+    break
 }
+
+
+
+
