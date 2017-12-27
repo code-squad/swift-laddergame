2fa98d4abe04cd98de89bfe1d872a6a687fce5dd
JINAA <jinagold.k@gmail.com>
2017-12-27 11:39:22 +0900

    Modify code 1. rename getPeople() -> getPlayer() 2. change processing optional unwrapping. 3. the host code move into the while statement on the bottom of the code.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -9,28 +9,25 @@
 import Foundation
 
 // 참여인원 입력 받기
-func getPeople() -> Int {
+func getPlayer() -> Int {
     print("참여할 사람은 몇 명 인가요?")
-    let people = readLine()
-    return Int(people!)!
+    let player = readLine()
+    return Int(player ?? "") ?? 0
 }
 
 // 사다리 높이 입력 받기
 func getLadderHeight() -> Int {
     print("최대 사다리 높이는 몇 개인가요?")
     let ladderHeight = readLine()
-    return Int(ladderHeight!)!
+    return Int(ladderHeight ?? "") ?? 0
 }
 
-// 사다리 만들기 구조체로 만들기...ing
-let people = getPeople()
-let ladderHeight = getLadderHeight()
-let colCount = people + (people - 1)
-
 // 사다리 만들기(2차원 배열)
-func makeLadder() -> [[String]] {
+func makeLadder(_ player:Int, _ ladderHeight:Int) -> [[String]] {
+    let colCount = player + (player - 1)
     var arrLadder: [[String]] = Array(repeating: Array(repeating: "|", count: colCount), count: ladderHeight)
     
+
     for raw in 0 ..< arrLadder.count {
         for col in 0 ..< colCount {
             if col % 2 == 1 {
@@ -43,24 +40,24 @@ func makeLadder() -> [[String]] {
 
 // 랜덤번호 추출(추후 만들어진 사다리 배열에 랜덤한 위치에 발판을 추가위한 행,열 랜덤 번호)
 // 행
-func rawRandom() -> Array<Int> {
+func rawRandom(_ player:Int, _ ladderHeight:Int) -> Array<Int> {
     var ladderRowRandom:[Int] = []
     
-    for _ in 0 ..< people {
+    for _ in 0 ..< player {
         ladderRowRandom.append(Int(arc4random_uniform(UInt32(ladderHeight))))
     }
     return ladderRowRandom
 }
 
 // 열
-func colRandom() -> Array<Int> {
+func colRandom(_ player:Int) -> Array<Int> {
     var ladderColRandom:[Int] = []
     var oddRandom = 0
     
-    while ladderColRandom.count != people  {
-        oddRandom = Int(arc4random_uniform(UInt32(people)) + 1)
+    while ladderColRandom.count != player  {
+        oddRandom = Int(arc4random_uniform(UInt32(player)) + 1)
         if oddRandom % 2 == 0 {
-            oddRandom = Int(arc4random_uniform(UInt32(people)) + 1)
+            oddRandom = Int(arc4random_uniform(UInt32(player)) + 1)
         } else {
             ladderColRandom.append(oddRandom)
         }
@@ -69,20 +66,20 @@ func colRandom() -> Array<Int> {
 }
 
 // 사다리 발판 만들기(랜덤하게 만든 행,열 숫자로 발판 만들기)
-func makeFootBoard() -> [[String]] {
-    var madeLadder = makeLadder()
-    let raw = rawRandom()
-    let col = colRandom()
+func makeFootBoard(_ player:Int, _ ladderHeight:Int) -> [[String]] {
+    var madeLadder = makeLadder(player,ladderHeight)
+    let raw = rawRandom(player,ladderHeight)
+    let col = colRandom(player)
     
-    for i in 0 ..< people {
+    for i in 0 ..< player {
         madeLadder[raw[i]][col[i]] = "-"
     }
     return madeLadder
 }
 
 // 사다리 출력
-func printLadder() {
-    let resultLadder = makeFootBoard()
+func printLadder(_ player:Int, _ ladderHeight:Int) {
+    let resultLadder = makeFootBoard(player,ladderHeight)
     for raw in 0 ..< resultLadder.count {
         for col in 0 ..< resultLadder[raw].count {
             print(resultLadder[raw][col], terminator: "")
@@ -90,4 +87,15 @@ func printLadder() {
         print()
     }
 }
-printLadder()
+
+while true {
+    let player = getPlayer()
+    let ladderHeight = getLadderHeight()
+    printLadder(player, ladderHeight)
+    break
+}
+
+
+
+
+
