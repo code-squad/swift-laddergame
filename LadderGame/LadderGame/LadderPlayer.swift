a3fd34cc94dee08361179da74c6dd087f3769a04
HaeSeongPark <cord7894@gmail.com>
2018-03-13 17:33:27 +0900

    add LadderPlayer structrue

---
+++ LadderGame/LadderGame/LadderPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  LadderPlayer.swift
+//  LadderGame
+//
+//  Created by rhino Q on 2018. 3. 13..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct LadderPlayer{
+    let name : String
+}
