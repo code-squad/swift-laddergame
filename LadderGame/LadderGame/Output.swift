d583cf508b67f1d906a9fcbb2d385508bc7d0b43
HaeSeongPark <cord7894@gmail.com>
2018-03-13 15:46:26 +0900

    Output 객체의 printLadder 함수 타입메서드로 변경

--- LadderGame/LadderGame/Output.swift
+++ LadderGame/LadderGame/Output.swift
@@ -10,14 +10,11 @@
  메소드의 라인 최대 10줄
  method가 한 가지일만 하도록
  
- 위 3개 다 메소드를 분리하기
- 
-  Output은 LadderGame이 넘겨준 값으로 사다리 출력
  */
 
 import Foundation
 struct Output {
-    func printLadder(_ ladderMatrix:[[String]]){
+    static func printLadder(_ ladderMatrix:[[String]]){
         for row in ladderMatrix{
             print("|", terminator:"")
             print(row.joined(separator: ""), terminator:"")
--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -3,15 +3,14 @@
  struct를 추가하면 파일을 나눠서 작업하기 파일을 추가하면 커밋을 해야
  좋은 커밋 로그 공부
  */
+ /* 피드백 반영할 것
+ ladderMatrix -> [[Bool]]
  
  
- /*
- LadderGame 이 모든 객체를 생성해서 소유하고 있어서 의존성을 갖고 있는 것보다는
- 다른 객체가 할 일을 넘겨주도록 (=의존성을 줄이도록) 작성하는 게 좋습니다.
- 여기 변수들이 (극단적으로보면) 존재하지 않고, LadderGame을 호출하는 상위 객체에서 넘겨주도록 작성하는 게 좋습니다. 이 부분은 3단계를 할 때 개선해봅시다.
- ->감이 안 잡힘.
+ 0을 입력하면 다시 입력받는 것을 재귀로 호출하는군요!
+ 어떤 함수를 호출하고 반복해서 호출할 지 판단하는 흐름 제어는 그 함수를 호출한 상위 모듈에서 하는게 좋습니다.
+ 하위 모듈에서 무한 반복이 되도록 재귀 호출을 하는 방법은 지양하세요.
  */
-
 
 import Foundation
 func main(){
@@ -20,7 +19,7 @@ func main(){
     let heightOfLadder = input.askHeightOfLadder()
     var ladderGame = LadderGame(numberOfPerson, heightOfLadder)
     let ladderMatrix = ladderGame.makeLadder()
-    Output().printLadder(ladderMatrix)
+    Output.printLadder(ladderMatrix)
 }
 
 main()
