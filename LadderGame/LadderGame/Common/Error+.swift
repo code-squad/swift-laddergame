13d4b60d2b3d052acfe9250f980a6f35d62c6246
linsaeng <duwjdtmd91@gamil.com>
2018-03-19 19:55:16 +0900

    공통

---
+++ LadderGame/LadderGame/Common/Error+.swift
@@ -0,0 +1,14 @@
+//
+//  Error+.swift
+//  LadderGame
+//
+//  Created by Jung seoung Yeo on 2018. 3. 19..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+enum LadderGameError: Error {
+    case isEmptyError
+    case convertError
+    case minusError
+    case limitError
+    case zeroError
+}
