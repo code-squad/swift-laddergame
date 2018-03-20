cc1af170cbc2b3057bec2af2a1c98e8a8962ddbc
linsaeng <duwjdtmd91@gamil.com>
2018-03-20 15:55:16 +0900

    error message 삭제

--- LadderGame/LadderGame/Common/String+.swift
+++ LadderGame/LadderGame/Common/String+.swift
@@ -7,17 +7,10 @@
 //
 
 //메시지
-let LADDERGAME_PARTICIPATION_COUNT_MESSAGE = "참여할 사람은 몇 명 인가요?"
+let LADDERGAME_USER_COUNT_MESSAGE = "참여할 사람은 몇 명 인가요?"
 let LADDERGAME_HEIGHT_COUNT_MESSAGE = "최대 사다리 높이는 몇 개인가요?"
 
 //사다리
 let LADDERGAME_LADDER = "|"
 let LADDERGAME_LADDER_CONNECT = "-"
 let LADDERGAME_LADDER_DISCONNECT = " "
-
-//에러 메시지
-let LADDERGAME_ISEMPTY_ERROR_MESSAGE = "입력 값을 확인 해주세요"
-let LADDERGAME_CONVERT_ERROR_MESSAGE = "숫자를 입력해주세요"
-let LADDERGAME_MINUS_ERROR_MESSAGE = "입력 값이 마이너스 입니다."
-let LADDERGAME_LIMIT_ERROR_MESSAGE = "입력 값의 최대치를 초과 하였습니다."
-let LADDERGAME_ZERO_ERROR_MESSAGE = "입력 숫자가 0 입니다."
