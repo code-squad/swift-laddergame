9de350a17d757d59a00cadfa3c59c606732f65d9
linsaeng <duwjdtmd91@gamil.com>
2018-03-20 15:57:10 +0900

    사다리 만드는 class 추가

--- LadderGame/LadderGame/Controller/Ladder.swift
+++ LadderGame/LadderGame/Controller/Ladder.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+class Ladder {
+    // 연결 부분 가져오기
+    private func getLadderConnectArray(_ userCount: Int) -> Array<String>{
+        
+        var ladderConnectArray: Array<String> = []
+        
+        for _ in 1 ... userCount {
+            ladderConnectArray.append(isConnect())
+        }
+    
+        return ladderConnectArray
+    }
+    
+    private func isConnect() -> String {
+        return (randomNumber() % 2) == 0 ? LADDERGAME_LADDER_CONNECT : LADDERGAME_LADDER_DISCONNECT
+    }
+    
+    private func randomNumber() -> Int {
+        return Int(arc4random_uniform(UINT32_MAX))
+    }
+    
+    private func getRopeArray(_ ropeCount: Int) -> Array<String>{
+        
+        var ropeArray: Array<String> = []
+        for _ in 1 ... ropeCount {
+            ropeArray.append(LADDERGAME_LADDER)
+        }
+        return ropeArray
+    }
+    
+    func makeLadderLine(_ userCount: Int) throws -> String {
+        guard userCount != 0 else {
+            throw LadderGameError.zeroError
+        }
+        
+        guard userCount > 0 else {
+            throw LadderGameError.minusError
+        }
+        
+        let ropeArray = getRopeArray(userCount)
+        let connectArray = getLadderConnectArray(userCount)
+        
+        guard ropeArray.count == connectArray.count else {
+            throw LadderGameError.notEqualsError
+        }
+        
+        var ladderline = ""
+        
+        for count in 0 ..< ropeArray.count {
+            ladderline += ropeArray[count]
+            
+            if count != (ropeArray.count - 1) {
+              ladderline += connectArray[count]
+            }
+        }
+        
+        return ladderline
+    }
+    
+}
