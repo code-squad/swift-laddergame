491146f68d944cb5937d677d7af7c28f36af1db1
linsaeng <duwjdtmd91@gamil.com>
2018-03-21 13:55:54 +0900

    inputcontroller class 삭제

--- LadderGame/LadderGame/Controller/InputController.swift
+++ LadderGame/LadderGame/Controller/InputController.swift
@@ -5,20 +5,14 @@
 //  Created by Jung seoung Yeo on 2018. 3. 20..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
-
-import Foundation
-
-class InputrController {
-    // 숫자를 입력
-    func number() throws -> Int {
-        guard let inputText = readLine() else {
+func number() throws -> Int {
+    guard let inputText = readLine() else {
             throw LadderGameError.isEmptyError
-        }
-        
-        guard let convertInt = Int(inputText) else {
-            throw LadderGameError.convertError
-        }
-        
-        return convertInt
     }
+        
+    guard let convertInt = Int(inputText) else {
+        throw LadderGameError.convertError
+    }
+        
+    return convertInt
 }
