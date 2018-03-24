27631ec68a104049e05d741adff882ae5dddfc16
Moon <myssun0325@gmail.com>
2018-03-24 10:49:30 +0900

    Add function & enumeration in 'InputView' struct
    enum Question : each case have a question string as a raw value
    ask(question:) : print out raw value of Questions's each case

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -9,4 +9,12 @@
 
 struct InputView {
     
+    enum Question: String {
+        case numberOfPeople = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"
+        case numberOfHeight = "최대 사다리 높이는 몇 개인가요?"
+    }
+    
+    func ask(question: Question) {
+        print(question.rawValue)
+    }
 }
