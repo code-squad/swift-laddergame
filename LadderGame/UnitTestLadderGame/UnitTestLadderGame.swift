58633ca59c209269f3e9971c0c82088ee1ef069b
Moon <myssun0325@gmail.com>
2018-03-28 12:53:44 +0900

    Add UnitTest bundle

---
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -0,0 +1,23 @@
+//
+//  UnitTestLadderGame.swift
+//  UnitTestLadderGame
+//
+//  Created by moon on 2018. 3. 27..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestLadderGame: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
