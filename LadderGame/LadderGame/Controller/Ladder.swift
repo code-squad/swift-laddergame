2e2ef18da25e0fa45c0c544611930a8b88dec3ff
linsaeng <duwjdtmd91@gamil.com>
2018-03-28 11:22:02 +0900

    getNotContinuousLadder, contiuousChecker 함수 추가

--- LadderGame/LadderGame/Controller/Ladder.swift
+++ LadderGame/LadderGame/Controller/Ladder.swift
@@ -9,18 +9,45 @@
 import Foundation
 
 private func getLadderConnect(_ userCount: Int) -> Array<Bool>{
-        
-    var ladderConnectArray: Array<Bool> = []
-        
+    var ladderConnect: Array<Bool> = []
+    
     for _ in 1 ..< userCount {
-        ladderConnectArray.append(isConnect())
+        ladderConnect.append(isConnect())
     }
 
-    return ladderConnectArray
+    return ladderConnect
 }
 
 private func isConnect() -> Bool {
     return (randomNumber() % 2) == 0
+}
+
+// 연속적이지 않은 ladder반환
+private func getNotContinuousLadder(_ ladderConnect: Array<Bool> ) -> Array<Bool>{
+    var notContinuousLadder = ladderConnect
+    
+    // 마지막 값은 체크 할 필요가 없으므로 -2
+    for index in 0 ... (notContinuousLadder.count - 2) {
+        notContinuousLadder[index] = contiuousChecker(notContinuousLadder[index], notContinuousLadder[index + 1])
+    }
+    
+    return notContinuousLadder
+}
+
+// 연속적인 값인지 확인
+private func contiuousChecker(_ frontIsConnect: Bool, _ nextIsConnect: Bool) -> Bool{
+    
+    // 두 값이 같은지 확인
+    guard frontIsConnect == nextIsConnect else {
+        return frontIsConnect
+    }
+    
+    // 연결이 되어 있는지 확인
+    guard frontIsConnect else {
+        return frontIsConnect
+    }
+    
+    return false
 }
 
 private func randomNumber() -> Int {
@@ -28,45 +55,26 @@ private func randomNumber() -> Int {
 }
 
 private func inspetionUserCount(_ userCount: Int) throws -> Int{
-    guard userCount != 0 else {
-        throw LadderGameError.zeroError
-    }
-    
-    guard userCount > 0 else {
-        throw LadderGameError.minusError
-    }
-    
-    guard userCount < 10 else {
-        throw LadderGameError.limitError
-    }
+    guard userCount != 0    else    { throw LadderGameError.zeroError  }
+    guard userCount > 0     else    { throw LadderGameError.minusError }
+    guard userCount < 10    else    { throw LadderGameError.limitError }
     return userCount
 }
 
 private func inspetionLadderHeight(_ ladderHeight: Int) throws -> Int {
-    guard ladderHeight != 0 else {
-        throw LadderGameError.zeroError
-    }
-    
-    guard ladderHeight > 0 else {
-        throw LadderGameError.minusError
-    }
-    
-    guard ladderHeight < 10 else {
-        throw LadderGameError.limitError
-    }
-    
+    guard ladderHeight != 0 else    { throw LadderGameError.zeroError  }
+    guard ladderHeight > 0  else    { throw LadderGameError.minusError }
+    guard ladderHeight < 10 else    { throw LadderGameError.limitError }
     return ladderHeight
 }
 
 func makeLadderCunnectionForm(_ userCount: Int, _ ladderHeight: Int) throws -> Array<Array<Bool>> {
-    
     let provenUserCount = try inspetionUserCount(userCount)
     let provenLadderHeight = try inspetionLadderHeight(ladderHeight)
-    
     var ladderConnectForm: Array<Array<Bool>> = []
     
     for _ in 1 ... provenLadderHeight {
-        ladderConnectForm.append(getLadderConnect(provenUserCount))
+        ladderConnectForm.append(getNotContinuousLadder(getLadderConnect(provenUserCount)))
     }
     
     return ladderConnectForm
