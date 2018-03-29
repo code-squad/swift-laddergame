922bb4bbe3f217958ebad8b38a509893b295b623
linsaeng <duwjdtmd91@gamil.com>
2018-03-29 14:49:52 +0900

    출력 문구 변경

--- LadderGame/LadderGame/Common/String+.swift
+++ LadderGame/LadderGame/Common/String+.swift
@@ -7,10 +7,10 @@
 //
 
 //메시지
-let LADDERGAME_USER_COUNT_MESSAGE = "참여할 사람은 몇 명 인가요?"
+let LADDERGAME_USER_COUNT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"
 let LADDERGAME_HEIGHT_COUNT_MESSAGE = "최대 사다리 높이는 몇 개인가요?"
 
 //사다리
 let LADDERGAME_LADDER = "|"
-let LADDERGAME_LADDER_CONNECT = "-"
-let LADDERGAME_LADDER_DISCONNECT = " "
+let LADDERGAME_LADDER_CONNECT = "-----"
+let LADDERGAME_LADDER_DISCONNECT = "     "
