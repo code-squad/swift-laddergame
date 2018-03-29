889c682dd6c930ae79a6ff969e6b8e4e23d30167
linsaeng <duwjdtmd91@gamil.com>
2018-03-29 14:52:08 +0900

    number 함수 -> heigth로 변경, playernames함수 추가

--- LadderGame/LadderGame/Controller/Input.swift
+++ LadderGame/LadderGame/Controller/Input.swift
@@ -5,5 +5,25 @@
 //  Created by Jung seoung Yeo on 2018. 3. 29..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
-
-import Foundation
+struct Input {
+    func heigth() throws -> Int {
+        guard let inputText = readLine() else {
+            throw LadderGameError.isEmptyError
+        }
+        
+        guard let convertInt = Int(inputText) else {
+            throw LadderGameError.convertError
+        }
+        
+        return convertInt
+    }
+    
+    func playerNames() throws -> [LadderPlayer] {
+        guard let names = readLine() else {
+            throw LadderGameError.isEmptyError
+        }
+        return names.split(separator: ",").map{
+                String($0)
+                }.map{ LadderPlayer(name: $0) }
+    }
+}
