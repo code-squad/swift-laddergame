3a10c70863a6c93540ee50da9bc68d6b1c659da3
linsaeng <duwjdtmd91@gamil.com>
2018-04-02 12:08:37 +0900

    오타 수정

--- LadderGame/LadderGame/Controller/main.swift
+++ LadderGame/LadderGame/Controller/main.swift
@@ -10,13 +10,13 @@ import Foundation
 func main() {
        print(LADDERGAME_USER_COUNT_MESSAGE)
     do {
-        let palyers = try Input().playerNames()
+        let players = try Input().playerNames()
         print(LADDERGAME_HEIGHT_COUNT_MESSAGE)
         let ladderHieght = try Input().height()
-        let ladderGame = LadderGame.init(palyers, ladderHieght)
+        let ladderGame = LadderGame.init(players, ladderHieght)
         let ladderConnectForm = try ladderGame.makeLadderForm()
         Output().ladder(ladderConnectForm)
-        Output().player(palyers)
+        Output().player(players)
         
     } catch {
         print("사다리 게임 에러")
