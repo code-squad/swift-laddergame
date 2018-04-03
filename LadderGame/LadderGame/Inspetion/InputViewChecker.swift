903c3538ba2de45057ea9f398f06d03255041c45
linsaeng <duwjdtmd91@gamil.com>
2018-04-03 12:25:46 +0900

    checker 생성

--- LadderGame/LadderGame/Inspetion/InputViewChecker.swift
+++ LadderGame/LadderGame/Inspetion/InputViewChecker.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-import Foundation
+struct InputViewChecker {
+    static func checker(_ players: [LadderPlayer], _ height: Int) -> Bool {
+        return playerChecker(players) && heightChecker(height)
+    }
+    
+    static private func playerChecker(_ players: [LadderPlayer]) -> Bool {
+        let playerCount = players.count
+        guard playerCount <= 0 || playerCount > 10 else {
+            return true
+        }
+        
+        return false
+    }
+    
+    static private func heightChecker(_ height: Int) -> Bool {
+        let result = (height > 0) && (height != 0) && (height < 10)
+        
+        return result
+    }
+}
--- LadderGame/LadderGame/Inspetion/LadderGameChecker.swift
+++ LadderGame/LadderGame/Inspetion/LadderGameChecker.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
-import Foundation
+struct LadderGameChecker {
+    
+    static func continuous(_ ladderConnectForm: Array<Array<Bool>>) -> Bool {
+        for ladderConnectLine in ladderConnectForm {
+            guard notCotinuousChecker(ladderConnectLine) else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    static private func notCotinuousChecker(_ ladderConnectLine: Array<Bool>) -> Bool {
+        guard !ladderNotCheckCount(ladderConnectLine.count) else {
+            return true
+        }
+        return ladderConnectLine.count == 2 ? twoRopeChecker(ladderConnectLine) : contiuniousChecker(ladderConnectLine)
+    }
+    
+    static private func ladderNotCheckCount(_ connectCount: Int) -> Bool {
+       
+        guard connectCount != 1 else {
+            return true
+        }
+        
+        guard connectCount >= 2 else {
+            return true
+        }
+        return false
+    }
+    
+    static private func contiuniousChecker(_ ladderConnectLine: Array<Bool>) -> Bool {
+        for index in 0 ... (ladderConnectLine.count - 2) {
+            guard ladderConnectLine[index] == ladderConnectLine[index + 1] && ladderConnectLine[index] else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    static private func twoRopeChecker(_ ladderConnectLine: Array<Bool>) -> Bool {
+        guard ladderConnectLine[0] == ladderConnectLine[1] && ladderConnectLine[0] else {
+            return true
+        }
+        return false
+    }
+}
