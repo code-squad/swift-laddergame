4f772cc7cb80dc280d68c5a106a67fabb8c2f52c
DrakeYang <drake.the.programmer@gmail.com>
2018-04-03 16:51:24 +0900

    문자열 배열 생성을 Bool 배열로 수정. 프린트 구조체에서 Bool 배열을 받아서 String 으로 출력하도록 수정

--- LadderGame/LadderGame/LadderGameBoardMaker.swift
+++ LadderGame/LadderGame/LadderGameBoardMaker.swift
@@ -26,57 +26,30 @@ struct LadderGameBoardMaker {
         return false
     }
     
-    /// 가로사다리를 넣어도 되면 가로사다리를, 안되면 빈가로사다리를 리턴
-    private func sideLadderFrom(aheadLadder : Bool)->String{
-        var sideLadder = LadderType.none.rawValue
-        if aheadLadder {
-            sideLadder = LadderType.side.rawValue
-        }
-        return String(repeating : sideLadder, count: Limiter.nameLengthLimit())
-    }
-    
     /// 사다리게임 가로줄만 있는 1차원 배열 리턴
-    private func makeSideLadders(peopleNumber : Int)-> Array<String>{
+    private func makeSideLadders(peopleNumber : Int)-> Array<Bool>{
         // 앞자리 가로사다리 우선 없다고 체크
         var aheadSideLadder = false
         // 리턴용 배열 생성
-        var sideLadders = Array<String>()
+        var sideLadders = Array<Bool>()
         // 입력받은 사람수 -1 만큼 반복문을 돌린다. 가로사다리는 사람보다 1개 적다.
         for _ in (0..<peopleNumber-1){
             // 앞자리에 가로사다리 여부 체크 후 변수에 가로나 세로사다리 입력
             aheadSideLadder = sideLadderAfter(aheadSideLadder: aheadSideLadder)
             // 현제 자리에 사다리를 넣어준다.
-            sideLadders.append(sideLadderFrom(aheadLadder: aheadSideLadder))
+            sideLadders.append(aheadSideLadder)
         }
         return sideLadders
     }
     
-    /// 사다리게임의 가로줄 1개에 해당하는 1차원 배열을 리턴. 입력값 사람수.
-    private func makeLadders(sideLadders : Array<String>) -> Array<String>{
-        // 리턴용 배열 선언. 이름길이 5자를 위해서 두칸 공백을 입력
-        var ladders = [LadderType.startSpace.rawValue]
-        // 첫번째 칸은 세로 사다리
-        ladders.append(LadderType.up.rawValue)
-        // 가로사다리의 개수만큼 반복한다
-        for sideLadder in sideLadders{
-            // 현제 자리에 사다리를 넣어준다.
-            ladders.append(sideLadder)
-            // 가로사다리 입력 후 다시 세로사다리 추가
-            ladders.append(LadderType.up.rawValue)
-        }
-        return ladders
-    }
-    
     /// 입력받은 횟수만큼 사다리 1채원 배열을 생성해서 2차원 배열로 리턴
-    func makeLadderGameBoard(peopleNumber : Int, ladderNumber : Int)->Array<Array<String>>{
+    func makeLadderGameBoard(peopleNumber : Int, ladderNumber : Int)->Array<Array<Bool>>{
         // 리턴용 2차원 배열을 생성
-        var ladderGameBoard = Array<Array<String>>()
+        var ladderGameBoard = Array<Array<Bool>>()
         // 입력받은 사다리 수 만큼 반복한다
         for _ in (0..<ladderNumber){
-            // 1차원 배열을 생성해서 2차원 배열에 추가해준다. 우선 가로사다리만 있는 배열 생성 후
-            let sideladders = makeSideLadders(peopleNumber: peopleNumber)
-            // 세로사다리를 합친 1차원 배열을 2차원 배열에 추가
-            ladderGameBoard.append(makeLadders(sideLadders: sideladders))
+            // 1차원 배열을 생성해서 2차원 배열에 추가해준다. 
+            ladderGameBoard.append(makeSideLadders(peopleNumber: peopleNumber))
         }
         return ladderGameBoard
     }
--- LadderGame/LadderGame/Printer.swift
+++ LadderGame/LadderGame/Printer.swift
@@ -10,25 +10,50 @@ import Foundation
 
 //출력을 위한 구조체
 struct Printer {
-    /// 입력받은 2차원 사다리게임을 보기 좋게 프린트
-    private func printLadderGameBoard(ladderGameBoard : Array<Array<String>>){
-        // 출력용 변수 선언
-        var printedLadderGame = ""
-        // 입력값으로 받은 2차원 배열을 반복문에 돌려서
-        for layer in ladderGameBoard {
-            // 각 배열을 출력용 변수에 추가. 줄바꿈 문자도 끝에 추가.
-            printedLadderGame += layer.joined()+"\n"
-        }
-        print (printedLadderGame)
-    }
-    
     /// 입력받은 1차원 배열을 보기 좋게 프린트
     private func printPeopleList(list : Array<String>){
         print ( list.joined())
     }
     
+    /// 가로사다리를 넣어도 되면 가로사다리를, 안되면 빈가로사다리를 리턴
+    private func sideLadderFrom(aheadLadder : Bool)->String{
+        var sideLadder = LadderType.none.rawValue
+        if aheadLadder {
+            sideLadder = LadderType.side.rawValue
+        }
+        return String(repeating : sideLadder, count: Limiter.nameLengthLimit())
+    }
+    
+    /// 사다리게임의 가로줄 1개에 해당하는 1차원 배열을 리턴. 입력값 사람수.
+    private func makeLadders(sideLadders : Array<Bool>) -> Array<String>{
+        // 리턴용 배열 선언. 이름길이 5자를 위해서 두칸 공백을 입력
+        var ladders = [LadderType.startSpace.rawValue]
+        // 첫번째 칸은 세로 사다리
+        ladders.append(LadderType.up.rawValue)
+        // 가로사다리의 개수만큼 반복한다
+        for sideLadder in sideLadders{
+            // 현제 자리에 사다리를 넣어준다.
+            ladders.append(sideLadderFrom(aheadLadder: sideLadder))
+            // 가로사다리 입력 후 다시 세로사다리 추가
+            ladders.append(LadderType.up.rawValue)
+        }
+        return ladders
+    }
+    
+    /// 입력받은 2차원 사다리게임을 보기 좋게 프린트
+    private func printLadderGameBoard(ladderGameBoard : Array<Array<Bool>>){
+        // 출력용 변수 선언
+        var printedLadderGame = ""
+        // 입력값으로 받은 2차원 배열을 반복문에 돌려서
+        for layer in ladderGameBoard {
+            // 각 배열을 출력용 변수에 추가. 줄바꿈 문자도 끝에 추가.
+            printedLadderGame += makeLadders(sideLadders: layer).joined()+"\n"
+        }
+        print (printedLadderGame)
+    }
+    
     /// 전체 게임 내용을 프린트
-    func printLadderGame(ladderGameBoard : Array<Array<String>>,peopleList : Array<String>){
+    func printLadderGame(ladderGameBoard : Array<Array<Bool>>,peopleList : Array<String>){
         printLadderGameBoard(ladderGameBoard : ladderGameBoard)
         printPeopleList(list : peopleList)
     }
