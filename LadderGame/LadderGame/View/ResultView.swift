ca0957e2946a9a0fa85bda4ff8e11bebf83baa40
linsaeng <duwjdtmd91@gamil.com>
2018-04-03 12:24:28 +0900

    output -> ResultView로 변경

--- LadderGame/LadderGame/View/ResultView.swift
+++ LadderGame/LadderGame/View/ResultView.swift
@@ -5,5 +5,22 @@
 //  Created by Jung seoung Yeo on 2018. 4. 2..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
-
-import Foundation
+struct ResultView {
+    static func ladder(_ ladderConnectFrom: Array<Array<Bool>>) {
+        for connectArray in ladderConnectFrom {
+            print(terminator: "  ")
+            print(LADDERGAME_LADDER, terminator: "")
+            for isConnect in connectArray {
+                print((isConnect) ? LADDERGAME_LADDER_CONNECT : LADDERGAME_LADDER_DISCONNECT, terminator: LADDERGAME_LADDER)
+            }
+            print()
+        }
+    }
+    
+    static func player(_ LadderPlayers: [LadderPlayer]) {
+        for player in LadderPlayers {
+            print(player.name, terminator: "  ")
+        }
+        print()
+    }
+}
