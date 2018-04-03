af4ef1afd2770983f2e2d703df40eafd134f594e
linsaeng <duwjdtmd91@gamil.com>
2018-04-03 12:25:25 +0900

    input -> InputView로 변경

--- LadderGame/LadderGame/View/InputView.swift
+++ LadderGame/LadderGame/View/InputView.swift
@@ -5,5 +5,28 @@
 //  Created by Jung seoung Yeo on 2018. 4. 2..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
-
-import Foundation
+struct InputView {
+    
+    private func inputText() throws -> String {
+        guard let inputText = readLine() else {
+            throw LadderGameError.isEmptyError
+        }
+        return inputText
+    }
+    
+    func height() throws -> Int {
+        let inputText = try self.inputText()
+        guard let height = Int(inputText) else {
+            throw LadderGameError.convertError
+        }
+        
+        return height
+    }
+    
+    func playerNames() throws -> [LadderPlayer] {
+        let names = try inputText()
+        return names.split(separator: ",").map{
+            String($0)
+            }.map{ LadderPlayer(name: $0) }
+    }
+}
