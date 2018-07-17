a0aa87b0c0ffc322525f9edd23324f83f317c82a
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-17 10:48:54 +0900

    Ladder Game Stage 2

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -19,9 +19,8 @@ func startGame(){
     var ladder:LadderType = []
     guard let applicants = getInputValue(comment: "참여할 사람은 몇 명 인가요?") else { return }
     guard let ladderHeight = getInputValue(comment: "최대 사다리 높이는 몇 개인가요?") else { return }
-    ladder = makeLadder(applicants: applicants, height: ladderHeight)
-    
-    displayLadder(ladder)
+    ladder = generateLadder(applicants: applicants, height: ladderHeight)
+    display(ladder: ladder)
 }
 
 func getInputValue(comment: String) -> Int?{
@@ -33,47 +32,53 @@ func getInputValue(comment: String) -> Int?{
     return nil
 }
 
-func makeLadder(applicants: Int, height: Int) -> LadderType {
-    let frame = makeLadderFrame(applicants: applicants, height: height)
+func generateLadder(applicants: Int, height: Int) -> LadderType {
+    let frame = generateLadderFrame(applicants: applicants, height: height)
     let ladder = insertBridge(to: frame)
     return ladder
 }
 
-func makeLadderFrame(applicants: Int, height: Int) -> LadderType {
+func generateLadderFrame(applicants: Int, height: Int) -> LadderType {
     let emptyLadder:LadderType = [[String]].init(repeating: [], count: height) // [ [], [], [], .... ]
     let frame = emptyLadder.map { (_) -> [String] in
-        return makeLadderStageFrame(applicants)
+        return generateStageFrame(applicants)
     }
     return frame
 }
 
-func makeLadderStageFrame(_ applicants: Int) -> [String]{
+func generateStageFrame(_ applicants: Int) -> [String]{
     let stage = [String].init(repeating: "|", count: applicants * 2 - 1) // [ "|", "|", "|", "|", "|"]
-    let result = stage.enumerated().map({ $0.offset % 2 != 0 ? " " : $0.element}) // ["|", " ", "|", " ", "|"]
-    return result
+    return stage
 }
 
 func insertBridge(to frame: LadderType) -> LadderType{
     let ladderFrame = frame
-    let ladderWithBridge = ladderFrame.map {$0.map(makeRandomBridge)}
+    let ladderWithBridge = ladderFrame.map(generateValidStage)
     return ladderWithBridge
 }
 
-func makeRandomBridge(space: String) -> String {
-    let shouldInsertBridge = Int(arc4random_uniform(2))
-    if space == " " && shouldInsertBridge == 1{ // 빈 공간에서 1 이면 다리를 만들고 0 이면 패스
-        return "-"
+func generateValidStage(_ stage: [String]) -> [String] {
+    var stageWithBridge = stage.enumerated().map(generateRandomBridge)
+    while !isValidStage(stageWithBridge) {
+        stageWithBridge = stage.enumerated().map(generateRandomBridge)
     }
-    return space
+    return stageWithBridge
 }
 
-func displayLadder(_ ladder: LadderType){
-    ladder.forEach(displayStage)
+func generateRandomBridge(transform: (offset: Int, element: String)) -> String {
+    let shouldInsertBridge = Int(arc4random_uniform(2))
+    if transform.offset % 2 == 1{
+        return shouldInsertBridge == 1 ? "-" : " " // 1 이면 다리를 만들고 0 이면 패스
+    }
+    return transform.element
 }
 
-func displayStage(_ stage: [String]) {
-    stage.forEach({print($0, terminator:"")})
-    print("")
+func isValidStage(_ stage:[String]) -> Bool {
+    return !stage.joined().contains("-|-")
+}
+
+func display(ladder: LadderType){
+    ladder.forEach({print($0.joined())})
 }
 
 startGame()
