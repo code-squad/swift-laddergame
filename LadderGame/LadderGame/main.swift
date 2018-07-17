eeaad4ec34b3970fb3f55355f6422c3279fde4eb
oingbong <oingbong@gmail.com>
2018-07-17 19:23:17 +0900

    ladder-step2 : repectoring code by requirements

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -10,15 +10,30 @@ import Foundation
 
 typealias LadderElements = (person:Int, ladder:Int)
 
-func main(){
+enum Foothold {
+    case `default`
+    case have
+    case none
     
+    func description() -> String {
+        switch self {
+        case .default:
+            return "|"
+        case .have:
+            return "-|"
+        case .none:
+            return " |"
+        }
+    }
+}
+
+func main(){
     let ladderElements:LadderElements = (person: getPerson(), ladder: getLadder())
     
     let ladders:[[String]] = makeLadder(elements: ladderElements)
   
     // print
     printLadders(elements: ladders)
-    
 }
 
 // 입력 받는 함수
@@ -36,51 +51,80 @@ func getLadder() -> Int {
 
 // 결과 출력 함수
 func printLadders(elements:[[String]]){
+    for valueX in 0..<elements.count {
+        print(takeOutFloor(elements: elements, valueX: valueX))
+    }
+}
+
+func takeOutFloor(elements:[[String]], valueX:Int) -> String{
+    var element:String = ""
+    for valueY in 0..<elements[valueX].count {
+        element.append(elements[valueX][valueY])
+    }
+    return element
+}
+
+// 사다리 발판 값을 변경하는 함수
+func switchHaveLadderValue(value:Int) -> Int {
+    // only : 0 or 1
+    guard value == 0 else {
+        return 0
+    }
+    return 1
+}
+
+// 검증 하는 함수
+func verifyDuplication(first:String, second:String, select:Int) -> String {
+    guard first == Foothold.have.description() && second == Foothold.have.description() else {
+        return haveHorizontalLadder(have: select)
+    }
+    return haveHorizontalLadder(have: switchHaveLadderValue(value: Int(select)))
+}
+
+// 사다리의 층 만드는 함수
+func makeFloor( element: Int) -> Array<String> {
+    var floor = Array<String>()
+    floor.append(defaultVertical()) // Default Setting
     
-    for x in 0..<elements.count {
-        var element:String = ""
-        for y in 0..<elements[x].count {
-            element.append(elements[x][y])
-        }
-        print(element)
+    for _ in 1..<element {
+        let select = Int(arc4random_uniform(2))
+        
+        let lastElement = floor.last
+        let newElement = haveHorizontalLadder(have: Int(select))
+        
+        floor.append(verifyDuplication(first: lastElement!, second: newElement, select: select))
     }
     
+    return floor
 }
 
 // 사다리 만드는 함수
 func makeLadder(elements:LadderElements) -> [[String]] {
     var ladders = [[String]]()
-    // 사다리 높이
+    
     for _ in 0..<elements.ladder {
-        // 사람인원에 따른 사다리 개수
-        var floor = Array<String>()
-        floor.append(defaultVertical())
-        for _ in 1..<elements.person {
-            let select = arc4random_uniform(2)
-            floor.append(haveHorizontalLadder(have: Int(select)))
-        }
-        ladders.append(floor)
+        ladders.append(makeFloor(element: elements.person))
     }
     
     return ladders
 }
 
+
 func defaultVertical() -> String {
-    return "|"
+    return Foothold.default.description()
 }
 func falseHorizontalLadder() -> String {
-    return " |"
+    return Foothold.none.description()
 }
 func trueHorizontalLadder() -> String {
-    return "-|"
+    return Foothold.have.description()
 }
 func haveHorizontalLadder(have:Int) -> String {
     // only : 0 or 1
-    if have == 0 {
-        return falseHorizontalLadder()
-    }else {
+    guard have == 0 else {
         return trueHorizontalLadder()
     }
+    return falseHorizontalLadder()
 }
 
 main()
