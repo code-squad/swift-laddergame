817975263f3c178e459fe8da396eaded0582b1eb
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-17 14:12:24 +0900

    Input struct

---
+++ LadderGame/LadderGame/Input.swift
@@ -0,0 +1,24 @@
+//
+//  Input.swift
+//  LadderGame
+//
+//  Created by 이동건 on 17/07/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    func readApplicants() -> [String]?{
+        print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)")
+        guard let input = readLine() else { return nil }
+        let applicants = input.split(separator: ",")
+        return applicants.map {String($0)}
+    }
+    
+    func readLadderHeight()->Int? {
+        print("최대 사다리 높이는 몇 개인가요?")
+        guard let input = readLine() else { return nil }
+        return Int(input)
+    }
+}
