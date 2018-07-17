c9bfd9290939afe2b2244c1fe4492206c7f3f59a
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-17 14:41:57 +0900

    Player struct

---
+++ LadderGame/LadderGame/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  LadderGame
+//
+//  Created by 이동건 on 17/07/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct LadderPlayer {
+    let name: String
+}
