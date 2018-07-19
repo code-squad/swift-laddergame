0cbd6fb6370846de6d834e95654dbdc1cb89b77a
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-19 17:31:52 +0900

    LadderGame Test

---
+++ LadderGame/UnitTestLadderGame/LadderGameTests.swift
@@ -0,0 +1,38 @@
+//
+//  LadderGameTests.swift
+//  UnitTestLadderGame
+//
+//  Created by 이동건 on 19/07/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class LadderGameTests: XCTestCase {
+
+    private var height:Int!
+    private var ladderPlayer:[LadderPlayer]!
+    private var applicants: Int!
+    private var ladderGame: LadderGame!
+    
+    override func setUp() {
+        height = 5
+        ladderPlayer = [LadderPlayer(name: "testname"), LadderPlayer(name: "test")]
+        applicants = ladderPlayer.count
+        ladderGame = LadderGame(height: height, applicants: ladderPlayer)
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        height = nil
+        ladderPlayer = nil
+    }
+    
+    func testInitializer(){
+        XCTAssertNoThrow(LadderGame(height: height, applicants: ladderPlayer), "초기화 실패")
+    }
+    
+    func testGenerateValidStage(){
+        ladderGame.generateLadder()
+    }
+}
