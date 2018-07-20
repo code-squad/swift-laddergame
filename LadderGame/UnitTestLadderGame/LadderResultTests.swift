1627436b1c3f60ca89faf2080cdd4ac8776245be
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-07-20 12:16:19 +0900

    Result Display 테스트 케이스별로 메소드 나눔.

--- LadderGame/UnitTestLadderGame/LadderResultTests.swift
+++ LadderGame/UnitTestLadderGame/LadderResultTests.swift
@@ -23,71 +23,24 @@ class LadderResultTests: XCTestCase {
         XCTAssertNoThrow(Result(element: element, applicants: applicants), "초기화 실패")
     }
     
-    func testDisplay(){
-        // Pass Cases
-        
-        var result = Result(element: [[true, false], [false, true]],
+    func testDisplayPassCase(){
+        let result = Result(element: [[true, false], [false, true]],
                              applicants: [LadderPlayer(name: "asdfg"), LadderPlayer(name: "asdfg"), LadderPlayer(name: "asd")])
-        var displayForm = result.generateDisplayForm().joined()
-        var expectedDisplayForm = ["   |-----|     |",
+        let displayForm = result.generateDisplayForm().joined()
+        let expectedDisplayForm = ["   |-----|     |",
                                    "   |     |-----|",
                                    " asdfg asdfg  asd "].joined()
         XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        result = Result(element: [[false]],
-                             applicants: [LadderPlayer(name: "tst"), LadderPlayer(name: "tst")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |     |",
-                               "  tst   tst "].joined()
-        XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        result = Result(element: [[true, false]],
-                             applicants: [LadderPlayer(name: "a"), LadderPlayer(name: "asdfg"), LadderPlayer(name: "as")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |-----|     |",
-                               "   a   asdfg  as  "].joined()
-        XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        result = Result(element: [[true, false, false], [false, false, true], [true, false, true]],
-                             applicants: [LadderPlayer(name: "as"), LadderPlayer(name: "asd"), LadderPlayer(name: "asdf"), LadderPlayer(name: "a")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |-----|     |     |",
-                               "   |     |     |-----|",
-                               "   |-----|     |-----|",
-                               "  as    asd  asdf    a  "].joined()
-        XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        // Fail Cases
-        
-        result = Result(element: [[true, false], [false, true]],
-                                 applicants: [LadderPlayer(name: "asd"), LadderPlayer(name: "asdfg"), LadderPlayer(name: "asd")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |-----|     |",
-                               "   |     |-----|",
-                               " asd asdfg  asd "].joined()
-        XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        result = Result(element: [[true]],
-                                 applicants: [LadderPlayer(name: "tst"), LadderPlayer(name: "tst")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |----|",
-                               "  tst   tst "].joined()
-        XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        result = Result(element: [[false, false]],
-                                 applicants: [LadderPlayer(name: "asdf"), LadderPlayer(name: "asdfg"), LadderPlayer(name: "as")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |     |     |",
-                               "  asdf asdfg  as  "].joined()
-        XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
-        
-        result = Result(element: [[false, true, false], [false, false, true], [true, false, true]],
-                                 applicants: [LadderPlayer(name: "asdfg"), LadderPlayer(name: "asd"), LadderPlayer(name: "asdf"), LadderPlayer(name: "a")])
-        displayForm = result.generateDisplayForm().joined()
-        expectedDisplayForm = ["   |     |-----|     |",
-                               "   |     |     |-----|",
-                               "   |-----|     |-----|",
-                               "asdfg   asd  asdf    a  "].joined()
+    }
+    
+    func testDisplayFailCase(){
+        let result = Result(element: [[false, true, false], [false, false, true], [true, false, true]],
+                        applicants: [LadderPlayer(name: "asdfg"), LadderPlayer(name: "asd"), LadderPlayer(name: "asdf"), LadderPlayer(name: "a")])
+        let displayForm = result.generateDisplayForm().joined()
+        let expectedDisplayForm = ["   |     |-----|     |",
+                                   "   |     |     |-----|",
+                                   "   |-----|     |-----|",
+                                   "asdfg   asd  asdf    a  "].joined()
         XCTAssertEqual(displayForm, expectedDisplayForm, "정상적인 출력 값이 아닙니다.")
     }
 }
