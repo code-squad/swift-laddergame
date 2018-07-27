85e76c494aba9aec7e7b97fed05d180f3552016d
Jobs <39956881+ITzombietux@users.noreply.github.com>
2018-07-26 19:24:40 -0700

    Update main.swift
    1. 하나의 함수에 하나의 입력만 받도록 수정
    2. 데이터 구조를 만드는 함수, 출력 함수 분리
    3. 사다리 데이터 구조 변경

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,33 +8,33 @@
 
 import Foundation
 
-//사다리 게임 메인
-func radderGameMain () {
-    
-    inputLaddergameData()
-}
-
-//참여할 사람, 사타리 높이 정하기
-func inputLaddergameData () {
-    //참여할 인원 입력 받음
+//참여할 사람 입력 받기
+func inputUserData () -> String {
     print("참여할 사람은 몇 명 인가요?")
     let inputUserNumber = readLine()
-    //최대 사다리 높이 입력 받음
+    return inputUserNumber!
+}
+//사다리 높이 정하기
+func inputLadderLength () -> String {
     print("최대 사다리 높이는 몇 개인가요?")
     let inputMaxLadderNumber = readLine()
-    var createLadder = ladderCreater(inputUserNumber!, inputMaxLadderNumber!)
+    return inputMaxLadderNumber!
+}
+// 출력 함수
+func printLadder(_ playerNumber: Int) {
+    for _ in 0..<playerNumber-1 {
+        print("ㅣ", terminator: ladderRowCreater())
+    }
 }
 
-
-//사다리 생성
-func ladderCreater(_ userNumber:String, _ maxLadderNumber:String) {
-    var createFrame = [[String]](repeating: Array(repeating: "|", count: Int(userNumber)!), count : Int(maxLadderNumber)!)
+// 사다리 프레임 생성
+func ladderFrameCreater(_ playerNumber: Int, _ maxLadderNumber: Int) {
+    var maxLadderNumber = maxLadderNumber
     
-    for createFrame in createFrame {
-        for createFrame in createFrame {
-            print(createFrame, terminator: ladderRowCreater())
-        }
-        print("")
+    while 0 < maxLadderNumber {
+        printLadder(playerNumber)
+        print("ㅣ")
+        maxLadderNumber -= 1
     }
 }
 
@@ -47,6 +47,13 @@ func ladderRowCreater() -> String {
     return "-"
 }
 
+//사다리 게임 메인
+func radderGameMain () {
+    let inputUserNumber = inputUserData()
+    let playerNumber = Int(inputUserNumber) // 입력 값 Int로 바꾸기
+    let inputMaxLadderNumber = inputLadderLength()
+    let maxLadderNumber = Int(inputMaxLadderNumber) // 입력 값 Int로 바꾸기
+    let createLadder = ladderFrameCreater(playerNumber!, maxLadderNumber!)
+}
 // 실행
 radderGameMain()
-
