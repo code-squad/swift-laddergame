c9084c15922fddf21fa4c8755b8a0795db4ea68c
Jobs <39956881+ITzombietux@users.noreply.github.com>
2018-07-26 19:46:52 -0700

    Update main.swift

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,53 +7,45 @@
 //
 
 import Foundation
-
-//참여할 사람 입력 받기
-func inputUserData () -> String {
-    print("참여할 사람은 몇 명 인가요?")
-    let inputUserNumber = readLine()
-    return inputUserNumber!
-}
-//사다리 높이 정하기
-func inputLadderLength () -> String {
-    print("최대 사다리 높이는 몇 개인가요?")
-    let inputMaxLadderNumber = readLine()
-    return inputMaxLadderNumber!
-}
-// 출력 함수
-func printLadder(_ playerNumber: Int) {
-    for _ in 0..<playerNumber-1 {
-        print("ㅣ", terminator: ladderRowCreater())
-    }
-}
-
-// 사다리 프레임 생성
-func ladderFrameCreater(_ playerNumber: Int, _ maxLadderNumber: Int) {
-    var maxLadderNumber = maxLadderNumber
-    
-    while 0 < maxLadderNumber {
-        printLadder(playerNumber)
-        print("ㅣ")
-        maxLadderNumber -= 1
-    }
-}
-
-//가로줄 사다리 생성
-func ladderRowCreater() -> String {
-    let rowCreate: Bool = arc4random_uniform(2) == 0 ? true : false
-    guard rowCreate == true else {
-        return  " " }
-    
-    return "-"
-}
-
-//사다리 게임 메인
-func radderGameMain () {
-    let inputUserNumber = inputUserData()
-    let playerNumber = Int(inputUserNumber) // 입력 값 Int로 바꾸기
-    let inputMaxLadderNumber = inputLadderLength()
-    let maxLadderNumber = Int(inputMaxLadderNumber) // 입력 값 Int로 바꾸기
-    let createLadder = ladderFrameCreater(playerNumber!, maxLadderNumber!)
-}
-// 실행
-radderGameMain()
+  
+ +//사다리 게임 메인
+ +func radderGameMain () {
+ +    
+ +    inputLaddergameData()
+ +}
+ +
+ +//참여할 사람, 사타리 높이 정하기
+ +func inputLaddergameData () {
+ +    //참여할 인원 입력 받음
+ +    print("참여할 사람은 몇 명 인가요?")
+ +    let inputUserNumber = readLine()
+ +    //최대 사다리 높이 입력 받음
+ +    print("최대 사다리 높이는 몇 개인가요?")
+ +    let inputMaxLadderNumber = readLine()
+ +    var createLadder = ladderCreater(inputUserNumber!, inputMaxLadderNumber!)
+ +}
+ +
+ +
+ +//사다리 생성
+ +func ladderCreater(_ userNumber:String, _ maxLadderNumber:String) {
+ +    var createFrame = [[String]](repeating: Array(repeating: "|", count: Int(userNumber)!), count : Int(maxLadderNumber)!)
+ +    
+ +    for createFrame in createFrame {
+ +        for createFrame in createFrame {
+ +            print(createFrame, terminator: ladderRowCreater())
+ +        }
+ +        print("")
+ +    }
+ +}
+ +
+ +//가로줄 사다리 생성
+ +func ladderRowCreater() -> String {
+ +    let rowCreate: Bool = arc4random_uniform(2) == 0 ? true : false
+ +    guard rowCreate == true else {
+ +        return  " " }
+ +    
+ +    return "-"
+ +}
+ +
+ +// 실행
+ +radderGameMain()
