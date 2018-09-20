c727e1905acfdcf9c09d7f9a8b1bb7d9f4d9dc78
김장수 <jangsookim.me@gmail.com>
2018-09-20 19:07:48 +0900

    사다림 게임 시작하기 STEP1

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,69 @@
 
 import Foundation
 
+// 사다리게임
+
+// 사다리를 출력하는 함수
+func printOut(_ ladders: [[String]]) {
+    for ladder in ladders {
+        print(ladder[0])
+    }
+}
+
+// 전체 사다리를 만드는 함수 (2차원 배열)
+// 계단 배열을 초기화해주고 각 단계의 계단을 대입
+func makeLadderUsing(_ numberOfPeople: Int, _ numberOfStairs: Int) -> [[String]] {
+    var ladders = Array(repeating: [String](), count: numberOfStairs)
+
+    for j in 0...numberOfStairs-1 {
+        ladders[j] = makeOneStairUsing(numberOfPeople)
+    }
+
+    return ladders
+}
+
+// 한 단계의 계단을 만드는 함수
+// 전체 사다리를 만드는 함수 makeLadderUsing에 종속
+func makeOneStairUsing(_ numberOfPeople: Int) -> [String]{
+    var ladder = ""
+    let steps = ["-", " "]
+    var stair = [String]()
+
+    for i in 0...(2*numberOfPeople-2) {
+        if i%2 == 0 {
+            ladder += "|"
+        } else {
+            // 사다리 다리를 랜덤으로 추출하기 위해, 다리 모음의 Index를 무작위 생성
+            ladder += steps[Int(arc4random_uniform(2))]
+        }
+    }
+
+    stair.append(ladder)
+    return stair
+}
+
+// 메인 함수
+func main() {
+    print("참여할 사람은 몇 명 인가요?")
+    if let participant = readLine() {
+        if let numberOfpeople = Int(participant), numberOfpeople > 1 {
+
+            // 사다리를 만들고 출력하는 부분
+            print("최대 사다리 높이는 몇 개 인가요?")
+            if let staircase = readLine() {
+                if let numberOfStairs = Int(staircase), numberOfStairs > 1 {
+                    let ladders = makeLadderUsing(numberOfpeople, numberOfStairs)
+                    printOut(ladders)
+                } else {
+                    print("2이상의 숫자를 입력해주세요.")
+                }
+            }
+
+        } else {
+            print("2이상의 숫자를 입력해주세요.")
+        }
+    }
+}
+
+main()
+
