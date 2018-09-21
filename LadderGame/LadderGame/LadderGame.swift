142437641299d88f81857924efe3aa872e15a344
popsmile <popsmile7@gmail.com>
2018-09-21 16:44:57 +0900

    LadderGame의 사다리배열 생성부분 및 ResultView의 출력부분 작성

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -11,4 +11,35 @@ import Foundation
 struct LadderGame {
     var height : Int
     var playerNames : [LadderPlayer]
+    
+    // 하나의 행에서 연결된 step이 있는지 검사 및 삭제 후 [LadderStep] 리턴
+    func removeConnectionOf(_ ladderRow:[LadderStep]) -> [LadderStep] {
+        var ladderRow = ladderRow
+        for index in 1...ladderRow.count-1 {
+            if (ladderRow[index-1].step && ladderRow[index].step) {
+                ladderRow[index-Int.random(in: 0...1)].step = false
+            }
+        }
+        return ladderRow
+    }
+    
+    // 사다리의 열 하나를 랜덤으로 만들고, removeConnectionOf() 거친 후 [LadderStep] 리턴
+    func makeLadderRow(rowIndex:Int) -> [LadderStep] {
+        var ladderRow : [LadderStep] = Array(repeating: LadderStep(), count: rowIndex)
+        for index in ladderRow.indices {
+            ladderRow[index].step = Bool.random()
+        }
+        ladderRow = removeConnectionOf(ladderRow)
+        return ladderRow
+    }
+    
+    // makeLadderRow()으로 만든 열을 쌓아 사다리배열 [[LadderStep]] 리턴
+    func makeLadder() -> [[LadderStep]] {
+        var ladders : [[LadderStep]] = []
+        for _ in 1...height {
+            ladders.append(makeLadderRow(rowIndex: (playerNames.count-1)))
+        }
+        return ladders
+    }
+    
 }
--- LadderGame/LadderGame/ResultView.swift
+++ LadderGame/LadderGame/ResultView.swift
@@ -8,7 +8,58 @@
 
 import Foundation
 
-//사용자의 입력을 받는 구조체(InputView)와 결과를 출력하는 구조체(ResultView)를 분리해서 구현한다.
 struct ResultView {
-    var result : String
+    var ladder : [[LadderStep]]
+    
+    // 하나의 행을 출력 (true: "-----|" false: "     |")
+    func printSingle(_ column:LadderStep) {
+        if(column.step==true) {
+            Swift.print("-----", terminator: "|")
+            return
+        }
+        Swift.print("     ", terminator: "|")
+    }
+    
+    // 위 printSingle(column)으로 만든 행으로 하나의 열을 완성
+    func printSingle(_ row:[LadderStep]) {
+        for column in row {
+            printSingle(column)
+        }
+    }
+    
+    // 이름 출력형식 가운데정렬해서 리턴
+    func center(_ player:LadderPlayer) -> String {
+        switch player.name.count {
+        case 1:
+            return "   \(player.name)  "
+        case 2:
+            return "  \(player.name)  "
+        case 3:
+            return "  \(player.name) "
+        case 4:
+            return " \(player.name) "
+        case 5:
+            return "\(player.name) "
+        default:
+            return "     "
+        }
+    }
+    
+    // 하단에 참여자 이름 출력하기
+    func print(players:[LadderPlayer]){
+        for player in players {
+            Swift.print(center(player), terminator: "")
+        }
+    }
+    
+    // 위 printSingle(row)으로 만든 열로 사다리 완성
+    func print(ladder:[[LadderStep]], players:[LadderPlayer]) {
+        for row in ladder {
+            Swift.print("  ", terminator: "|")
+            printSingle(row)
+            Swift.print("")
+        }
+        print(players:players)
+    }
+    
 }
