ecfd337b5af7886f66ab56778ad5d82f6e58a153
Jun ho Lee <june20516@gmail.com>
2018-09-22 00:47:47 +0900

    입력 값의 종류에 따라 함수 분리 > 사다리 구조를 배열당 세로줄에서 배열당 가로층계로 변경

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,17 +7,23 @@
 //
 
 import Foundation
-
+//----------------------------------------------------------------------------
 //값을 입력받음
-func receiveInput() -> (columnString: String, rowString: String) {
+func receiveParticipants() -> String {
     print("참여할 사람은 몇 명입니까?")
-    let optionalColumnString = readLine()
-    print("사다리 높이는 몇 층인가요?")
-    let optionalRowString = readLine()
-    if let columnString = optionalColumnString, let rowString = optionalRowString {
-        return (columnString, rowString)
+    if let columnString = readLine() {
+    return columnString
     }
-    return ("nil", "nil")
+    return "nil"
+}
+
+// 사다리 높이 입력
+func receiveStairs() -> String {
+    print("참여할 사람은 몇 명입니까?")
+    if let rowString = readLine() {
+        return rowString
+    }
+    return "nil"
 }
 
 //값이 유효한지 검사. 유효하지 않으면 0을 반환
@@ -29,20 +35,17 @@ func checkInputValue(columnString: String, rowString: String) -> (columnNumber: 
 }
 
 //0을 반환 받았을 때 경고문 출력
-func preventWrongInput(checkedInput: (columnNumber: UInt,rowNumber: UInt)) -> Void {
+func preventWrongInput(checkedInput: (columnNumber: UInt,rowNumber: UInt)) -> String {
     if checkedInput.columnNumber == 0 || checkedInput.rowNumber == 0 {
-        print("입력값이 잘못되었습니다.(1이상의 정수만 가능)")
+        print("입력값이 잘못되었습니다.(2이상의 정수만 가능)")
+        return "Stop"
     }
+    return "OK"
 }
 
-//세로 줄 그리는 배열 생성
-func createVerticalLines(rowNumber: UInt) -> [String] {
-    let VerticalLine = Array(repeating: "|", count: Int(rowNumber))
-    return VerticalLine
-}
-
+//----------------------------------------------------------------------------
 //가로 줄 랜덤 결정
-func SetBarOrNil() -> String {
+func setLadderStairs() -> String {
     let bar = arc4random_uniform(2)
     if bar == 1 {
         return "—"
@@ -51,63 +54,66 @@ func SetBarOrNil() -> String {
     }
 }
 
-
 //가로 줄 그리는 배열 생성
-func createHorizontalLines(rowNumber: UInt) -> [String] {
+func createHorizontalLines(columnNumber: UInt) -> [String] {
     var horizontalLine: [String] = Array()
-    for _ in 0..<rowNumber {
-        horizontalLine.append(SetBarOrNil())
+    for _ in 0..<columnNumber {
+        horizontalLine.append(setLadderStairs())
     }
     return horizontalLine
 }
+//-----------------------------------------------------------------------------
+
 
 //전체 배열이 홀수,짝수인지 판단하여 부분 배열 반환
-func checkLinesOddEven(ladderIndex: UInt, columnNumber: UInt, rowNumber: UInt) -> [String] {
-    if ladderIndex % 2 == 1 {
-        return createVerticalLines(rowNumber: rowNumber)
-    } else {
-        return createHorizontalLines(rowNumber: rowNumber)
-    }
-}
+//func checkLinesOddEven(ladderIndex: UInt, columnNumber: UInt, rowNumber: UInt) -> [String] {
+//    if ladderIndex % 2 == 1 {
+//        return createVerticalLines(rowNumber: rowNumber)
+//    } else {
+//        return createHorizontalLines(rowNumber: rowNumber)
+//    }
+//}
 
 //전체 배열 조립
-func createWholeLadderKit(columnNumber: UInt, rowNumber: UInt) -> [[String]] {
+func createWholeLadderLines(columnNumber: UInt, rowNumber: UInt) -> [[String]] {
     var ladder: [[String]] = Array()
-    for i in 0..<columnNumber*2-1 {
-        let partialArray: [String] = checkLinesOddEven(ladderIndex: i+1, columnNumber: columnNumber, rowNumber: rowNumber)
-        ladder.append(partialArray)
+    for _ in 0..<columnNumber-1 {
+        let oneStair: [String] = createHorizontalLines(columnNumber: columnNumber)
+        ladder.append(oneStair)
     }
     return ladder
 }
 
 //이중 배열을 1열씩 부분문자열 생성
-func assembleALineOfLadder(indexOfInnerArray: Int, ladderStr: String, wholeLadderKit: [[String]]) -> String {
-    var ladder: String = ladderStr
-    for i in 0..<wholeLadderKit.count {
-        ladder += "\(wholeLadderKit[i][indexOfInnerArray])"
-    }
-    ladder = "\(ladder)\n"
-    return ladder
-}
+//func assembleALineOfLadder(indexOfInnerArray: Int, ladderStr: String, wholeLadderKit: [[String]]) -> String {
+//    var ladder: String = ladderStr
+//    for i in 0..<wholeLadderKit.count {
+//        ladder += "\(wholeLadderKit[i][indexOfInnerArray])"
+//    }
+//    ladder = "\(ladder)\n"
+//    return ladder
+//}
 
-//각 부분 문자열을 큰 문자열로 조립
-func assembleWholeLadder(wholeLadderKit: [[String]]) -> String {
-    var ladder: String = ""
-    for i in 0..<wholeLadderKit[0].count {
-        ladder = assembleALineOfLadder(indexOfInnerArray: i, ladderStr: ladder, wholeLadderKit: wholeLadderKit)
-    }
-    return ladder
-}
+////각 부분 문자열을 큰 문자열로 조립
+//func assembleWholeLadder(wholeLadderKit: [[String]]) -> String {
+//    var ladder: String = ""
+//    for i in 0..<wholeLadderKit[0].count {
+//        ladder = assembleALineOfLadder(indexOfInnerArray: i, ladderStr: ladder, wholeLadderKit: wholeLadderKit)
+//    }
+//    return ladder
+//}
+
+//-----------------------------------------------------------------------------
 
 //참가자 번호 생성
 func makeParticipant(columnNumber: Int) -> [String] {
     var participants: [String] = Array.init(repeating: " ", count: columnNumber*2-1)
-    var i = 0
-    var j = 1
-    while i < columnNumber*2-1 {
-        participants[i] = String(j)
-        j += 1
-        i += 2
+    var index = 0
+    var personNumber = 1
+    while index < columnNumber*2-1 {
+        participants[index] = String(personNumber)
+        personNumber += 1
+        index += 2
         
     }
     return participants
@@ -120,6 +126,8 @@ func makeLosingTicket(columnNumber: Int) -> [String] {
     gameResults[Int(losingPoint)] = "X"
     return gameResults
 }
+//-----------------------------------------------------------------------------
+
 
 //참가자, 완성된 사다리, 꽝을 출력
 func printGame(completedLadder: String, makeParticipant: [String], makeLosingTiket: [String]) ->Void {
@@ -128,6 +136,8 @@ func printGame(completedLadder: String, makeParticipant: [String], makeLosingTik
     print(makeLosingTiket.joined(separator: ""))
 }
 
+
+//-----------------------------------------------------------------------------
 
 //메인함수
 func main(inputValue: (columnString: String,rowString: String)) -> Void {
