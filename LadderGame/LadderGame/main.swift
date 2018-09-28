c7e1b552c7720e879904ca725fada2394aa77f55
popsmile <popsmile7@gmail.com>
2018-09-28 16:33:49 +0900

    상수로 선언한 guideMessage를 매개변수로 전달하도록 한다

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -10,8 +10,9 @@
 import Foundation
 
 func execute() {
-    let playerNames = InputView(guideMessage: "참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)").getInput()
-    let maxHeight = InputView(guideMessage: "최대 사다리 높이는 몇 개인가요?").getInput()
+    let guideMessage = ["참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)","최대 사다리 높이는 몇 개인가요?"]
+    let playerNames = InputView(guideMessage: guideMessage[0]).getInput()
+    let maxHeight = InputView(guideMessage: guideMessage[1]).getInput()
     
     do { try Validator().checkValidate(height: maxHeight, names: playerNames) }
     catch InputError.outOfNameLength { return }
