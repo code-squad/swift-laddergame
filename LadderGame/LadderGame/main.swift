ab4feae8e6e083fa929cbbbcdc0ad14a26cc753f
popsmile <popsmile7@gmail.com>
2018-09-28 14:54:50 +0900

    main에서 InputView, LadderGame, ResultView 구조체를 사용하여 사다리게임을 실행하도록 수정한다

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -9,14 +9,17 @@
 
 import Foundation
 
-do {
-    var inputView = InputView()
-    try inputView.execute()
-    let ladder = inputView.ladderGame.makeLadder()
-    let resultView = ResultView(ladder:ladder)
-    resultView.print(ladder: ladder, players: inputView.ladderGame.playerNames)
-} catch InputError.outOfNameLength {
-    print("참여자 이름은 다섯글자 이하로 입력해주세요.")
-} catch InputError.invalidType {
-    print("숫자를 입력해주세요.")
+func execute() {
+    let playerNames = InputView(guideMessage: "참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)").getInput()
+    let maxHeight = InputView(guideMessage: "최대 사다리 높이는 몇 개인가요?").getInput()
+    
+    do { try Validator().checkValidate(height: maxHeight, names: playerNames) }
+    catch InputError.outOfNameLength { return }
+    catch InputError.notIntType { return }
+    catch { return }
+    
+    let ladderGame = LadderGame(height: Int(maxHeight)!, names: playerNames)
+    ResultView(ladder:ladderGame.makeLadder(), players:ladderGame.players).printGame()
 }
+
+execute()
