6aa80455e3138d2e08900e6069232097d34fe5c4
popsmile <popsmile7@gmail.com>
2018-09-28 12:13:27 +0900

    InputView를 간략화한다
    변수로 가지고있던 LadderGame 구조체를 InputView와 대등하게 상위모듈로 올리기위하여, InputView내에서 안내메세지를 출력하고 입력값을 받아 문자열로 리턴하는 기능만 남겨놓는다.
    구조체 내부 변수 guideMessage는 private으로 수정한다.

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -8,57 +8,16 @@
 
 import Foundation
 
-public enum InputError: Error {
-    case invalidType
-    case outOfNameLength
-}
-
 struct InputView {
-    var guideMessage : [String]
-    var ladderGame : LadderGame
+    private var guideMessage : String
     
-    init() {
-        guideMessage = ["참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)", "최대 사다리 높이는 몇 개인가요?"]
-        ladderGame = LadderGame(height:0, playerNames:[])
+    init(guideMessage:String) {
+        self.guideMessage = guideMessage
     }
     
-    // guide 메세지를 프린트 + 입력값을 받아와서 문자열로 리턴하는 함수
-    func getInput(_ guideMessage:String) -> String {
-        print(guideMessage)
+    func getInput() -> String {
+        print(self.guideMessage)
         guard let input:String = readLine() else { return String() }
         return input
     }
-    
-    // 문자열을 ,로 분리해서 문자열배열로 리턴하는 함수
-    func separate(inputNames:String) -> [String] {
-        return inputNames.split(separator: ",").map({String($0)})
-    }
-    
-    // 입력한 이름들에 대해 최대허용글자수 체크하여 Bool 리턴하는 함수
-    func isOutOf(allowedLength:Int, names:[String]) -> Bool {
-        for name in names {
-            if(name.count > allowedLength) { return true }
-        }
-        return false
-    }
-    
-    // 위 문자열과 문자열배열로 ladderGame 값을 변경해주는 함수
-    mutating func writeInfoOf(_ maxHeight:Int, _ playerNames:[String]) {
-        ladderGame.height = maxHeight
-        for name in playerNames {
-            ladderGame.playerNames.append(LadderPlayer(name:name))
-        }
-    }
-    
-    mutating func execute() throws {
-        var input = getInput(guideMessage[0])
-        let playerNames:[String] = separate(inputNames: input)
-        if isOutOf(allowedLength: 5, names: playerNames) { throw InputError.outOfNameLength }
-        
-        input = getInput(guideMessage[1])
-        guard let maxHeight:Int = Int(input) else { throw InputError.invalidType }
-        
-        writeInfoOf(maxHeight, playerNames)
-    }
-
 }
