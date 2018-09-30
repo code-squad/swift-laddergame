44d53c102b6261e5bae28db7d2784ffa4e978e63
김장수 <jangsookim.me@gmail.com>
2018-09-30 17:17:12 +0900

    다리 구조를 String에서 Bool타입으로 추상화

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -47,18 +47,18 @@ func getElements() -> (Int, Int) {
 
 
 // 중복을 바꿔주는 함수
-func changeLegToEmpty(before: String, after: String) -> String {
+func changeLegToEmpty(before: Bool, after: Bool) -> Bool {
     var result = after
     
-    if before == "-" && after == "-" {
-        result = " "
+    if before == true && after == true {
+        result = false
     }
 
     return result
 }
 
 // 사다리가 옆으로 연속해서 나오는 걸 방지하는 함수
-func preventOverlap(legs: [String]) -> [String] {
+func preventOverlap(legs: [Bool]) -> [Bool] {
     var refined = legs
     
     for i in 0..<(refined.count-1) {
@@ -70,7 +70,7 @@ func preventOverlap(legs: [String]) -> [String] {
 
 // 사다리가 옆으로 연속해서 나오지 검증하는 함수
 // 연속해서 나올 가능성이 있을/없을 경우로 분리
-func checkSuccession(line: [String]) -> [String] {
+func checkSuccession(line: [Bool]) -> [Bool] {
     if line.count > 1 {
         let preventedLegs = preventOverlap(legs: line)
         return preventedLegs
@@ -80,9 +80,9 @@ func checkSuccession(line: [String]) -> [String] {
 }
 
 // 한 계단을 만드는 함수
-func makeOneStepAlong(number: Int) -> [String] {
-    let legs = ["-", " "]
-    var step = [String]()
+func makeOneStepAlong(number: Int) -> [Bool] {
+    let legs = [true, false]
+    var step = [Bool]()
     
     for _ in 1..<number {
         step.append(legs[Int(arc4random_uniform(2))])
@@ -93,9 +93,9 @@ func makeOneStepAlong(number: Int) -> [String] {
 }
 
 // 사다리를 만드는 함수
-func buildLadderWith(elements: (Int, Int)) -> [[String]] {
+func buildLadderWith(elements: (Int, Int)) -> [[Bool]] {
     let people = elements.0, height = elements.1
-    var steps = Array(repeating: [String](), count: height)
+    var steps = Array(repeating: [Bool](), count: height)
     
     for i in 0..<height {
         steps[i] = makeOneStepAlong(number: people)
@@ -106,16 +106,31 @@ func buildLadderWith(elements: (Int, Int)) -> [[String]] {
 
 
 
+// 추상화된 사다리 정보를 출력을 위해 변환
+func legToVisible(imaginary: Bool) -> String {
+    if imaginary == true {
+        return "-"
+    }
+    
+    return " "
+}
+
 // 사다리 중 한 계단을 출력하는 함수
-func printOneStepUsing(_ leg: String) {
-    print("|\(leg)|")
+func printOneStepUsing(_ leg: [Bool]) {
+    var visibleLeg = String()
+    let edge = "|"
+    
+    for i in leg {
+        visibleLeg += legToVisible(imaginary: i) + edge
+    }
+    
+    print("\(edge)\(visibleLeg)")
 }
 
 // 사다리를 출력하는 함수
-func printOut(_ ladder: [[String]]) {
+func printOut(_ ladder: [[Bool]]) {
     for step in ladder {
-        let leg = step.joined(separator: "|")
-        printOneStepUsing(leg)
+        printOneStepUsing(step)
     }
 }
 
