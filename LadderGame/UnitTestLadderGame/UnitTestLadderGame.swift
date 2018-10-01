97df016d05e7ab79894c1fde59fafb7bf347e32b
popsmile <popsmile7@gmail.com>
2018-10-01 14:45:33 +0900

    단위테스트를 위해 UnitTestLadderGame을 생성한다

---
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -0,0 +1,29 @@
+//
+//  UnitTestLadderGame.swift
+//  UnitTestLadderGame
+//
+//  Created by 윤지영 on 01/10/2018.
+//  Copyright © 2018 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+@testable import LadderGame
+
+class UnitTestLadderGame: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
