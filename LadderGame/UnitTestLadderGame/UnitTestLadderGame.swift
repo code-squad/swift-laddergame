66c5493d0343c01331e22f6edc163c39b83ed2bb
popsmile <popsmile7@gmail.com>
2018-10-01 14:51:58 +0900

    Validator를 테스트하는 메소드를 추가한다
    checkValidate()메소드가 입력값에 따라 적절할 에러를 던지는지 테스트한다

--- LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -18,6 +18,23 @@ class UnitTestLadderGame: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func test_ValidatorShouldThrowError_WhenInputIsNotAllowed() {
+        let namesAllowed = "cat,dog"
+        let namesNotAllowed = "rabbit,dog"
+        let heightAllowed = "1"
+        let heightNotAllowed = "a"
+        // 두 입력값 모두 허용될 때 -> NoThrow
+        XCTAssertNoThrow(try Validator().checkValidate(height: heightAllowed, names: namesAllowed))
+        // names 입력값 중 5글자를 넘는 이름이 있을 때 -> InputError.outOfNameLength
+        XCTAssertThrowsError(try Validator().checkValidate(height: heightAllowed, names: namesNotAllowed)) {
+            (error) -> Void in XCTAssertEqual(error as? InputError, InputError.outOfNameLength)
+        }
+        // height 입력값이 정수형이 아닐 때 -> InputError.notIntType
+        XCTAssertThrowsError(try Validator().checkValidate(height: heightNotAllowed, names: namesAllowed)) {
+            (error) -> Void in XCTAssertEqual(error as? InputError, InputError.notIntType)
+        }
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
