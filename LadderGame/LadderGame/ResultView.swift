c458f89e85913f56770a878f549580e8b0833547
popsmile <popsmile7@gmail.com>
2018-10-01 18:04:26 +0900

    ResultView 메소드 모두 static으로 수정한다
    main에서 ResultView 인스턴스가 불필요하여 printGame() 및 하위 메소드 모두 타입 메소드로 수정한다

--- LadderGame/LadderGame/ResultView.swift
+++ LadderGame/LadderGame/ResultView.swift
@@ -12,7 +12,7 @@ struct ResultView {
     
     // -------- 사다리 출력 --------
     // 하나의 행을 출력 (true: "-----|" false: "     |")
-    private func printSingle(_ column:LadderStep) {
+    static private func printSingle(_ column:LadderStep) {
         if(column.step==true) {
             Swift.print("-----", terminator: "|")
             return
@@ -20,13 +20,13 @@ struct ResultView {
         Swift.print("     ", terminator: "|")
     }
     // 위 printSingle(column)으로 만든 행으로 하나의 열을 완성
-    private func printSingle(_ row:[LadderStep]) {
+    static private func printSingle(_ row:[LadderStep]) {
         for column in row {
             printSingle(column)
         }
     }
     // 위 printSingle(row)으로 만든 열로 사다리 완성
-    private func printLadder(_ ladder:[[LadderStep]]) {
+    static private func printLadder(_ ladder:[[LadderStep]]) {
         for row in ladder {
             print("  ", terminator: "|")
             printSingle(row)
@@ -36,13 +36,13 @@ struct ResultView {
     
     // -------- 이름 출력 --------
     // 이름 뒤 공백추가해 5글자로 만들어주기
-    private func printWhiteSpace(nameLength:Int) {
+    static private func printWhiteSpace(nameLength:Int) {
         for _ in 0...(5-nameLength) {
             print(" ", terminator: "")
         }
     }
     // 하단에 참여자 이름 출력하기
-    private func printPlayers(_ names:[String]){
+    static private func printPlayers(_ names:[String]){
         for name in names {
             print(name, terminator: "")
             printWhiteSpace(nameLength: name.count)
@@ -50,7 +50,7 @@ struct ResultView {
     }
     
     // 최종 게임 출력
-    func printGame(ladder:[[LadderStep]], names:[String]) {
+    static func printGame(ladder:[[LadderStep]], names:[String]) {
         printLadder(ladder)
         printPlayers(names)
     }
--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -20,7 +20,7 @@ func execute() {
     catch { return }
     
     let ladderGame = LadderGame(height: Int(maxHeight)!, names: playerNames)
-    ResultView().printGame(ladder:ladderGame.makeLadder(), names:ladderGame.getPlayerNames())
+    ResultView.printGame(ladder:ladderGame.makeLadder(), names:ladderGame.getPlayerNames())
 }
 
 execute()
