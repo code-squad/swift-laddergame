319d27c2679cb5019be89a7af8bfb7552d87471a
hngfu <djsl1018@naver.com>
2018-10-01 13:39:54 +0900

    [UnitTestValidCheck,UnitTestLadderStep] 유닛테스트 파일 분리
    파일별로 1대1 대응이 되도록 유닛테스트 파일을 추가하여 분리하였음

--- LadderGame/UnitTestLadderGame/UnitTestLadderStep.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderStep.swift
@@ -1,18 +1,16 @@
 //
-//  UnitTestLadderGame.swift
+//  UnitTestLadderStep.swift
 //  UnitTestLadderGame
 //
-//  Created by 조재흥 on 2018. 9. 29..
+//  Created by 조재흥 on 2018. 10. 1..
 //  Copyright © 2018년 Codesquad Inc. All rights reserved.
 //
 
 import XCTest
 @testable import LadderGame
 
-class UnitTestLadderGame: XCTestCase {
-    
-    var ladderGame = LadderGame(height: 3, names: "<#T##String#>")
-    
+class UnitTestLadderStep: XCTestCase {
+
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -20,17 +18,13 @@ class UnitTestLadderGame: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-//
-//    func testMakeLadder() {
-//        let ladderGame = LadderGame(height: 3, names: "gd,HNGFU,zico")
-//        let ladderGameDTO = ladderGame.translateToDTO()
-//        ladderGame
-//    }
-//
-//    func testTranslateDTO() {
-//        let
-//    }
-    
+
+    func testBringStepInfo() {
+        let broughtSteoInfo = LadderStep().bringStepInfo()
+        let isCorrectInfo = (broughtSteoInfo == [true : "-----", false : "     "])
+        XCTAssertTrue(isCorrectInfo)
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
---
+++ LadderGame/UnitTestLadderGame/UnitTestValidCheck.swift
@@ -0,0 +1,49 @@
+//
+//  UnitTestValidCheck.swift
+//  UnitTestLadderGame
+//
+//  Created by 조재흥 on 2018. 10. 1..
+//  Copyright © 2018년 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+@testable import LadderGame
+
+class UnitTestValidCheck: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testIsValidNamesFromOneToFive() {
+        let oneToFive = "a,bb,ccc,dddd,eeeee"
+        XCTAssertTrue(ValidityCheck().isValid(names: oneToFive))
+    }
+    
+    func testIsValidNamesFromSixToSeven() {
+        let sixToSeven = "ffffff,ggggggg"
+        XCTAssertFalse(ValidityCheck().isValid(names: sixToSeven))
+    }
+    
+    func testIsValidHeightOfTwo() {
+        let two = 2
+        XCTAssertTrue(ValidityCheck().isValid(height: two))
+    }
+    
+    func testIsValidHeightOfOne() {
+        let one = 1
+        XCTAssertFalse(ValidityCheck().isValid(height: one))
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
