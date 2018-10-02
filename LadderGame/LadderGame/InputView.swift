6719abca7cebc07078386900047b8c8906ccfb74
popsmile <popsmile7@gmail.com>
2018-10-02 17:09:33 +0900

    guidMessage enum을 생성한다

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -8,15 +8,20 @@
 
 import Foundation
 
+enum guideMessage : String {
+    case playerNames = "참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"
+    case maxHeight = "최대 사다리 높이는 몇 개인가요?"
+}
+
 struct InputView {
-    private var guideMessage : String
+    private var guideMessage : guideMessage
     
-    init(guideMessage:String) {
+    init(guideMessage:guideMessage) {
         self.guideMessage = guideMessage
     }
     
     func getInput() -> String {
-        print(self.guideMessage)
+        print(self.guideMessage.rawValue)
         guard let input:String = readLine() else { return String() }
         return input
     }
--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -10,10 +10,9 @@
 import Foundation
 
 func execute() {
-    let guideMessage = ["참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)","최대 사다리 높이는 몇 개인가요?"]
     do {
-        let playerNames = InputView(guideMessage: guideMessage[0]).getInput()
-        let maxHeight = InputView(guideMessage: guideMessage[1]).getInput()
+        let playerNames = InputView(guideMessage: .playerNames).getInput()
+        let maxHeight = InputView(guideMessage: .maxHeight).getInput()
         try Validator.throwInputError(names:playerNames, height:maxHeight)
         let ladderGame = LadderGame(height: maxHeight, names: playerNames)
         ResultView.printGame(ladderGame.getLadderGameDTO())
