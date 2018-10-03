9ffdc1c28f3ef4bd09caf39661a897910667348a
Jun ho Lee <june20516@gmail.com>
2018-10-03 17:32:17 +0900

    LadderStep 중복 요소 방지 구현
    초기 Step을 만들어두고,
    이전 step을 검사하여 겹치지않는 step을 만드는 checkPriorStep 함수 생성

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+struct LadderGame {
+    // 입력값을 가져옴
+    let inputValue = InputView()
+    
+    // LadderStep들로 된 배열을 반환
+    private func makeOneLine() -> [LadderStep] {
+        //배열을 첫요소와 함께 선언
+        var oneLine = [LadderStep()]
+        //중복 안되게 생성하여 배열에 추가
+        for stepIndex in 0..<inputValue.players.count-2 {
+            oneLine.append(checkPriorStep(lastStep: oneLine[stepIndex]))
+        }
+        return oneLine
+    }
+    
+    // LadderStep의 이전 요소와 중복되지 않게 검사
+    func checkPriorStep(lastStep: LadderStep) -> LadderStep {
+        if lastStep.dataStep {
+            var nextStep = LadderStep()
+            nextStep.dataStep = false
+            return nextStep
+        }
+        let nextStep = LadderStep()
+        return nextStep
+    }
+    
+    // 배열을 모아 이중 배열을 반환
+    func completeWholeLadder() -> [[LadderStep]] {
+        var wholeLadder = Array<[LadderStep]>()
+        for _ in 0..<inputValue.height {
+            wholeLadder.append(makeOneLine())
+        }
+        return wholeLadder
+    }
+
+    
+}
