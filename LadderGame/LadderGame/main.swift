3e7839d54c2fae0faa5ecace2ef3d204de6990c0
kcushy <dev.kcushy@gmail.com>
2018-10-08 15:15:33 +0900

    feat: Add a function to check input value

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,36 @@
 
 import Foundation
 
+enum inputError: Error {
+    case wrongType
+    case unknown
+}
+
+func check(type: String) throws -> Int {
+    guard let result = Int(type) else {
+        throw inputError.wrongType
+    }
+    return result
+}
+
+func countPeople() throws -> Int {
+    print("참여할 사람은 몇명인가요?(ex: 3): " , terminator: " ")
+    guard let participate = readLine() else {
+        throw inputError.unknown
+    }
+    return try check(type: participate)
+}
+
+func countLadder() throws -> Int {
+    print("최대 사다리의 높이는 몇개인가요?(ex: 5): " , terminator: " ")
+    guard let countLadder = readLine() else {
+        throw inputError.unknown
+    }
+    return try check(type: countLadder)
+}
+
+func main() {
+    
+}
+
+main()
