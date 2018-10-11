c7ac310c990828897bb5312b7e6b3a4d32610140
Jun ho Lee <june20516@gmail.com>
2018-10-11 18:34:08 +0900

    LadderStep 중복 체크 개선
    LadderStep.stepDecision 초기 랜덤값 가지도록 변경
    LadderStep 내부에 decideStep method 생성하여 LadderGame에서 중복 확인 로직 이동
    LadderGame의 property 접근범위 수정

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -10,14 +10,18 @@ import Foundation
 
 struct LadderGame {
     // 입력값을 가져옴
-    let players: [LadderPlayer]
-    let height: Int
+    private let players: [LadderPlayer]
+    private let height: Int
+    
+    init(players: [LadderPlayer], height: Int) {
+        self.players = players
+        self.height = height
+    }
     
     // LadderStep들로 된 배열을 반환
     private func makeOneLine() -> [LadderStep] {
         //배열 선언 후 첫요소
         var oneLine = Array<LadderStep>(repeating: LadderStep(), count: players.count-1)
-        oneLine[0].makeRandomStep()
         //중복 안되게 생성하여 배열에 추가
         for stepIndex in 0..<players.count-2 {
             let oneStep = checkPriorStep(lastStep: oneLine[stepIndex])
@@ -28,14 +32,8 @@ struct LadderGame {
     
     // LadderStep의 이전 요소와 중복되지 않게 검사
     private func checkPriorStep(lastStep: LadderStep) -> LadderStep {
-        if lastStep.stepDecision {
-            var nextStep = LadderStep()
-            nextStep.makeEmptyStep()
-            return nextStep
-        }
-        var nextStep = LadderStep()
-        nextStep.makeRandomStep()
-        return nextStep
+        let nextStep = LadderStep()
+        return nextStep.decideStep(pastStep: lastStep)
     }
     
     // 배열을 모아 이중 배열을 반환
@@ -46,6 +44,4 @@ struct LadderGame {
         }
         return wholeLadder
     }
-
-    
 }
--- LadderGame/LadderGame/LadderStep.swift
+++ LadderGame/LadderGame/LadderStep.swift
@@ -11,16 +11,22 @@ import Foundation
 //사다리 한개
 struct LadderStep {
     
-    private(set) var stepDecision = true
-    
-    //사다리 랜덤 결정
-    mutating func makeRandomStep() {
-        self.stepDecision = arc4random_uniform(UInt32(2)) == 1
-    }
+    //랜덤으로 사다리 요소 생성
+    private(set) var stepDecision = arc4random_uniform(UInt32(2)) == 1
     
     //사다리 공백으로 변경
-    mutating func makeEmptyStep() {
+    private mutating func makeEmptyStep() {
         self.stepDecision = false
     }
-
+    
+    //중복 검사 결과에 따라 사다리 요소 결정
+    func decideStep(pastStep: LadderStep) -> LadderStep {
+        var nextStep = LadderStep()
+        if pastStep.stepDecision {
+            nextStep.makeEmptyStep()
+            return nextStep
+        }
+        return nextStep
+    }
+    
 }
