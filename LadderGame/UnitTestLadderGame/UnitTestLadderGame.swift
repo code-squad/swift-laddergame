8422ef93fd094658a2749fa03a4ae0c1c19d5a7e
김장수 <jangsookim.me@gmail.com>
2018-10-16 12:00:05 +0900

    테스트 실패 경우 추가

--- LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -24,22 +24,40 @@ class UnitTestLadderGame: XCTestCase {
     func testLadderGameDTOHeight() {
         // 높이가 올바르게 입력되었는지 확인
         let ladderGame = LadderGame(height: 5, names: "JK,Crong,Honux,Pobi")
-        let ladderGameDTO = ladderGame.delieverDTO()
-        XCTAssertEqual(ladderGameDTO.ladder.count, 5)
+        let ladderGameDTO = ladderGame?.delieverDTO()
+        XCTAssertEqual(ladderGameDTO?.ladder.count, 5)
     }
     
     func testLadderGameDTOPlayers() {
         // 참가자 명단이 올바르게 전달되는지 확인
         let ladderGame = LadderGame(height: 5, names: "JK,Crong,Honux,Pobi")
-        let ladderGameDTO = ladderGame.delieverDTO()
-        XCTAssertEqual(ladderGameDTO.players.count, 4)
+        let ladderGameDTO = ladderGame?.delieverDTO()
+        XCTAssertEqual(ladderGameDTO?.players.count, 4)
     }
     
     func testLadderGameDTOLadder() {
         // 사다리 데이터 구조가 올바르게 전달되는지 확인
         let ladderGame = LadderGame(height: 5, names: "JK,Crong,Honux,Pobi")
-        let ladderGameDTO = ladderGame.delieverDTO()
-        XCTAssertTrue(checkStep(ladder: ladderGameDTO.ladder))
+        let ladderGameDTO = ladderGame?.delieverDTO()
+        XCTAssertTrue(checkStep(ladder: (ladderGameDTO?.ladder)!))
+    }
+    
+    func testLadderGameInitailzedFailHeightOne() {
+        // 사다리 높이가 2 미만인 경우 초기화 실패
+        let ladderGame = LadderGame(height: 2, names: "JK,Crong,Honux,Pobi")
+        XCTAssertNil(ladderGame)
+    }
+    
+    func testLadderGameInitailzedFailHeightTwo() {
+        // 사다리 높이가 20 초과인 경우 초기화 실패
+        let ladderGame = LadderGame(height: 21, names: "JK,Crong,Honux,Pobi")
+        XCTAssertNil(ladderGame)
+    }
+    
+    func testLadderGameInitailzedFailNamesCase() {
+        // 3명 미만, 20명 초과인 경우 초기화 실패
+        let ladderGame = LadderGame(height: 5, names: "JK,Crong")
+        XCTAssertNil(ladderGame)
     }
     
     
