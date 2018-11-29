313707cccff448c5e1e929be8206a3974ca84eab
kcushy <dev.kcushy@gmail.com>
2018-11-29 12:15:43 +0900

    refactor: Arrange a function

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -22,91 +22,6 @@ enum inputError: Error {
     }
 }
 
-// receive input how many ladders are.
-func receiveLadder() -> String {
-    print("최대 사다리의 높이는 몇개인가요? ex) 5:\n>>> ", terminator: "")
-    let input = readLine() ?? " "
-    return input
-}
-
-// receive input how many people are.
-func receivePeople() -> String {
-    print("참여하는 사람은 몇명인가요? ex) 3:\n>>> ", terminator: "")
-    let input = readLine() ?? " "
-    return input
-}
-
-func meetMinimum() throws -> (people: Int, ladder: Int) {
-    let ladder = receiveLadder()
-    let people = receivePeople()
-    guard let height = Int(ladder),
-        let participant = Int(people) else {
-            throw inputError.wrongValue
-    }
-    guard participant >= 1 else {
-        throw inputError.lackPeople
-    }
-    guard height >= 1 else {
-        throw inputError.lackLadder
-    }
-    return (participant, height)
-}
-
-// inspect a except case
-func inspectExcept(_ ladders: [String], _ index: Int) -> String {
-    guard index > 0 && ladders[index - 1] == "-" else {
-        return makeHorizon()
-    }
-    return " "
-}
-
-// make a horizon line
-func makeHorizon() -> String {
-    guard arc4random_uniform(2) == 1 else {
-        return " "
-    }
-    return "-"
-}
-
-// create one line ladder
-func makeLadderPart(from people: Int) -> [String] {
-    var ladders = [String](repeating: " ", count: people - 1)
-    guard !ladders.isEmpty else {
-        return []
-    }
-    for index in 0..<people - 1 {
-        ladders[index] = inspectExcept(ladders, index)
-    }
-    return ladders
-}
-
-
-// create completed ladder
-func makeFullLadder(_ people: Int, _ maxLadder: Int) -> [[String]] {
-    var ladders = [[String]]()
-    while ladders.count < maxLadder {
-        let part = makeLadderPart(from: people)
-        ladders.append(part)
-    }
-    return ladders
-}
-
-// print a line of ladder
-func printPart(of ladders: [String]) {
-    print("|", terminator: "")
-    for part in ladders {
-        print(part, terminator: "|")
-    }
-}
-
-// print full ladder
-func printFull(_ ladders: [[String]]) {
-    for ladder in ladders {
-        printPart(of: ladder)
-        print()
-    }
-}
-
 func main() {
     while true {
         do {
@@ -128,7 +43,92 @@ func main() {
             print("Error: \(error.localizedDescription)")
         }
     }
-    printFull(ladders)
+}
+
+// test minimum value
+func meetMinimum() throws -> (people: Int, ladder: Int) {
+    let ladder = receiveLadder()
+    let people = receivePeople()
+    guard let height = Int(ladder),
+        let participant = Int(people) else {
+            throw inputError.wrongValue
+    }
+    guard participant >= 1 else {
+        throw inputError.lackPeople
+    }
+    guard height >= 1 else {
+        throw inputError.lackLadder
+    }
+    return (participant, height)
+}
+
+// receive input how many ladders are.
+func receiveLadder() -> String {
+    print("최대 사다리의 높이는 몇개인가요? ex) 5:\n>>> ", terminator: "")
+    let input = readLine() ?? " "
+    return input
+}
+
+// receive input how many people are.
+func receivePeople() -> String {
+    print("참여하는 사람은 몇명인가요? ex) 3:\n>>> ", terminator: "")
+    let input = readLine() ?? " "
+    return input
+}
+
+
+// create completed ladder
+func makeFullLadder(_ people: Int, _ maxLadder: Int) -> [[String]] {
+    var ladders = [[String]]()
+    while ladders.count < maxLadder {
+        let part = makeLadderPart(from: people)
+        ladders.append(part)
+    }
+    return ladders
+}
+
+// create one line ladder
+func makeLadderPart(from people: Int) -> [String] {
+    var ladders = [String](repeating: " ", count: people - 1)
+    guard !ladders.isEmpty else {
+        return []
+    }
+    for index in 0..<people - 1 {
+        ladders[index] = inspectExcept(ladders, index)
+    }
+    return ladders
+}
+
+// inspect a except case
+func inspectExcept(_ ladders: [String], _ index: Int) -> String {
+    guard index > 0 && ladders[index - 1] == "-" else {
+        return makeHorizon()
+    }
+    return " "
+}
+
+// make a horizon line
+func makeHorizon() -> String {
+    guard arc4random_uniform(2) == 1 else {
+        return " "
+    }
+    return "-"
+}
+
+// print full ladder
+func printFull(_ ladders: [[String]]) {
+    for ladder in ladders {
+        printPart(of: ladder)
+        print()
+    }
+}
+
+// print a line of ladder
+func printPart(of ladders: [String]) {
+    print("|", terminator: "")
+    for part in ladders {
+        print(part, terminator: "|")
+    }
 }
 
 main()
