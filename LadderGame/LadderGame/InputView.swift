1bc4669dd4df2ca06339113a00b174fb8c03c466
kcushy <dev.kcushy@gmail.com>
2018-12-27 17:27:10 +0900

    fix: Modify a receivePeople() message

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -21,7 +21,7 @@ struct InputView {
     }
     // receive input how many people are.
     static func receivePeople() -> String {
-        let input = deliver("참여하는 사람은 몇명인가요? ex) 3:\n>>> ")
+        let input = deliver("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요) ")
         return input
     }
 }
