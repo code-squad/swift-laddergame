12651f6b9e06bdc3bb4a36763513b344f61bfc03
kcushy <dev.kcushy@gmail.com>
2018-12-27 17:19:36 +0900

    fix: Create a deliver(message:)

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct InputView {
+    // receive input how many ladders are.
+    private static func deliver(_ message: String) -> String {
+        print(message)
+        return readLine() ?? ""
+    }
+    static func receiveLadder() -> String {
+        print("최대 사다리의 높이는 몇개인가요? ex) 5:\n>>> ", terminator: "")
+        let input = readLine() ?? " "
+        return input
+    }
+    //receive input how many people are.
+    static func receivePeople() -> String {
+        print("참여하는 사람은 몇명인가요? ex) 3:\n>>>", terminator: "")
+        let input = readLine() ?? " "
+        return input
+    }
+}
