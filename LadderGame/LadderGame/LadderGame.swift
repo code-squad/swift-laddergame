98aca977eeb65c824ba685869ad0b0a55f224ba3
kcushy <dev.kcushy@gmail.com>
2019-01-15 15:48:36 +0900

    fix: Change a makeHorizon()'s return value, string -> Bool

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+struct LadderGame {
+    private var height = 0
+    private var names : [LadderPlayer]
+    
+    static func makeFullLadder(_ people: Int, _ maxLadder: Int) -> [[String]] {
+        var ladders = [[String]]()
+        while ladders.count < maxLadder {
+            let part = makeLadderPart(from: people)
+            ladders.append(part)
+        }
+        return ladders
+    }
+    
+    // create one line ladder
+    static func makeLadderPart(from people: Int) -> [String] {
+        var ladders = [String](repeating: " ", count: people - 1)
+        guard !ladders.isEmpty else {
+            return []
+        }
+        for index in 0..<people - 1 {
+            ladders[index] = inspectExcept(ladders, index)
+        }
+        return ladders
+    }
+    
+    // make a horizon line
+    func makeHorizon() -> Bool {
+        guard arc4random_uniform(2) == 1 else {
+            return false
+        }
+        return true
+    }
+
+    // split name into array
+    static func getNames(_ input: String) -> [String] {
+        return input.split(separator: ",").map{ String($0) }
+    }
+    
+
+
+}
