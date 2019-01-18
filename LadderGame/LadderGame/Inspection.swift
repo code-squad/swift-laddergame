4e52abd537366b56b69256091e63aa839366d5da
kcushy <dev.kcushy@gmail.com>
2019-01-18 11:49:50 +0900

    fix: Add paratmeters in meetMinimum()

--- LadderGame/LadderGame/Inspection.swift
+++ LadderGame/LadderGame/Inspection.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+// set error case
+enum inputError: Error {
+    case lackValue
+    case wrongValue
+    var description: String {
+        switch self {
+        case .lackValue: return "1 이상의 숫자를 입력해주세요"
+        case .wrongValue: return "잘못된 값입니다."
+        }
+    }
+}
+
+struct Inspection {
+    // inspect a except case
+
+    static func inspectExcept(_ ladders: [Bool], _ index: Int) -> Bool {
+        guard index > 0 && ladders[index - 1] == true else {
+            return LadderGame.makeRandom()
+        }
+        return false
+    }
+
+    static func meetMinimum(_ people: String, _ ladder: String) throws -> (people: Int, ladder: Int) {
+        // confirm a Int type
+        guard let participant = Int(people),
+            let height = Int(ladder) else {
+                throw inputError.wrongValue
+        }
+        guard participant >= 1 && height >= 1 else {
+                throw inputError.lackValue
+        }
+        return (participant, height)
+    }
+}
