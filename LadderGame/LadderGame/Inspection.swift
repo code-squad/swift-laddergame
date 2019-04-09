e00215c391fb3664df0d8a047e0f29a4434cb60e
kcushy <dev.kcushy@gmail.com>
2019-04-09 12:33:12 +0900

    fix: Change meetMinimum(player:ladder:) properties

--- LadderGame/LadderGame/Inspection.swift
+++ LadderGame/LadderGame/Inspection.swift
@@ -43,16 +43,46 @@ struct Inspection {
         }
         return ladders[index].step
     }
-
-    static func meetMinimum(_ people: String, _ ladder: String) throws -> (people: Int, ladder: Int) {
-        // confirm a Int type
-        guard let participant = Int(people),
-            let height = Int(ladder) else {
-                throw inputError.wrongValue
+    
+    /**
+     check whether or not the minimum length value is met. If not meet, throw error.
+     
+     - parameters:
+         - names: names of player
+     
+             *e.g) ["hngfu", "justin", "NORI"]*
+     
+    */
+    static func meetLength(of names: [String]) throws {
+        let isExceed = names.allSatisfy({ $0.trimmingCharacters(in: .whitespacesAndNewlines).count <= 5 })
+        guard isExceed else {
+            throw inputError.exceedLength
         }
-        guard participant >= 1 && height >= 1 else {
-                throw inputError.lackLadder
+    }
+    /**
+     If input doesn't meet minimum, throw error. otherwise, return incoming value.
+     
+     - parameters:
+         - player: String of names
+     
+             *e.g.) justin, oingbong, drake, cony*
+     
+         - ladder: A string of number of ladder
+     
+     - throws:
+     If incoming ladder isn't number, numbers of name and ladder is below 1, it throw exception.
+     
+     - returns:
+     A passed incoming value.
+    */
+    static func meetMinimum(_ player: String, _ ladder: String) throws -> (names: [String], ladder: Int) {
+        let names = LadderGame.extract(player)
+        guard let height = Int(ladder) else {
+            throw inputError.wrongValue
         }
-        return (participant, height)
+        guard [player].count >= 1 && height >= 1 else {
+            throw inputError.lackValue
+        }
+        return (names, height)
     }
 }
