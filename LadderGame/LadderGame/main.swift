a7818038b752b2d58ce789a59d78e72725b20dca
Leejinyeong0315 <yago0315@gmail.com>
2019-04-09 17:01:52 +0900

    Add main and User Input

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,19 @@
 
 import Foundation
 
+func getUserInput() -> (numberOfParticipants: Int, ladderHeight: Int) {
+    print("참여할 사람은 몇 명인가요?")
+    guard let numberOfParticipants = Int(readLine()!) else { return (0,0) }
+    
+    print("최대 사다리 높이는 몇 개인가요?")
+    guard let ladderHeight = Int(readLine()!) else { return (0,0) }
+    
+    return (numberOfParticipants, ladderHeight)
+}
+
+func main() {
+    let userInput = getUserInput()
+    
+}
+
+main()
