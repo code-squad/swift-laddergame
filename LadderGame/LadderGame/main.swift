2e37092e40db7ba565470bbea0e0c7bcd96a7fa5
Daheen Lee <allwhite.dev@gmail.com>
2019-04-09 12:22:37 +0900

    feat: Add main and input function

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,25 @@
 
 import Foundation
 
+
+
+func executeLadderGame() {
+    let (n, m) = getUserInputForGame()
+    print(n, m)
+}
+
+func getUserInputForGame() -> (Int, Int){
+    print("참여할 사람 수: ")
+    let n = readLine()
+    
+    print("최대 사다리 높이: ")
+    let m = readLine()
+    
+    guard let numberOfPeople = Int(n!), let maxLadderHeight = Int(m!) else {
+        fatalError("잘못된 입력입니다.")
+    }
+    
+    return (numberOfPeople, maxLadderHeight)
+}
+
+executeLadderGame()
