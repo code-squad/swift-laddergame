890cd1ec9a9f3f88dd0f73e2254d72146e60e0e8
kcushy <dev.kcushy@gmail.com>
2019-04-09 12:33:57 +0900

    refactor: Make a markup

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -9,20 +9,43 @@
 import Foundation
 
 struct InputView {
-    // receive input how many ladders are.
+    /**
+     receive input how many ladders are.
+    
+     - parameters:
+         - message: message for printing.
+     
+     - returns:
+     A string of input message. If not String, return empty string.
+    */
     private static func deliver(_ message: String) -> String {
         print(message, terminator: "")
         return readLine() ?? ""
     }
-    // receive input how many ladders are
+
+    /**
+     receive input how many ladders are
+     
+     - returns:
+     A String of number of ladders.
+    */
     static func receiveLadder() -> String {
         let input = deliver("최대 사다리의 높이는 몇개인가요? ex) 5:\n>>> ")
         return input
     }
-    // receive input how many people are.
+    
+    /**
+     receive input how many people are.
+     
+     - returns:
+     A String of number of people.
+         ```
+         nori, hngfu, jamie, billy
+         ```
+     */
+    //NOTE: return: LadderPlayer(name: input), return type: LadderPlayer
     static func receiveName() -> String {
         let input = deliver("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요) ")
         return input
     }
 }
-
