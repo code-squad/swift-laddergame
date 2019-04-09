a65440868fb215f6340e89ea3187e16d0154bbc2
Leejinyeong0315 <yago0315@gmail.com>
2019-04-09 18:07:56 +0900

    Add makeLadder()

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,18 +8,26 @@
 
 import Foundation
 
-func getUserInput() -> (numberOfParticipants: Int, ladderHeight: Int) {
+func getUserInput() -> (numberOfParticipants: Int, ladderHeight: Int)? {
     print("참여할 사람은 몇 명인가요?")
-    guard let numberOfParticipants = Int(readLine()!) else { return (0,0) }
+    guard let numberOfParticipants = Int(readLine()!) else { return nil }
     
     print("최대 사다리 높이는 몇 개인가요?")
-    guard let ladderHeight = Int(readLine()!) else { return (0,0) }
+    guard let ladderHeight = Int(readLine()!) else { return nil }
     
     return (numberOfParticipants, ladderHeight)
 }
 
+func makeLadder(numberOfParticipants: Int, ladderHeight: Int) -> [[Bool]] {
+    let column = numberOfParticipants * 2 - 1
+    let ladder = Array(repeating: Array(repeating: false, count: column), count: ladderHeight)
+    
+    return ladder
+}
+
 func main() {
-    let userInput = getUserInput()
+    guard let userInput = getUserInput() else { return }
+    var ladder = makeLadder(numberOfParticipants: userInput.numberOfParticipants, ladderHeight: userInput.ladderHeight)
     
 }
 
