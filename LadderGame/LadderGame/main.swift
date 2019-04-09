1881471dff2f40e621f373b25766d1ae5144c7c5
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-09 15:18:10 +0900

    run 함수에서 출력하도록 수정 주석 추가 처음 배열 만들 때 참가인원에서 -1을 해줌

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -16,7 +16,6 @@ extension Array {
 
 
 struct Ladder {
-    
     enum Component: String {
         case rung = "-"
         case empty = " "
@@ -26,21 +25,22 @@ struct Ladder {
     var printable = ""
     
     init(numberOfParticipants: Int, height: Int) {
-        let row = [Component](repeating: Component.empty, count: numberOfParticipants)
+        let row = [Component](repeating: Component.empty, count: numberOfParticipants - 1)
         info = [[Component]](repeating: row, count: height)
         for index in info.indices {
             info[index].insertRandomly(Ladder.Component.rung)
         }
     }
     
-    
+    /// 사다리를 출력가능한 문자열로 변환합니다.
     mutating func makePrintable() {
+        /// 열에 세로줄을 추가합니다.
         func getPrintableRow(row: [Component]) -> String {
             var printableRow = ""
             let verticalLine = "|"
             printableRow += verticalLine
             for component in row {
-                    printableRow += component.rawValue
+                printableRow += component.rawValue
                 printableRow += verticalLine
             }
             return printableRow
@@ -56,16 +56,18 @@ struct Ladder {
 
 func run() {
     print("참여할 사람은 몇 명 인가요?")
-    guard let numberOfParticipant = Int(readLine()!) else {
+    guard let numberOfParticipants = Int(readLine()!) else {
         print("오류: 숫자 아님")
         return
     }
     print("최대 사다리 높이는 몇 개인가요?")
-    guard let ladderHeight = Int(readLine()!) else {
+    guard let height = Int(readLine()!) else {
         print("오류: 숫자 아님")
         return
     }
-    
+    var ladder = Ladder(numberOfParticipants: numberOfParticipants, height: height)
+    ladder.makePrintable()
+    print(ladder.printable)
 }
 
 run()
