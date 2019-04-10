f75d58898b615af2179f327fef02e9076c2d12e9
Leejinyeong0315 <yago0315@gmail.com>
2019-04-11 00:48:45 +0900

    사다리 구성 함수 추가

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,26 +8,60 @@
 
 import Foundation
 
-func getUserInput() -> (numberOfParticipants: Int, ladderHeight: Int)? {
+func getUserInput() -> (numberOfParticipants: Int, heightOfLadder: Int)? {
     print("참여할 사람은 몇 명인가요?")
     guard let numberOfParticipants = Int(readLine()!) else { return nil }
     
     print("최대 사다리 높이는 몇 개인가요?")
-    guard let ladderHeight = Int(readLine()!) else { return nil }
+    guard let heightOfLadder = Int(readLine()!) else { return nil }
     
-    return (numberOfParticipants, ladderHeight)
+    return (numberOfParticipants, heightOfLadder)
 }
 
-func makeLadder(numberOfParticipants: Int, ladderHeight: Int) -> [[Bool]] {
-    let column = numberOfParticipants * 2 - 1
-    let ladder = Array(repeating: Array(repeating: false, count: column), count: ladderHeight)
+func createLadder(numberOfParticipants: Int, heightOfLadder: Int) -> [[String]] {
+    let numberOfColumn = numberOfParticipants * 2 - 1
+    let ladder = Array(repeating: Array(repeating: " ", count: numberOfColumn), count: heightOfLadder)
     
     return ladder
 }
 
+func getRandomPart() -> String {
+    let randomPartNumber = arc4random_uniform(2)
+    var randomParts: String
+    
+    if randomPartNumber == 0 {
+        randomParts = " "
+    } else {
+        randomParts = "-"
+    }
+    
+    return randomParts
+}
+
+func getLadderPart(columnNumber: Int) -> String {
+    var ladderPart: String
+    
+    if columnNumber % 2 == 0 {
+        ladderPart = "|"
+    } else {
+        ladderPart = getRandomPart()
+    }
+    
+    return ladderPart
+}
+
+func configureLadder(_ ladder: inout [[String]]) {
+    for (rowNumber, row) in ladder.enumerated() {
+        for columnNumber in 0..<row.count {
+            ladder[rowNumber][columnNumber] = getLadderPart(columnNumber: columnNumber)
+        }
+    }
+}
+
 func main() {
     guard let userInput = getUserInput() else { return }
-    var ladder = makeLadder(numberOfParticipants: userInput.numberOfParticipants, ladderHeight: userInput.ladderHeight)
+    var ladder = createLadder(numberOfParticipants: userInput.numberOfParticipants, heightOfLadder: userInput.heightOfLadder)
+    configureLadder(&ladder)
     
 }
 
