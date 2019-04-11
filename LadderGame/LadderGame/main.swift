7f24a8624cebb43d9fafb54b93726dfe4f848203
gukjin jang <unknown>
2019-04-11 18:01:32 +0900

    Modify : 튜플에 이름 추가 및 [[0]] => [[]]으로 수정

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 /// 사람과 사다리의 수를 입력받는 함수
-func inputCount()->(countOfPeople:Int,countOfLadder:Int) {
+func inputCount()->(countOfPeople:Int, countOfLadder:Int) {
     print("참여할 사람은 몇 명 인가요?")
     let countOfPeopleText = readLine()
     print("최대 사다리 높이는 몇 개인가요?")
@@ -9,7 +9,7 @@ func inputCount()->(countOfPeople:Int,countOfLadder:Int) {
     guard let countOfPeople = Int(countOfPeopleText!) else { return (0,0) }
     guard let countOfLadder = Int(countOfLadderText!) else { return (0,0) }
     
-    return (countOfPeople,countOfLadder)
+    return (countOfPeople:countOfPeople, countOfLadder:countOfLadder)
 }
 
 func makeLadderOfLayer(_ countOfPeople : Int)->[Int]{
@@ -23,17 +23,17 @@ func makeLadderOfLayer(_ countOfPeople : Int)->[Int]{
 }
 
 /// 사람의 숫자와 사다리의 숫자를 입력 받아 사다리를 만들고 출력하는 함수
-func makeLadder(_ countOfPeopleAndLadder : (Int,Int))->[[Int]] {
-    if countOfPeopleAndLadder.0 > 0 || countOfPeopleAndLadder.1 > 0 {
+func makeLadder(_ countOfPeopleAndLadder : (countOfPeople:Int,countOfLadder:Int))->[[Int]] {
+    if countOfPeopleAndLadder.countOfPeople > 0 && countOfPeopleAndLadder.countOfLadder > 0 {
         var ladders :[[Int]] = []
         
         // 사람의 숫자와 사다리의 숫자를 바탕으로 사다리의 모양을 2차원 배열에 저장
-        for ladderIndex in 0...countOfPeopleAndLadder.1-1 {
-            ladders.append(makeLadderOfLayer(countOfPeopleAndLadder.0))
+        for ladderIndex in 0...countOfPeopleAndLadder.countOfLadder-1 {
+            ladders.append(makeLadderOfLayer(countOfPeopleAndLadder.countOfPeople))
         }
         return ladders
     }
-    return [[0]]
+    return [[]]
 }
 
 /// 층별 사다리 출력함수
@@ -51,7 +51,7 @@ func outputLadderOfLayer(_ layer:[Int]){
 
 /// 층별 출력함수
 func outputLadder(_ ladders:[[Int]]) {
-    guard ladders == [[0]]
+    guard ladders == [[]]
     else {
         for ladderIndex in 0...ladders.count-1 {
             outputLadderOfLayer(ladders[ladderIndex])
