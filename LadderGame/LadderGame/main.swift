3005ad7a4302fe4b8e4bfd463eb5ff72550800b6
hwj0623 <hwj0623@naver.com>
2019-04-11 16:51:16 +0900

    STEP 4-2-1 STEP4-2의 |-|-| 검증 구현

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -24,7 +24,6 @@ enum ErrorCode: Int {
     case invalidInputRangeNumber = -1
 }
 
-
 /// 2차원 사다리 문자열 배열의 print 함수
 func printLadder(ladder2dMap : [[String]]) -> Void {
     for (rowItems) in ladder2dMap {
@@ -55,7 +54,6 @@ func buildLadder(ladder2dMap : [[String]]) -> [[String]] {
     let horizontalLadder: String = "-"
     ///사다리 없음
     let emptyLadder: String = " "
-    
     /// stride를 사용한 방식
     for (rowIndex, rowItems) in ladder2dMap.enumerated() {
         let byIndex: Int = 2
@@ -63,7 +61,15 @@ func buildLadder(ladder2dMap : [[String]]) -> [[String]] {
             /// 이진 난수생성에 기반한 Bool값으로 사다리 놓을지 여부 정하기
             let isLadderOn: Bool = binaryRandomGenerate()
             if isLadderOn {
-                resultLadder[rowIndex][columnIndex] = horizontalLadder
+                if columnIndex > 1 {
+                    if resultLadder[rowIndex][columnIndex-2] != "-" {
+                        resultLadder[rowIndex][columnIndex] = horizontalLadder
+                    }else {
+                        resultLadder[rowIndex][columnIndex] = emptyLadder
+                    }
+                } else{
+                    resultLadder[rowIndex][columnIndex] = emptyLadder
+                }
             }else{
                 resultLadder[rowIndex][columnIndex] = emptyLadder
             }
@@ -89,26 +95,16 @@ func inputPairNumber() -> (Int, Int, String ) {
                                                  ValidResultCode.invalid.rawValue)
     ///`n` 명의 사람
     print("참여할 사람은 몇 명 인가요? (2이상의 자연수 입력)")
-    guard let n: String = readLine()
-        else{
-            return invalidInputPair
-    }
-    /// 유효 숫자 체크 입력값이 정수인지 - 에러코드(ErrorCode.notANumber.rawValue)
-    let typeCastNumberOfPeople = checkInvalidCharacter(n)
-    /// 유효 범위 체크 - 에러코드 (ErrorCode.invalidInputRangeNumber.rawValue)
-    let people: Int = checkValidIntegerNumber(typeCastNumberOfPeople)
-    
-    ///`m` 개의 m 높이의 사다리
+    guard let n: String = readLine() else{ return invalidInputPair }
+    let typeCastNumberOfPeople = checkInvalidCharacter(n)     /// 유효 숫자 체크 입력값이 정수인지 - 에러코드(ErrorCode.notANumber.rawValue)
+    let people: Int = checkValidIntegerNumber(typeCastNumberOfPeople)     /// 유효 범위 체크 - 에러코드 (ErrorCode.invalidInputRangeNumber.rawValue)
+    ///`m` 높이의 사다리
     print("최대 사다리 높이는 몇 개인가요? (2이상의 자연수 입력)")
-    guard let m: String = readLine()
-        else{
-            return invalidInputPair
-    }
+    guard let m: String = readLine() else{ return invalidInputPair }
     /// 유효 숫자 체크 입력값이 정수인지 - 에러코드(ErrorCode.notANumber.rawValue)
     let typeCastNumberOfLadders = checkInvalidCharacter(m)
     /// 유효 범위 체크 - 에러코드 (ErrorCode.invalidInputRangeNumber.rawValue)
     let ladders: Int = checkValidIntegerNumber(typeCastNumberOfLadders)
-    
     let isValid: String = checkTotalValidity(people, ladders)
     
     return (people, ladders, isValid)
