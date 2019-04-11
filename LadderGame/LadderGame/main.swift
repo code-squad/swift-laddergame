3d0468bdd4d30d495d0dbe34646343ae71f70cfa
박준희 <junibibi04@gmail.com>
2019-04-11 17:26:29 +0900

    이중배열 원소범위, for문 범위 수정
    이중배열 원소갯수 부분 수정
    for문 범위를 수정

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -18,11 +18,11 @@ var randomNum = arc4random_uniform(500)
 
 
 //Bool값을 받을 이중배열 생성
-var boolArr : [[Bool]] = Array(repeating: Array(repeating: true,count:5 ), count: 3)
+var boolArr : [[Bool]] = Array(repeating: Array(repeating: true,count:peopleNum-1 ), count: ladderNum)
 
 //랜덤한 Bool값을 이중배열에 넣어줌
 for a in 0..<3 {
-    for b in 0...peopleNum - 2 {
+    for b in 0..<peopleNum - 1 {
         let randomNum = arc4random_uniform(500)
         if randomNum % 2 == 1 {
             boolArr[a][b]=(true)
