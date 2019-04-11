57bf55c3ee907d83bf0770bd42b2b5621e33e8e0
박준희 <junibibi04@gmail.com>
2019-04-11 17:03:25 +0900

    이중배열생성
    사다리가 연결되는지 안되는지 저장하는 bool이중배열 생성

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,15 @@
 
 import Foundation
 
+
+//사용자한테 입력받을 인원수와 사다리갯수(일단 입력->나중에 사용자가 입력하게끔 수정)
+var peopleNum = 5
+var ladderNum = 3
+
+//랜덤한수
+var randomNum = arc4random_uniform(500)
+
+
+//Bool값을 받을 이중배열 생성
+var boolArr : [[Bool]] = Array(repeating: Array(repeating: true,count:5 ), count: 3)
+
