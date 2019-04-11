fe134f9a2de361a555f2e158cddf2d5e44a81bca
kimjieun <zieunv@gmail.com>
2019-04-11 13:53:36 +0900

    프로그램 설계하기 (1차)

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,55 @@
 
 import Foundation
 
+enum Component: String {
+    case connect = "-"
+    case blank = " "
+    case bothsides = "|"
+}
+
+func makeLadder(maximumPeople: Int, maximumLayer: Int) -> [[Component]] {
+    var ladder: [[Component]] = [[]]
+    ladder[0].append(Component.bothsides)
+    
+    for _ in 1..<maximumPeople {
+        ladder[0].append(Component.blank)
+        ladder[0].append(Component.bothsides)
+    }
+    for _ in 1..<maximumLayer {
+        ladder.append(ladder[0])
+    }
+    return ladder
+}
+
+func connectLadder(ladder: [[Component]], maximumLayer: Int, maximumPeople: Int) -> [[Component]] {
+    var ladderConnect = ladder
+    
+    for row in 0..<maximumLayer {
+        for colunm in stride(from: 1, to: (maximumPeople * 2 - 1), by: 2)
+        {
+            if Bool.random() {
+                ladderConnect[row][colunm] = Component.connect
+            }
+        }
+        
+    }
+    return ladderConnect
+    
+}
+
+func main(){
+    print("참여할 사람은 몇 명 인가요 ?")
+    guard let maximumPeople = Int(readLine()!) else { return }
+    print("최대 사다리 높이는 몇 개 인가요 ?")
+    guard let maximumLayer = Int(readLine()!) else { return }
+    var ladder = makeLadder(maximumPeople: maximumPeople, maximumLayer: maximumLayer)
+    ladder = connectLadder(ladder: ladder, maximumLayer: maximumLayer, maximumPeople: maximumPeople)
+    
+    for row in ladder {
+        for column in row {
+            print(column.rawValue, terminator: "")
+        }
+        print()
+    }
+}
+main()
