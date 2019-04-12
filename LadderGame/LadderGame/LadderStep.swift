de8c4f6e87446c830aa4d168b7003815e1cbb795
Daheen Lee <allwhite.dev@gmail.com>
2019-04-12 16:41:19 +0900

    Add LadderStep.swift

---
+++ LadderGame/LadderGame/LadderStep.swift
@@ -0,0 +1,13 @@
+//
+//  LadderStep.swift
+//  LadderGame
+//
+//  Created by Daheen Lee on 12/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct LadderStep {
+    let exists: Bool
+}
