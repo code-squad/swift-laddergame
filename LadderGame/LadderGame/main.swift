039096e0b8fcf1bc345e1fcfa167c16ba0add3c0
gukjin jang <unknown>
2019-04-12 13:18:33 +0900

    Modify : 기존의 print 입력 문구 출력 부분을 enum으로 변경, 이에 따라 inputPeopleAndLadder() 삭제 및 makeLadder()에 배열이 인자로 들어가도록 변경

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -1,11 +1,23 @@
 import Foundation
+
+enum CountQuestions : String{
+    case people = "참여할 사람은 몇 명 인가요?"
+    case ladder = "최대 사다리 높이는 몇 개인가요?"
+    
+    static let allValues = [people,ladder]
+}
+
 /// 사람과 사다리의 수를 입력받는 함수
-func inputCount()->Int {
-    let countOfText = readLine()
-    
-    guard let count = Int(countOfText!) else { return 0 }
-    
-    return count
+func inputCount()->[Int] {
+    var counts:[Int] = []
+    for question in CountQuestions.allValues{
+        print(question.rawValue)
+        let countOfText = readLine()
+        
+        guard let count = UInt(countOfText!) else { return [] }
+        if count>0 { counts.append(Int(count)) }
+    }
+    return counts
 }
 
 func makeLadderOfLayer(_ countOfPeople : Int)->[Int]{
@@ -19,17 +31,15 @@ func makeLadderOfLayer(_ countOfPeople : Int)->[Int]{
 }
 
 /// 사람의 숫자와 사다리의 숫자를 입력 받아 사다리를 만들고 출력하는 함수
-func makeLadder()->[[Int]] {
-    print("참여할 사람은 몇 명 인가요?")
-    let countOfPeople = inputCount()
-    print("최대 사다리 높이는 몇 개인가요?")
-    let countOfLadder = inputCount()
-    if countOfPeople > 0 && countOfLadder > 0 {
+func makeLadder(_ totalCount : [Int])->[[Int]] {
+    if totalCount.contains(0) != true && totalCount.count>1{
+        let countLadder = totalCount[1]
+        let countPeople = totalCount[0]
         var ladders :[[Int]] = []
         
         // 사람의 숫자와 사다리의 숫자를 바탕으로 사다리의 모양을 2차원 배열에 저장
-        for ladderIndex in 0...countOfLadder-1 {
-            ladders.append(makeLadderOfLayer(countOfPeople))
+        for ladderIndex in 0...countLadder-1 {
+            ladders.append(makeLadderOfLayer(countPeople))
         }
         return ladders
     }
@@ -62,6 +72,6 @@ func outputLadder(_ ladders:[[Int]]) {
     print("0보다 큰 정수를 입력해주세요!")
 }
 
-let ladders = makeLadder()
+let ladders = makeLadder(inputCount())
 outputLadder(ladders)
 
