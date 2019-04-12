b4dd78d42c9b62135c0f3ad9d6a8a40cdf63e1aa
dongY <ldcpaul@hanmail.net>
2019-04-13 00:05:01 +0900

    사디리 기둥 세우기 함수 추가

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -12,11 +12,10 @@ typealias LadderInfo = (Int,Int)
 typealias Columes = [[String]]
 typealias Steps = [[String]]
 
-func inputLadderInfo()->(Int,Int){
-    let numberOfPeople = inputNumberOfPeople()
-    let heightOfLadder = inputHeightOfLadder()
-    return (numberOfPeople,heightOfLadder)
-}
+//=====================
+//    1단계 데이터 입력
+//=====================
+
 func inputNumberOfPeople()->(Int){
     print("참여할 사람은 몇 명 인가요?")
     let numberOfPeople = stringToInt(target: readLine()!)
@@ -36,6 +35,37 @@ func stringToInt(target:String)->(Int){
     return int
 }
 
+//=====================
+//    2단계 데이터 처리
+//=====================
+func inputLadderInfo()->(LadderInfo){
+    let numberOfPeople = inputNumberOfPeople()
+    let heightOfLadder = inputHeightOfLadder()
+    return (numberOfPeople,heightOfLadder)
+}
+
+//=====================
+//    3단계 데이터 저장
+//=====================
+func makeColumes(ladderInfo : LadderInfo)->(Columes){
+    let (numberOfPeople,heightOfLadder) = ladderInfo
+    var columes = Columes()
+    for _ in 1...numberOfPeople{
+        let colume = makeColume(heightOfLadder: heightOfLadder)
+        columes.append(colume)
+    }
+    return columes
+}
+func makeColume(heightOfLadder:Int)->([String]){
+    let colume = PatternColume.colume
+    return [String].init(repeating: colume.rawValue, count: heightOfLadder)
+}
+
+//=====================
+//    4단계 데이터 형식화
+//=====================
 
 
-
+//=====================
+//    2단계 데이터 출력
+//=====================
