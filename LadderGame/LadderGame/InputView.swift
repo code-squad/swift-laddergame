a1b3097c4de6aa7b7a2909e82db92db17db77352
Daheen Lee <allwhite.dev@gmail.com>
2019-04-12 17:17:05 +0900

    Add InputView.swift

---
+++ LadderGame/LadderGame/InputView.swift
@@ -0,0 +1,17 @@
+//
+//  InputView.swift
+//  LadderGame
+//
+//  Created by Daheen Lee on 12/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    //1. get names of player
+    //2. create LadderPlayer instance using names
+    //3. get height of ladder
+    //4. create LadderGame instance using LadderPlayer instance, and height
+    //5. return LadderGame instance
+}
