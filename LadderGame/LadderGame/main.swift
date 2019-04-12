227c211a1415bcabe9ede466814b80470220afda
dongY <ldcpaul@hanmail.net>
2019-04-12 23:35:10 +0900

    게임시작에 앞서 사다리의 정보를 입력받는 함수 선언

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -9,6 +9,31 @@ enum PattenOfStep:String{
     case none = " "
 }
 
+func inputLadderInfo()->(Int,Int){
+    let numberOfPeople = inputNumberOfPeople()
+    let heightOfLadder = inputHeightOfLadder()
+    return (numberOfPeople,heightOfLadder)
+}
+func inputNumberOfPeople()->(Int){
+    print("참여할 사람은 몇 명 인가요?")
+    let numberOfPeople = stringToInt(target: readLine()!)
+    
+    return numberOfPeople
+}
+func inputHeightOfLadder()->(Int){
+    print("최대 사다리 높이는 몇 개인가요?")
+    let ladderOfHeight = stringToInt(target: readLine()!)
+    
+    return ladderOfHeight
+}
+
+func stringToInt(target:String)->(Int){
+    guard let int = Int(target) else{
+        return 0
+    }
+    return int
+}
+
 
 
 
