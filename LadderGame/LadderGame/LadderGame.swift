36e00892388567d8eb89dacf25028bf185f2bcce
Daheen Lee <allwhite.dev@gmail.com>
2019-04-12 16:42:49 +0900

    Add LadderGame.swift

---
+++ LadderGame/LadderGame/LadderGame.swift
@@ -0,0 +1,15 @@
+//
+//  LadderGame.swift
+//  LadderGame
+//
+//  Created by Daheen Lee on 12/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct LadderGame {
+    var height: Int
+    var players: [LadderPlayer]
+    
+}
