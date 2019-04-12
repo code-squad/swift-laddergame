563ba687e5518de20bdb74c3ab5e76216a4e590a
kiljh <kiljh@me.com>
2019-04-12 13:17:20 +0900

    사람 숫자와 최대 사다리 높이를 저장하는 변수 이름 변경
    제이케이의 피드백처럼 numberOf를 생략해보았다.
    최대 사다리 높이에서 이미 함수명이나 전후 사정으로 사다리라는 것은 알 수 있으므로 maximumHeight 로 줄였다.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -10,8 +10,8 @@ import Foundation
 
 /// 입력할 수 있는 변수 이름들과 입력 안내 멘트
 enum InputableVariableName: String {
-    case numberOfParticipants = "참여할 사람은 몇 명 인가요?"
-    case maximumLadderHeight = "최대 사다리 높이는 몇 개인가요?"
+    case participants = "참여할 사람은 몇 명 인가요?"
+    case maximumHeight = "최대 사다리 높이는 몇 개인가요?"
 }
 
 /// 에러 타입
@@ -21,20 +21,20 @@ enum LadderGameErrors: Error {
 
 /// 사용자로부터 입력받는 함수
 func input () -> (Int, Int) {
-    var numberOfParticipants = 0
-    var maximumLadderHeight = 0
+    var participants = 0
+    var maximumHeight = 0
     
     while true {
         do {
-            numberOfParticipants = try inputFromUser(InputableVariableName.numberOfParticipants)
-            maximumLadderHeight = try inputFromUser(InputableVariableName.maximumLadderHeight)
+            participants = try inputFromUser(InputableVariableName.participants)
+            maximumHeight = try inputFromUser(InputableVariableName.maximumHeight)
             break
         } catch {
             print("참여할 사람과 최대 사다리 높이는 최소 2 이상이어야 합니다.")
         }
     }
     
-    return (numberOfParticipants, maximumLadderHeight)
+    return (participants, maximumHeight)
 }
 func inputFromUser (_ variableName: InputableVariableName) throws -> (Int) {
     print(variableName.rawValue)
@@ -45,22 +45,22 @@ func inputFromUser (_ variableName: InputableVariableName) throws -> (Int) {
 }
 
 /// 사다리를 만들고 저장하는 함수
-func makeLadder (_ numberOfParticipants: Int, _ maximumLadderHeight: Int) -> [[Bool]] {
-    var ladderBoolBoard : [[Bool]] = Array(repeating: Array(repeating: false, count:numberOfParticipants-1), count: maximumLadderHeight)
+func makeLadder (_ participants: Int, _ maximumHeight: Int) -> [[Bool]] {
+    var ladderBoolBoard : [[Bool]] = Array(repeating: Array(repeating: false, count:participants-1), count: maximumHeight)
     
-    for heightIndex in 0..<maximumLadderHeight {
-        ladderBoolBoard[heightIndex] = makeLadderRow(numberOfParticipants)
+    for heightIndex in 0..<maximumHeight {
+        ladderBoolBoard[heightIndex] = makeLadderRow(participants)
     }
     
     return ladderBoolBoard
 }
 
 /// 사다리 한줄을 만드는 함수
-func makeLadderRow (_ numberOfParticipants: Int) -> [Bool] {
-    var ladderBoolRow : [Bool] = Array(repeating: false, count:numberOfParticipants)
+func makeLadderRow (_ participants: Int) -> [Bool] {
+    var ladderBoolRow : [Bool] = Array(repeating: false, count:participants)
     var boolRandom = false
     
-    for humanIndex in 0..<numberOfParticipants-1 {
+    for humanIndex in 0..<participants-1 {
         boolRandom = boolRandomGenerate(prevBool: boolRandom)
         ladderBoolRow[humanIndex] = boolRandom
     }
@@ -80,17 +80,17 @@ func boolRandomGenerate(prevBool: Bool) -> Bool {
 
 /// 사다리를 출력하는 함수
 func printLadder (_ ladderBoolBoard:[[Bool]]) -> () {
-    let maximumLadderHeight = ladderBoolBoard.count
+    let maximumHeight = ladderBoolBoard.count
     
-    for heightIndex in 0..<maximumLadderHeight {
+    for heightIndex in 0..<maximumHeight {
         printLadderRow(ladderBoolRow: ladderBoolBoard[heightIndex])
     }
 }
 
 /// 사다리 한줄을 출력하는 함수
 func printLadderRow (ladderBoolRow:[Bool]) -> () {
-    let numberOfParticipants = ladderBoolRow.count
-    for humanIndex in 0..<numberOfParticipants-1 {
+    let participants = ladderBoolRow.count
+    for humanIndex in 0..<participants-1 {
         print("|", terminator: "")
         switch ladderBoolRow[humanIndex] {
         case true : print("-", terminator: "")
@@ -102,9 +102,9 @@ func printLadderRow (ladderBoolRow:[Bool]) -> () {
 
 /// 사다리 게임 함수
 func ladderGame() {
-    let numberOfParticipants, maximumLadderHeight: Int
-    (numberOfParticipants, maximumLadderHeight) = input()
-    let ladderBoolBoard = makeLadder(numberOfParticipants, maximumLadderHeight)
+    let participants, maximumHeight: Int
+    (participants, maximumHeight) = input()
+    let ladderBoolBoard = makeLadder(participants, maximumHeight)
     printLadder(ladderBoolBoard)
 }
 
