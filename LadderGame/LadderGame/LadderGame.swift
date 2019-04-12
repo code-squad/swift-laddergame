7247c735eb4919b3a8eb4450a09cc63d25f62ee3
kiljh <kiljh@me.com>
2019-04-12 21:07:53 +0900

    사다리를 만드는 LadderGame 구조체 구현

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct LadderGame {
+    var height = 0
+    var names : [LadderPlayer]
+    
+    /// 사다리를 만들고 저장하는 함수
+    func makeLadder (_ participants: Int, _ maximumHeight: Int) -> [[Bool]] {
+        var ladderBoolBoard : [[Bool]] = Array(repeating: Array(repeating: false, count:participants-1), count: maximumHeight)
+        
+        for heightIndex in 0..<maximumHeight {
+            ladderBoolBoard[heightIndex] = makeLadderRow(participants)
+        }
+        
+        return ladderBoolBoard
+    }
+    
+    /// 사다리 한줄을 만드는 함수
+    func makeLadderRow (_ participants: Int) -> [Bool] {
+        var ladderBoolRow : [Bool] = Array(repeating: false, count:participants)
+        var boolRandom = false
+        
+        for humanIndex in 0..<participants-1 {
+            boolRandom = boolRandomGenerate(prevBool: boolRandom)
+            ladderBoolRow[humanIndex] = boolRandom
+        }
+        
+        return ladderBoolRow
+    }
+    
+    ///사다리를 만들지 여부를 랜덤으로 결정하는 함수
+    func boolRandomGenerate(prevBool: Bool) -> Bool {
+        var boolRandom = Bool.random()
+        if prevBool == true && boolRandom == true {
+            boolRandom = false
+        }
+        
+        return boolRandom
+    }
+}
