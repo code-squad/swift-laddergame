495ec3f061b8557ecd745d941c9281f09df26a5d
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-12 13:13:39 +0900

    실행 함수 제거 재 작성 예정

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -1,36 +1,13 @@
 import Foundation
 
 
-enum InputError: Error {
-    case invalidInput
-    case invalidNumberOfParticipants
-    case invalidLadderHeight
-}
-
-func readLadderInfo() throws -> (numberOfParticipants: Int, height: Int) {
-    let numberOfParticipants = try askUserAndReadNumber(question: "참여할 사람은 몇 명 인가요?")
-    guard numberOfParticipants > 1 else {
-        throw InputError.invalidNumberOfParticipants
-    }
-    let height = try askUserAndReadNumber(question: "사다리 높이는 몇 개인가요?")
-    guard height > 0 else {
-        throw InputError.invalidLadderHeight
-    }
-    return (numberOfParticipants, height)
-}
-
-
-func askUserAndReadNumber(question: String) throws -> Int {
-    print(question)
-    guard let numberEnterd = Int(readLine()!) else {
-        throw InputError.invalidInput
-    }
-    return numberEnterd
-}
-
-
-func runLadderGame() throws {
-    let ladderInfo = try readLadderInfo()
+func main() throws {
+    
+    
+    
+    
+    
+    
     
 }
 
