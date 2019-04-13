fc87775dc7c12edb6c291101789d5dd40e2e5323
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-13 16:02:15 +0900

    Ladder를 LadderGame에 내장함

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -1,15 +1,52 @@
 import Foundation
 
+extension Array where Element == LadderComponent {
+    /// 사다리 배열에 가로대를 무작위로 삽입합니다. 단, 바로 전에 가로대를 넣은 경우 넣지 않습니다.
+    func rungsRandomlyInserted() -> [LadderComponent] {
+        var wasPlacedJustBefore = false
+        var rowWithRungs = self
+        for index in self.indices {
+            if !wasPlacedJustBefore && Bool.random() {
+                rowWithRungs[index] = LadderComponent.rung
+                wasPlacedJustBefore = true
+            } else {
+                wasPlacedJustBefore = false
+            }
+        }
+        return rowWithRungs
+    }
+    
+}
+
+enum LadderComponent: String {
+    case rung = "-"
+    case empty = " "
+}
+
 struct LadderGame {
     
-    let ladder: Ladder
+    let ladder: [[LadderComponent]]
     let players: [Player]
     static let maximumNameLength = 5
     
     init(players: [Player], height: Int) {
         self.players = players
-        ladder = Ladder(numberOfPlayers: players.count, height: height)
+        
+        var ladder: [[LadderComponent]] = []
+        let row = [LadderComponent](repeating: LadderComponent.empty, count: players.count - 1)
+        let emptyLadder: [[LadderComponent]] = [[LadderComponent]](repeating: row, count: height)
+        
+        for row in emptyLadder.indices {
+            ladder.append(emptyLadder[row].rungsRandomlyInserted())
+            // 사다리 타기 로직 구현에 용이하도록 추가하는 항목입니다.
+            ladder[row].insert(LadderComponent.empty, at: 0)
+            ladder[row].append(LadderComponent.empty)
+        }
+        
+        self.ladder = ladder
+        
     }
+    
     
     
 }
--- LadderGame/LadderGame/LadderGameView.swift
+++ LadderGame/LadderGame/LadderGameView.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Array where Element == Ladder.Component {
+extension Array where Element == LadderComponent {
     /// 사다리 열을 문자열로 변환합니다.
     func stringized() -> String {
         let stringizedInfo = self.map { String(repeating: $0.rawValue, count: LadderGame.maximumNameLength) }
@@ -8,7 +8,7 @@ extension Array where Element == Ladder.Component {
     }
 }
 
-extension Array where Element == [Ladder.Component] {
+extension Array where Element == [LadderComponent] {
     /// 사다리를 문자열로 변환합니다.
     func stringized() -> String {
         var stringizedRows: [String] = []
@@ -25,14 +25,20 @@ struct LadderGameView {
     private let stringizedPlayers: String
     
     init(ladderGame: LadderGame) {
-        stringizedLadder = ladderGame.ladder.info.stringized()
+        stringizedLadder = ladderGame.ladder.stringized()
         let players = ladderGame.players.map { $0.alignedName }
         self.stringizedPlayers = players.joined(separator: " ")
     }
     
     func printPlayersAndLadder() {
         let leadingSpaces = String(repeating: " ", count: LadderGame.maximumNameLength / 2 + 1)
-        print("\(stringizedLadder)\n\(leadingSpaces)\(stringizedPlayers)")
+        print(
+            """
+            \(leadingSpaces)\(stringizedPlayers)
+            \(stringizedLadder)
+            \(leadingSpaces)
+            """
+        )
     }
     
 }
