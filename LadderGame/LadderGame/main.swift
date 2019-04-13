1375cba5cc699cdd4413d37c9e07c7b9b44dc544
junyng <pom0894@naver.com>
2019-04-13 13:26:28 +0900

    함수 사이 마다 빈 간격 추가

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -26,6 +26,7 @@ func startGame(_ settings: Settings) {
     buildLadder(&ladderMatrix)
     printLadder(ladderMatrix)
 }
+
 /// 게임을 설정합니다.
 func setupGame() -> Settings {
     let userNumber = setUserNumber()
@@ -33,22 +34,26 @@ func setupGame() -> Settings {
     
     return (userNumber, ladderHeight)
 }
+
 /// 유저수를 설정합니다.
 func setUserNumber() -> Int? {
     print("참여할 사람은 몇 명 인가요?")
     guard let input = readLine() else { return nil }
     return Int(input)
 }
+
 /// 사다리의 높이를 설정합니다.
 func setLadderHeight() -> Int? {
     print("최대 사다리 높이는 몇 개인가요?")
     guard let input = readLine() else { return nil }
     return Int(input)
 }
+
 /// 2차원 배열을 생성합니다.
 func create2DMatrix(rowCount: Int, colCount: Int) -> [[LadderPart]] {
     return Array(repeating: Array(repeating: .none, count: rowCount * 2 - 1), count: colCount)
 }
+
 /// 사다리를 구성합니다.
 func buildLadder(_ matrix: inout [[LadderPart]]) {
     for (colNumber, array) in matrix.enumerated() {
@@ -57,6 +62,7 @@ func buildLadder(_ matrix: inout [[LadderPart]]) {
         }
     }
 }
+
 /// 사다리 부품을 가져옵니다.
 func getLadderPart(index: Int) -> LadderPart {
     // 짝수는 기둥, 홀수는 랜덤 값에 의해 가로 기둥을 가져옵니다.
@@ -67,6 +73,7 @@ func getLadderPart(index: Int) -> LadderPart {
         return isLadderPartEmpty ? .none : .bar
     }
 }
+
 /// 사다리를 출력합니다.
 func printLadder(_ matrix: [[LadderPart]]) {
     for array in matrix {
@@ -76,6 +83,7 @@ func printLadder(_ matrix: [[LadderPart]]) {
         print(separator: "\n")
     }
 }
+
 /// 랜덤 불 값을 가져옵니다.
 func randomBool() -> Bool {
     return Int(arc4random_uniform(2)) == 1
