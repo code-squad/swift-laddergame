00327f72bd3ccfd02080c095198ce99bd215296c
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-13 14:58:29 +0900

    가로대 무작위 추가 함수의 위치를 Ladder.swift로 옮김

--- LadderGame/LadderGame/LadderGameView.swift
+++ LadderGame/LadderGame/LadderGameView.swift
@@ -1,11 +1,23 @@
 import Foundation
 
+extension Array where Element == Ladder.Component {
+    /// 사다리 열을 문자열로 변환합니다.
+    func stringized() -> String {
+        let stringizedInfo = self.map { String(repeating: $0.rawValue, count: 5) }
+        return stringizedInfo.joined(separator: "|")
+    }
+}
+
 struct LadderGameView {
     
+    let stringizedLadder: String
+    let stringizedPlayers: String
+    let stringizedResult: String
     
     
-    
-    
+    func stringize(ladder: Ladder) -> String {
+        <#function body#>
+    }
     
     
     
