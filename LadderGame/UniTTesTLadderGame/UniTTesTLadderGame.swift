1e5b66297087e75692dcfa736c3275236e81954f
kcushy <dev.kcushy@gmail.com>
2019-04-15 02:10:14 +0900

    Feat: Add a Unit Test

--- LadderGame/UniTTesTLadderGame/UniTTesTLadderGame.swift
+++ LadderGame/UniTTesTLadderGame/UniTTesTLadderGame.swift
@@ -1,43 +1,70 @@
 //
-//  UnitTesTLaddeRGame.swift
-//  UnitTesTLaddeRGame
+//  UnitTestLadderGame.swift
+//  UnitTestLadderGame
 //
-//  Created by cushy k on 13/04/2019.
+//  Created by cushy k on 14/04/2019.
 //  Copyright © 2019 Codesquad Inc. All rights reserved.
 //
 
 import XCTest
 @testable import LadderGame
 
-class UnitTesTLaddeRGame: XCTestCase {
-
-    var ladderGame: LadderGame!
-    var ladderPlayer: LadderPlayer!
-
+class UnitTestLadderGame: XCTestCase {
+    var ladderGame : LadderGame!
+    var ladderPlayer : LadderPlayer!
+    var names : [String]!
+    var notEnoughHeight: String!
+    var notEnoughName: [String]!
+    var inspection: Inspection!
+    var notInt: String!
+    var normalHeight: String!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         super.setUp()
-        ladderPlayer = LadderPlayer(names: ["nori","jamie","justin","drake"])
-        ladderGame = LadderGame(people: ladderPlayer, height: 5)
-
+        names = ["hngfu", "nori", "drake", "cony"]
+        ladderPlayer = LadderPlayer(names: names)
+        inspection = Inspection()
+        notInt = "five"
+        notEnoughHeight = "0"
+        notEnoughName = [""]
+        normalHeight = "5"
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        ladderGame = nil
+        ladderPlayer = nil
+        names = nil
+        inspection = nil
+        notInt = nil
+        notEnoughHeight = nil
+        notEnoughName = nil
+        normalHeight = nil
     }
 
-    func testExample() {
-//
-//        XCTAssertEqual(ladderPlayers.names, ["nori"])
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHeightOver() {
+        let namesIncludedOverMaxLength = ["hngfu", "nori", "justin"]
+        ladderPlayer = LadderPlayer(names: namesIncludedOverMaxLength)
+        XCTAssertThrowsError(try inspection.meetLength(of: ladderPlayer), "A name length is over, but nothing happend") { (error) in
+            XCTAssertEqual(error as? inputError, inputError.exceedLength)}
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testHeightNotIncludedInt() {
+        XCTAssertThrowsError(try inspection.meetMinimum(of: ladderPlayer, of: notInt), "A ladder is not int type, but nothing happend") { (error) in
+            XCTAssertEqual(error as? inputError, inputError.wrongValue)}
     }
 
+    func testNameHasNotEnough() {
+        let lackPlayer = LadderPlayer(names: notEnoughName)
+        XCTAssertThrowsError(try inspection.meetMinimum(of: lackPlayer, of: normalHeight), "No name was entered, but nothing happend") { (error) in
+            XCTAssertEqual(error as? inputError, inputError.lackValue)}
+    }
+
+    func testHeightHasNotEnough() {
+        XCTAssertThrowsError(try inspection.meetMinimum(of: ladderPlayer, of: notEnoughHeight), "There is not enough ladder, but nothing happend") { (error) in
+            XCTAssertEqual(error as? inputError, inputError.lackValue)}
+    }
 }
