af32543b71c3d4990c72a76495292a4a03a5f960
gukjin jang <unknown>
2019-04-14 18:20:13 +0900

    Add : 참가자의 배열과 사다리의 높이를 입력받아 사다리를 제작하는 LadderStep 구조체 생성

--- LadderGame/LadderGame/LadderStep.swift
+++ LadderGame/LadderGame/LadderStep.swift
@@ -10,6 +10,22 @@ import Foundation
 
 /// 사다리 발판 하나를 표현하는 구조체
 struct LadderStep {
+    func makeLayerOfLadder(players:[String])->[Int]{
+        var layerOfLadders : [Int] = []
+        for playerIndex in 0...players.count-1{
+            var judgmentLadder = Int.random(in:0...1)
+            if playerIndex >= 1 && layerOfLadders[playerIndex-1] == 1 { judgmentLadder = 0}
+            layerOfLadders.append(judgmentLadder)
+        }
+        return layerOfLadders
+    }
     
-    
+    func makeLadder(players: [String], ladderHeight:Int)->[[Int]]{
+        var ladders : [[Int]] = []
+        for ladderIndex in 0...ladderHeight-1{
+            let layerOfLadders = makeLayerOfLadder(players: players)
+            ladders.append(layerOfLadders)
+        }
+        return ladders
+    }
 }
