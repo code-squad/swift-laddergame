a92d727e51be816588116109afc4cb6109ad607c
Daheen Lee <allwhite.dev@gmail.com>
2019-04-15 16:08:11 +0900

    feat: Add method to print ladder game board in ResultView struct

--- LadderGame/LadderGame/ResultView.swift
+++ LadderGame/LadderGame/ResultView.swift
@@ -9,6 +9,44 @@
 import Foundation
 
 struct ResultView {
-    //1. print player names
-    //1. print LadderGame.board
+    func printLadderGameBoard(using ladderGame: LadderGame) {
+        //1. print ladderGame.board
+        printLadder(using: ladderGame.board)
+        //2. print player names
+        printPlayers(using: ladderGame.players)
+        
+    }
+    
+    // print whole ladder
+    func printLadderGame(using board: [[LadderStep]]) {
+        for row in board {
+            printLadderBy(row)
+        }
+    }
+    
+    func printLadderBy(_ row: [LadderStep]) {
+        print("  ", terminator: "")
+        printRail()
+        for step in row {
+            printStep(if: step.exists)
+            printRail()
+        }
+        print("")
+    }
+    
+    func printRail() {
+        let rail = "|"
+        print(rail, terminator: "")
+    }
+    
+    func printStep(if exist: Bool) {
+        let step = exist ? "-----" : "    "
+        print(step, terminator: "")
+    }
+    
+    func printPlayers(using players: [LadderPlayer]) {
+        for player in players {
+            print(player.name, terminator: "  ")
+        }
+    }
 }
