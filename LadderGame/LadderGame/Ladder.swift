51b47745d7d890220fcacb77c6e3b55819026afb
cmindy <minjidev@naver.com>
2019-04-15 16:00:49 +0900

    refactor: Add Ladder Struct to express ladder

---
+++ LadderGame/LadderGame/Ladder.swift
@@ -0,0 +1,35 @@
+//
+//  Ladder.swift
+//  LadderGame
+//
+//  Created by CHOMINJI on 15/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Ladder {
+    
+    var ladderFrame: [LadderFloor] = []
+    
+    init(numberOfPlayer: Int, ladderHeight: Int) {
+        
+        for _ in 0..<ladderHeight {
+            ladderFrame.append(LadderFloor.init(numberOfPlayer: numberOfPlayer))
+        }
+    }
+    
+    private subscript(floor: Int) -> LadderFloor {
+        get {
+            return ladderFrame[floor]
+        }
+    }
+    
+    func drawLadder() {
+        
+        for floor in ladderFrame.indices{
+            ladderFrame[floor].drawFloor()
+        }
+    }
+}
+
