50cfe82ae2f1271d59b325d32768f7fc6a667da9
Kroisttt <yago0315@gmail.com>
2019-04-15 16:12:10 +0900

    LadderPlayer 구조체 추가

---
+++ LadderGame/LadderGame/LadderPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  LadderPlayer.swift
+//  LadderGame
+//
+//  Created by 이진영 on 15/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct LadderPlayer {
+    let name: String
+}
