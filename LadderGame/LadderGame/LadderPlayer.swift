cf8b196ff8fa3e04fb2c63c2bde350cf347ef7e1
cmindy <minjidev@naver.com>
2019-04-15 15:49:00 +0900

    refactor: Add LadderPlayer struct to express ladder player

---
+++ LadderGame/LadderGame/LadderPlayer.swift
@@ -0,0 +1,14 @@
+//
+//  LadderPlayer.swift
+//  LadderGame
+//
+//  Created by CHOMINJI on 15/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+
+struct LadderPlayer {
+    let name: String
+}
