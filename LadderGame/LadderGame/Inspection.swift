28abc706592ac10607f31a1822978758d348a57f
kcushy <dev.kcushy@gmail.com>
2019-04-15 17:21:45 +0900

    fix: Change a parameter

--- LadderGame/LadderGame/Inspection.swift
+++ LadderGame/LadderGame/Inspection.swift
@@ -53,8 +53,8 @@ struct Inspection {
              *e.g) ["hngfu", "justin", "NORI"]*
      
     */
-    func meetLength(of players: LadderPlayer) throws {
-        let isExceed = players.names.allSatisfy({ $0.trimmingCharacters(in: .whitespacesAndNewlines).count <= 5 })
+    func meetLength(of players: [LadderPlayer]) throws {
+        let isExceed = players.allSatisfy({ $0.name.trimmingCharacters(in: .whitespacesAndNewlines).count <= 5 })
         guard isExceed else {
             throw inputError.exceedLength
         }
@@ -75,13 +75,13 @@ struct Inspection {
      - returns:
      A passed incoming value.
     */
-    func meetMinimum(of players: LadderPlayer, of ladder: String) throws -> (names: LadderPlayer, ladder: Int) {
+    func meetMinimum(of players: [LadderPlayer], of ladder: String) throws -> (names: [LadderPlayer], ladder: Int) {
         
         guard let height = Int(ladder) else {
             throw inputError.wrongValue
         }
 
-        let filteredNames = players.names.filter { !($0.isEmpty) }
+        let filteredNames = players.filter{ !($0.name.isEmpty) }
         guard filteredNames.count >= 1 && height >= 1 else {
             throw inputError.lackValue
         }
