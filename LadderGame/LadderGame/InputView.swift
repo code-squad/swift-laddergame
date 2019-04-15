b0c46b0bf5bc7163c25d27ce6492d36191a4480b
kiljh <kiljh@me.com>
2019-04-15 17:41:59 +0900

    InputView 구조체 수정
    InputView 구조체 내에서 반복문 돌 던 것 main.swift 에서 돌도록 수정
    input 함수 나눠져있던 것 합침

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -24,45 +24,30 @@ struct InputView {
     var players = [LadderPlayer]()
     var maximumHeight = 0
     
-    /// 입력받는 함수 메인
-    mutating func input () -> ([LadderPlayer], Int) {
-        while true {
-            do {
-                players = try inputFromUserToPlayerNames(InputableVariableName.players)
-                maximumHeight = try inputFromUserToMaximumHeight(InputableVariableName.maximumHeight)
-                break
-            } catch LadderGameErrors.lessNumber {
-                print("참여할 사람과 최대 사다리 높이는 최소 2 이상이어야 합니다.")
-            } catch LadderGameErrors.toLongName {
-                print("사람의 이름은 최대 영문 5자입니다.")
-            } catch {
-                print("알 수 없는 오류입니다")
-            }
-        }
-        
-        return (players, maximumHeight)
+    /// 입력받는 함수
+    private func inputFromUser (_ variableName: InputableVariableName) -> (String) {
+        print(variableName.rawValue)
+        let valueEntered = readLine() ?? "0"
+        return valueEntered
     }
 
     /// 사용자로부터 최대 사다리 높이를 입력받는 함수
-    private func inputFromUserToMaximumHeight (_ variableName: InputableVariableName) throws -> (Int) {
-        print(variableName.rawValue)
-        guard let numberEnterd = Int(readLine() ?? "0"), numberEnterd > 1 else {
+    mutating func inputMaximumHeight () throws -> () {
+        let valueEntered = inputFromUser(InputableVariableName.maximumHeight)
+        guard let numberEnterd = Int(valueEntered), numberEnterd > 1 else {
             throw LadderGameErrors.lessNumber
         }
-        return numberEnterd
+        maximumHeight = numberEnterd
     }
 
     /// 사용자로부터 이름을 입력받는 함수
-    private func inputFromUserToPlayerNames (_ variableName: InputableVariableName) throws -> [LadderPlayer] {
-        var playerArray = [LadderPlayer]()
-        print(variableName.rawValue)
-        let namesEnterd = readLine() ?? "0"
-        let names = namesEnterd.split(separator: ",")
+    mutating func inputPlayerNames () throws -> () {
+        let valueEntered = inputFromUser(InputableVariableName.players)
+        let names = valueEntered.split(separator: ",")
         if names.count < 2 { throw LadderGameErrors.lessNumber }
         for name in names {
             if name.count > 5 { throw LadderGameErrors.toLongName }
-            playerArray.append(LadderPlayer(playerName: String(name)))
+            players.append(LadderPlayer(playerName: String(name)))
         }
-        return playerArray
     }
 }
--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,13 +8,35 @@
 
 import Foundation
 
-var inputView: InputView = InputView()
-inputView.input()
-let resultView: ResultView = ResultView()
-var ladderGame: LadderGame = LadderGame(inputView.maximumHeight, inputView.players)
-ladderGame.makeLadder()
-resultView.printLadder(inputView.players, ladderGame.ladderBoard)
+func inputRoop () -> InputView {
+    var inputView: InputView = InputView()
+    
+    while true {
+        do {
+            try inputView.inputPlayerNames()
+            try inputView.inputMaximumHeight()
+            break
+        }
+        catch LadderGameErrors.lessNumber {
+            print("참여할 사람과 최대 사다리 높이는 최소 2 이상이어야합니다.")
+        }
+        catch LadderGameErrors.toLongName {
+            print("참가자 이름은 최대 영문 5글자입니다.")
+        }
+        catch {
+            print("알 수 없는 오류입니다")
+        }
+    }
+    
+    return inputView
+}
 
+func main () {
+    let inputView = inputRoop()
+    let resultView: ResultView = ResultView()
+    var ladderGame: LadderGame = LadderGame(inputView.maximumHeight, inputView.players)
+    ladderGame.makeLadder()
+    resultView.printLadder(inputView.players, ladderGame.ladderBoard)
+}
 
-
-
+main ()
