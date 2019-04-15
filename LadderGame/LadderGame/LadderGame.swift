369be477209b2509e0a4716fb18b9d904ffbaa16
Daheen Lee <allwhite.dev@gmail.com>
2019-04-15 18:11:33 +0900

    refactor: Define type Ladder with two-dimensional LadderStep array

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 
+typealias Ladder = [[LadderStep]]
+
 struct LadderGame {
     var height: Int
     var players: [LadderPlayer]
-    var board: [[LadderStep]] {
+    var ladder: Ladder {
         get {
-            return buildBoard()
+            return buildLadder()
         }
     }
     
@@ -22,16 +24,16 @@ struct LadderGame {
         self.players = playerNames.map{ (name) in LadderPlayer(of: name) }
     }
     
-    func buildBoard() -> [[LadderStep]]{
-        var board = Array(repeating: [LadderStep](), count: height)
+    private func buildLadder() -> Ladder {
+        var ladder = Array(repeating: [LadderStep](), count: height)
         
-        for i in 0..<board.count {
-            board[i] = createStepsInRow(for: players.count)
+        for i in 0..<ladder.count {
+            ladder[i] = createStepsInRow(for: players.count)
         }
-        return board
+        return ladder
     }
     
-    // create steps in one row with true or false, and return it
+    // create steps in one row
     private func createStepsInRow(for numberOfPlayer: Int) -> [LadderStep] {
         let stepTypes: [Bool] = [true, false]
         let rowSize = numberOfPlayer - 1
