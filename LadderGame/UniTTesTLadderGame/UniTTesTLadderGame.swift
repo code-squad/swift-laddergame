1b56072aac3336e4af6c49d5feb9a11c76c7c879
kcushy <dev.kcushy@gmail.com>
2019-04-15 12:26:06 +0900

    feat: Add a unit test method

--- LadderGame/UniTTesTLadderGame/UniTTesTLadderGame.swift
+++ LadderGame/UniTTesTLadderGame/UniTTesTLadderGame.swift
@@ -24,6 +24,7 @@ class UnitTestLadderGame: XCTestCase {
         super.setUp()
         names = ["hngfu", "nori", "drake", "cony"]
         ladderPlayer = LadderPlayer(names: names)
+        ladderGame = LadderGame(people: ladderPlayer, height: 5)
         inspection = Inspection()
         notInt = "five"
         notEnoughHeight = "0"
@@ -66,5 +67,14 @@ class UnitTestLadderGame: XCTestCase {
     func testHeightHasNotEnough() {
         XCTAssertThrowsError(try inspection.meetMinimum(of: ladderPlayer, of: notEnoughHeight), "There is not enough ladder, but nothing happend") { (error) in
             XCTAssertEqual(error as? inputError, inputError.lackValue)}
+    }
+
+    func testConsecutiveLineHasNot() {
+        let ladders = ladderGame.FullLadder()
+        var isConsecutive = false
+        for laddersPart in ladders {
+            isConsecutive = inspection.verifyChain(from: laddersPart)
+        }
+        XCTAssertEqual(isConsecutive, false)
     }
 }
