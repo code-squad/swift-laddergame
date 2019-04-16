9e99565b0da6345c330a3c43299e2b6f06f43c19
gukjin jang <unknown>
2019-04-16 19:57:30 +0900

    Modify : testLadderStep()과 testLadderGame()을 각각 한가지 경우의 수만 테스트하도록 함수를 새로 생성 및 LadderPlayer 테스트 추가

--- LadderGame/LadderGameTest/LadderGameTest.swift
+++ LadderGame/LadderGameTest/LadderGameTest.swift
@@ -9,41 +9,6 @@
 import XCTest
 
 class LadderGameTest: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    /// 한층의 사다리를 만드는 함수 테스트
-    func testLadderStep(){
-        let element = [LadderPlayer(player: "aiden"),LadderPlayer(player: "lily")]
-        let ladderStep = LadderStep()
-        XCTAssertEqual(ladderStep.makeLayerOfLadder(players: element).count, 1)
-        XCTAssertTrue(ladderStep.makeLayerOfLadder(players: element) is [Bool])
-    }
-    /// 사다리의 전체 층을 만드는 함수 테스트
-    func testLadderGame(){
-        let element = [LadderPlayer(player: "aiden"),LadderPlayer(player: "lily")]
-        let ladderGame = LadderGame(players: element, ladderHeight: 3)
-        XCTAssertEqual(ladderGame.makeLadder(ladderInfo: ladderGame).count, 3)
-        XCTAssertTrue(ladderGame.makeLadder(ladderInfo: ladderGame) is [[Bool]])
-        XCTAssertEqual(ladderGame.makeLadder(ladderInfo: ladderGame)[0].count, 1)
-    }
     /// 플레이어의 이름을 정제해주는 함수 테스트
     func testAlignName(){
         let resultView = ResultView()
@@ -54,4 +19,27 @@ class LadderGameTest: XCTestCase {
         let inputView = InputView()
         XCTAssertThrowsError(try inputView.errorJudgmentName(invertStringNames: "helloworld"))
     }
+    
+    /// 리턴된 타입 테스트
+    func testCorrectType(){
+        let element = [LadderPlayer(player: "aiden"),LadderPlayer(player: "lily")]
+        let ladderPlayer = LadderPlayer(player: "aiden")
+        let ladderStep = LadderStep()
+        let ladderGame = LadderGame(players: element, ladderHeight: 3)
+        
+        XCTAssertTrue(ladderPlayer is LadderPlayer)
+        XCTAssertTrue(ladderStep.makeLayerOfLadder(players: element) is [Bool])
+        XCTAssertTrue(ladderGame.makeLadder(ladderInfo: ladderGame) is [[Bool]])
+    }
+    /// 사람에 따른 사다리의 가로 간격 갯수와 높이를 테스트
+    func testPlayerAndHeightCount(){
+        let element = [LadderPlayer(player: "aiden"),LadderPlayer(player: "lily")]
+        let ladderStep = LadderStep()
+        let ladderGame = LadderGame(players: element, ladderHeight: 3)
+        
+        XCTAssertEqual(ladderStep.makeLayerOfLadder(players: element).count, 1)
+        XCTAssertEqual(ladderGame.makeLadder(ladderInfo: ladderGame).count, 3)
+        XCTAssertEqual(ladderGame.makeLadder(ladderInfo: ladderGame)[0].count, 1)
+    }
+    ///
 }
