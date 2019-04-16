c332bec7ef4c6a15733f47bf5c8e9329a6c9b6dc
dongY <ldcpaul@hanmail.net>
2019-04-16 17:26:22 +0900

    errorHandler / ErrorType 열거형 정의

--- LadderGame/LadderGame/Util.swift
+++ LadderGame/LadderGame/Util.swift
@@ -8,5 +8,15 @@
 
 import Foundation
 
+enum ErrorType:String,Error{
+    case wrongInput = "입력형식이 맞지않습니다"
+    case outOfRange = "숫자범위가 벗어났습니다"
+}
+struct ErrorHandler{
+    func alertErrorMessage(type:ErrorType){
+        print("             ❗️\(type.rawValue)❗️")
+        print("====================================================")
+    }
+}
 
 
