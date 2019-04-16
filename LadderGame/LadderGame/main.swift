bf3ef1edc4c1d768ff88ec4733f7ed287c7fa6f0
Unknown <yago0315@gmail.com>
2019-04-16 15:49:43 +0900

    main() 수정

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,84 +8,21 @@
 
 import Foundation
 
-enum LadderComponent: String {
-    case empty = " "
-    case rung = "-"
-    case verticalComponent = "|"
-}
-
-///사다리 생성 함수
-func createLadder(participantCount: Int, ladderHeight: Int) -> [[Bool]] {
-    let columnSize = participantCount - 1
-    let ladder = Array(repeating: Array(repeating: false, count: columnSize), count: ladderHeight)
-    
-    return ladder
-}
-
-///규칙에 따라 사다리 검증 후 랜덤 값 반환
-func randomComponent(row: [Bool], columnNumber: Int) -> Bool {
-    if columnNumber == 0 {
-        return Bool.random()
-    }
-    
-    //이전 값이 true일 경우 false 반환
-    if row[columnNumber - 1] == true {
-        return false
-    }
-    
-    return Bool.random()
-}
-
-///사다리 행 구성 함수
-func configureLadderRow(row: inout [Bool]) {
-    for columnNumber in 0..<row.count {
-        row[columnNumber] = randomComponent(row: row, columnNumber: columnNumber)
-    }
-}
-
-///사다리 구성 함수
-func configureLadder(_ ladder: inout [[Bool]]) {
-    for rowNumber in 0..<ladder.count {
-        configureLadderRow(row: &ladder[rowNumber])
-    }
-}
-
-///true, false에 따라 사다리 부품 구별
-func distinguishComponent(columnNumber: Int, columnValue: Bool) -> LadderComponent {
-    if columnValue {
-        return .rung
-    }
-    
-    return .empty
-}
-
-///사다리 행 문자열로 변환
-func convertLadderRow(_ row: [Bool]) -> String {
-    var rowText = ""
-    
-    for (columnNumber, columnValue) in row.enumerated() {
-        rowText.append(LadderComponent.verticalComponent.rawValue)
-        rowText.append(distinguishComponent(columnNumber: columnNumber, columnValue: columnValue).rawValue)
-    }
-    
-    rowText.append(LadderComponent.verticalComponent.rawValue)
-    
-    return rowText
-}
-
-///사다리 출력 함수
-func printLadder(_ ladder: [[Bool]]) {
-    for row in ladder {
-        print(convertLadderRow(row))
-    }
-}
-
 ///시작 함수
-func main() {
-    guard let userInput = getUserInput() else { return }
-    var ladder = createLadder(participantCount: userInput.participantCount, ladderHeight: userInput.ladderHeight)
-    configureLadder(&ladder)
-    printLadder(ladder)
+func main() throws {
+    let userInput = try InputView()
+    let ladder = LadderGame(players: userInput.players, height: userInput.ladderHeight).ladder
+    ResultView().printLadder(ladder: ladder, players: userInput.players)
 }
 
-main()
+do {
+    try main()
+} catch InputError.invalidInput {
+    print("입력 오류")
+} catch InputError.invalidPlayerCount {
+    print("사람 수 오류")
+} catch InputError.invalidPlayerName {
+    print("사람 이름 오류")
+} catch InputError.invalidLadderHeight {
+    print("사다리 높이 오류")
+}
