e0cecaa8f5ef3cd79e0aafa66919a588aa6d9e49
gukjin jang <unknown>
2019-04-16 17:14:25 +0900

    Add : ResultView구조체 내에 있는 플레이어의 이름을 5글자로 정제해주는 함수 alignName() 테스트 생성

--- LadderGame/LadderGameTest/LadderGameTest.swift
+++ LadderGame/LadderGameTest/LadderGameTest.swift
@@ -29,19 +29,24 @@ class LadderGameTest: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
+    /// 한층의 사다리를 만드는 함수 테스트
     func testLadderStep(){
         let element = [LadderPlayer(player: "aiden"),LadderPlayer(player: "lily")]
         let ladderStep = LadderStep()
         XCTAssertEqual(ladderStep.makeLayerOfLadder(players: element).count, 1)
         XCTAssertTrue(ladderStep.makeLayerOfLadder(players: element) is [Bool])
     }
-    
+    /// 사다리의 전체 층을 만드는 함수 테스트
     func testLadderGame(){
         let element = [LadderPlayer(player: "aiden"),LadderPlayer(player: "lily")]
         let ladderGame = LadderGame(players: element, ladderHeight: 3)
         XCTAssertEqual(ladderGame.makeLadder(ladderInfo: ladderGame).count, 3)
         XCTAssertTrue(ladderGame.makeLadder(ladderInfo: ladderGame) is [[Bool]])
         XCTAssertEqual(ladderGame.makeLadder(ladderInfo: ladderGame)[0].count, 1)
+    }
+    /// 플레이어의 이름을 정제해주는 함수 테스트
+    func testAlignName(){
+        let resultView = ResultView()
+        XCTAssertTrue(resultView.alignName(beforeAlignName: LadderPlayer(player: "lily")).count == 5)
     }
 }
