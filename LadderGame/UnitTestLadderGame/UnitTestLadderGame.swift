bdd2e5bc1fa7cd591a20fd7d66fd1a4509043622
kiljh <kiljh@me.com>
2019-04-16 17:45:22 +0900

    UnitTestLadderGame 유닛 테스트 구현
    LadderMake 함수를 테스트하는 함수 구현

--- LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -8,26 +8,44 @@
 
 import XCTest
 
-class UnitTestLadderGame: XCTestCase {
 
+class UnitTestLadderGame: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    ///
+    func testMakeLadder() {
+        var players: [LadderPlayer] = [LadderPlayer]()
+        let height = 5
+        var prev = false
+        players.append(LadderPlayer(playerName: "jk"))
+        players.append(LadderPlayer(playerName: "doran"))
+        players.append(LadderPlayer(playerName: "aiden"))
+        var ladderGame = LadderGame(height, players)
+        ladderGame.makeLadder()
+        XCTAssertEqual(ladderGame.ladderBoard.count, height)
+        for heightIndex in 0..<height {
+            XCTAssertEqual(ladderGame.ladderBoard[heightIndex].count, players.count)
+            prev = false
+            for playerIndex in 0..<players.count {
+                if prev {
+                    XCTAssertFalse(ladderGame.ladderBoard[heightIndex][playerIndex])
+                }
+                prev = ladderGame.ladderBoard[heightIndex][playerIndex]
+            }
         }
+        XCTAssertTrue(ladderGame.ladderBoard is [[Bool]])
     }
-
+    
 }
