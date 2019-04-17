20faaa43a96936c6ef14c120d1f7d6159960d0f3
kiljh <kiljh@me.com>
2019-04-17 10:55:39 +0900

    LadderGame Init 테스트 추가

--- LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -24,12 +24,30 @@ class UnitTestLadderGame: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    ///
+    /// LadderPlayer Init test
     func testInitLadderPlayer () {
         do {
             XCTAssertNoThrow(try LadderPlayer(playerName: "doran"))
             XCTAssertThrowsError(try LadderPlayer(playerName: "dominic"), "이름은 최대 영문 5글자이므로 7글자일 때 throwError를 반환해야한다.")
         }
+    }
+    
+    /// LadderGame Init test
+    func testInitLadderGame () {
+        var players: [LadderPlayer] = [LadderPlayer]()
+        let height = 5
+        
+        do {
+            players.append(try LadderPlayer(playerName: "jk"))
+            players.append(try LadderPlayer(playerName: "doran"))
+            players.append(try LadderPlayer(playerName: "aiden"))
+        }
+        catch {
+            print("참가자 이름은 최대 영문 5글자입니다.")
+        }
+        
+        let ladderGame = LadderGame(height, players)
+        XCTAssertNotNil(ladderGame)
     }
     
     func testMakeLadder () {
