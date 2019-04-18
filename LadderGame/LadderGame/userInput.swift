c22c1cda699e5540539244ae8526bf70c4bc7cfa
chan <clown4014@gmail.com>
2019-04-18 12:52:35 +0900

    유저의 입력을 저장하는 프로퍼티 생성

---
+++ LadderGame/LadderGame/userInput.swift
@@ -0,0 +1,15 @@
+//
+//  userInput.swift
+//  LadderGame
+//
+//  Created by 이희찬 on 18/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct UserInputManage{
+    var requestNumber : String? {
+        return readLine()
+    }
+}
