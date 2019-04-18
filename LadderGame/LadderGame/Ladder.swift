5820fbffe7e6b4a3e0d758799b364746ad220146
부엉이 <ldcpaul@hanmail.net>
2019-04-18 16:52:00 +0900

    Ladder 구조체 정의

---
+++ LadderGame/LadderGame/Ladder.swift
@@ -0,0 +1,14 @@
+//
+//  Ladder.swift
+//  LadderGame
+//
+//  Created by 이동영 on 18/04/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Ladder {
+    private let rows :[LadderRow]
+    public getLadder()->[LadderRow]{return self.ladder}
+}
