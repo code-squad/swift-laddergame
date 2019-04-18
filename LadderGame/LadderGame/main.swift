c16b00c8b1ddccc3ba4bcfb304fc7f3adbb5d705
junyng <pom0894@naver.com>
2019-04-18 17:51:26 +0900

    게임 시작 함수 주석 수정, 게임 설정을 위한 입력 함수 조건 및 에러 발생 추가, 게임 실행 함수 추가

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -21,11 +21,11 @@ enum LadderPart: Character {
     }
 }
 
-/// 게임 실행에 필요한 유저 숫자와 사다리 높이를 설정합니다.
+/// 게임 시작에 필요한 유저 숫자와 사다리 높이를 설정합니다.
 /// - returns: 유저 숫자와 사다리 높이를 포함한 튜플
-func setupGame() -> Settings {
-    let userNumber = try? setUserNumber()
-    let ladderHeight = try? setLadderHeight()
+func setupGame() throws -> Settings {
+    let userNumber = try setUserNumber()
+    let ladderHeight = try setLadderHeight()
     
     return (userNumber, ladderHeight)
 }
@@ -59,7 +59,7 @@ func endGame(_ result: [[Bool]]) {
 ///     - 입력을 정수형으로 형변환 실패시 Nil
 func setUserNumber() throws -> Int {
     print("참여할 사람은 몇 명 인가요?")
-    guard let input = readLine() else {
+    guard let input = readLine(), !input.isEmpty else {
         throw InputError.isEmpty
     }
     
@@ -76,7 +76,7 @@ func setUserNumber() throws -> Int {
 ///     - 입력을 정수형으로 형변환 실패시 Nil
 func setLadderHeight() throws -> Int {
     print("최대 사다리 높이는 몇 개인가요?")
-    guard let input = readLine() else {
+    guard let input = readLine(), !input.isEmpty else {
         throw InputError.isEmpty
     }
     
@@ -128,11 +128,16 @@ func getLayer(_ parts: [Bool]) -> String {
     return layer
 }
 
-// 게임 실행
-do {
-    let settings = setupGame()
+/// 게임을 실행 합니다.
+func playGame() throws {
+    let settings = try setupGame()
     let result = try startGame(settings)
+    
     endGame(result)
+}
+
+do {
+    try playGame()
 } catch InputError.isEmpty {
     print("입력이 정의되지 않았습니다.")
 } catch InputError.notANumber {
