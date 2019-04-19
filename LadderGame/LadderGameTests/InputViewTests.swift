d588dedb82f188bd8cd3436f24aaf38dbfede09e
Daheen Lee <allwhite.dev@gmail.com>
2019-04-19 15:29:43 +0900

    test: Add testInvalidEmptyNameArray()

--- LadderGame/LadderGameTests/InputViewTests.swift
+++ LadderGame/LadderGameTests/InputViewTests.swift
@@ -18,10 +18,12 @@ class InputViewTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInvalidEmptyNameArray() {
+        let emtpyName = [String]()
+        XCTAssertTrue(InputView.areNamesInvalidForGame(namesOfPlayer: emtpyName), "빈 배열은 invalid로 판별해야 함")
     }
+    
+    
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
