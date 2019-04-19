a3d10b92097eea49f6d5444243d6518d8a711e9e
박준희 <junibibi04@gmail.com>
2019-04-19 15:59:53 +0900

    함수의 이름과 변수명을 좀더 구제척인 표현으로 수정.
    함수의 이름과 변수명을 수정하였습니다.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 //유저가 입력하는 사람수와 사다리수를 받는 함수
-func inputUserPeopleAndLadderCounts() -> (Int, Int){
+func inputUserPeopleAndLadderCounts() -> (Int, Int) {
     print("참여할 사람은 몇 명 인가요?")
     let inputPeopleCount = readLine()!
     let peopleCount = Int(inputPeopleCount)!
@@ -25,71 +25,71 @@ func inputUserPeopleAndLadderCounts() -> (Int, Int){
 }
 
 
-//중복값을 체크해서 사다리만들 값을 저장하는 이중배열만드는 함수
-func hasHorizontalValues(peopleNum: Int, ladderNum: Int)->[[Bool]]{
+//중복값을 체크해서 대상이 되는 값을 저장하는 이중배열만드는 함수
+func repeatMadeSubjects(peopleNum: Int, ladderNum: Int) -> [[Bool]]{
     let ladderWidth = peopleNum
     let ladderHeight = ladderNum
     
-    var horizontalValues : [[Bool]] = Array(repeating: Array(repeating: true,count: ladderWidth-1 ), count: ladderHeight)
+    var horizontalSubjects: [[Bool]] = Array(repeating: Array(repeating: true, count: ladderWidth-1 ), count: ladderHeight)
     
-    for horizontalValuesIndex in 0 ..< ladderHeight {
-        horizontalValues[horizontalValuesIndex] = makeOneHorizontalValues(checkValues: horizontalValues[horizontalValuesIndex])
+    for horizontalSubjectsIndex in 0 ..< ladderHeight {
+        horizontalSubjects[horizontalSubjectsIndex] = makeSubjects(checkSubjects: horizontalSubjects[horizontalSubjectsIndex])
     }
-    return horizontalValues
+    return horizontalSubjects
 }
 
 
-//중복체크된 값으로 1행 만드는 함수
-func makeOneHorizontalValues (checkValues: [Bool]) -> [Bool] {
-    var changeValues = checkValues
-    for index in 0..<changeValues.count {
-        changeValues[index] = checkValue(target: changeValues, checkIndex: index)
+//중복체크된 값을 가지고 배열을 만들어 주는 함수.
+func makeSubjects(checkSubjects: [Bool]) -> [Bool] {
+    var changeSubjects = checkSubjects
+    for index in 0 ..< changeSubjects.count {
+        changeSubjects[index] = checkRepetition(target: changeSubjects, checkIndex: index)
     }
-    return changeValues
+    return changeSubjects
 }
-
-
 
 
 //true뒤에 true가 올 수 없게 체크하는 함수.(- -중복체크)
-func checkValue (target: [Bool], checkIndex: Int) -> Bool {
+func checkRepetition(target: [Bool], checkIndex: Int) -> Bool {
     var checkTarget = target
     if checkIndex > 0 && checkTarget[checkIndex - 1] == true {
         return false
     }
     return Bool.random()
 }
-    
+
+
+
 
 //사다리의 1가로줄만드는걸 가지고 사다리높이만큼 반복하는 함수
-func horizontalValuesChangeHorizontalLadders(changValues: [[Bool]])->[[String]]{
-    let ladderWidth = changValues[0].count
-    let ladderHeight = changValues.count
+func horizontalSubjectsChangeHorizontalLadders(changeSubjects: [[Bool]]) -> [[String]]{
+    let ladderWidth = changeSubjects[0].count
+    let ladderHeight = changeSubjects.count
     
     
-    var horizontalLadders : [[String]] = Array(repeating: Array(repeating: "-", count: ladderWidth), count: ladderHeight)
+    var horizontalLadders: [[String]] = Array(repeating: Array(repeating: "-", count: ladderWidth), count: ladderHeight)
     
-    for horizontalLaddersIndex in 0..<ladderHeight {
-        horizontalLadders[horizontalLaddersIndex] = makeOneHorizontalLadders(Values: changValues[horizontalLaddersIndex], ladders: horizontalLadders[horizontalLaddersIndex])
+    for horizontalLaddersIndex in 0 ..< ladderHeight {
+        horizontalLadders[horizontalLaddersIndex] = makeOneHorizontalLadders(subjects: changeSubjects[horizontalLaddersIndex], ladders: horizontalLadders[horizontalLaddersIndex])
     }
     return horizontalLadders
 }
 
 
 //사다리의 1가로줄을 만드는 함수
-func makeOneHorizontalLadders (Values: [Bool],ladders: [String]) -> [String] {
-    var existingValues = Values
+func makeOneHorizontalLadders(subjects: [Bool],ladders: [String]) -> [String] {
+    var existingSubjects = subjects
     var changeladders = ladders
-    for index in 0..<existingValues.count {
-        changeladders[index] = changeValue(targetValues: existingValues, changeTargetIndex: index)
+    for index in 0 ..< existingSubjects.count {
+        changeladders[index] = subjectChangeLadder(target: existingSubjects, changeTargetIndex: index)
     }
     return changeladders
 }
 
 
 //Bool값을 사다리모양으로 바꾸는 함수
-func changeValue (targetValues: [Bool],changeTargetIndex: Int)->String {
-    var changeTarget = targetValues
+func subjectChangeLadder(target: [Bool],changeTargetIndex: Int) -> String {
+    var changeTarget = target
     
     if changeTarget[changeTargetIndex] == true {
         return "-"
@@ -99,14 +99,12 @@ func changeValue (targetValues: [Bool],changeTargetIndex: Int)->String {
 
 
 
-
-
 //배열에있는 그림으로 사다리1행 만드는 함수
-func makeLadderOneLine (lineCount : Int, ladderValues : [[String]]) {
+func makeLadderOneLine(lineCount: Int, ladderValues: [[String]]) {
     let ladderWidth = ladderValues[0].count
     
     var verticalLine = "|"
-    for oneLineladderCount in 0..<ladderWidth {
+    for oneLineladderCount in 0 ..< ladderWidth {
         verticalLine = verticalLine + ladderValues[lineCount][oneLineladderCount]
         verticalLine = verticalLine + "|"
     }
@@ -116,10 +114,10 @@ func makeLadderOneLine (lineCount : Int, ladderValues : [[String]]) {
 
 
 //사다리높이만큼 사다리그림을 1행씩 증가시키는 함수
-func increaseByladderLine (ladders: [[String]]) {
+func increaseByladderLine(ladders: [[String]]) {
     let ladderHeight = ladders.count
     
-    for ladderHeightCount in 0..<ladderHeight {
+    for ladderHeightCount in 0 ..< ladderHeight {
         makeLadderOneLine(lineCount: ladderHeightCount, ladderValues: ladders)
     }
 }
@@ -127,8 +125,8 @@ func increaseByladderLine (ladders: [[String]]) {
 //게임 실행하는 함수
 func playGame() {
     let (peopleNum, ladderNum) = inputUserPeopleAndLadderCounts()
-    let values = hasHorizontalValues(peopleNum: peopleNum, ladderNum: ladderNum)
-    let ladders = horizontalValuesChangeHorizontalLadders(changValues: values)
+    let subjects = repeatMadeSubjects(peopleNum: peopleNum, ladderNum: ladderNum)
+    let ladders = horizontalSubjectsChangeHorizontalLadders(changeSubjects: subjects)
     increaseByladderLine(ladders: ladders)
 }
 
