14c8dc116fb572093c87b51727bda084e6e2a888
부엉이 <ldcpaul@hanmail.net>
2019-04-19 19:01:22 +0900

    플레이어 이름 규격화 범위 자세하게하기위해 축소

--- LadderGame/LadderGameTests/LadderGameTests.swift
+++ LadderGame/LadderGameTests/LadderGameTests.swift
@@ -10,30 +10,24 @@ import XCTest
 
 
 class LadderGameTests: XCTestCase {
-
-    var players : [LadderPlayer] = [LadderPlayer]()
-    var ladder : Ladder?
-    var heightOfLadder : Int = 10
+    
+    
+    //var ladder : Ladder?
+    //var heightOfLadder : Int = 10
     
     override func setUp()  {
-        do{
-            players.append(try LadderPlayer.init(name: "Swift"))
-            players.append(try LadderPlayer.init(name: "Java"))
-            players.append(try LadderPlayer.init(name: "C++"))
-            players.append(try LadderPlayer.init(name: "HTML"))
-            ladder = Ladder.init(height: heightOfLadder, numOfSteps: players.count-1)
-        }catch{
-            
-        }
+        // ladder = Ladder.init(height: heightOfLadder, numOfSteps: players.count-1)
     }
     
     func testPlayerNameNomalization(){
-        let filterdPlayer = self.players.filter{
-            player in
-            return player.getName().count == 6
+        
+        do{
+            let player = try LadderPlayer.init(name: "HTML")
+            XCTAssertEqual(player.getName().count,6,"이름이 6글자로 규격화 되었습니다.")
+        }catch{
+            ()
         }
-        XCTAssertEqual(players.count,filterdPlayer.count,"이름이 6글자로 규격화 되었습니다.")
-    }
+   }
     
     
     func testGetRandomSteps(){
@@ -41,5 +35,5 @@ class LadderGameTests: XCTestCase {
         
         XCTAssertNotEqual(exist.generateAfter(), StepType.exist, "기존에 발판이 존재하면 빈 발판을 반환하지 않습니다.")
     }
-
+    
 }
