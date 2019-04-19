178124c61bcf9215c9fed6700fce782c45056da1
부엉이 <ldcpaul@hanmail.net>
2019-04-19 17:00:43 +0900

    test 이전 setUp 함수 추가

--- LadderGame/LadderGameTests/LadderGameTests.swift
+++ LadderGame/LadderGameTests/LadderGameTests.swift
@@ -11,6 +11,20 @@ import XCTest
 
 class LadderGameTests: XCTestCase {
 
-   
+    var players : [LadderPlayer] = [LadderPlayer]()
+    var ladder : Ladder?
+    var heightOfLadder : Int = 10
+    
+    override func setUp()  {
+        do{
+            players.append(try LadderPlayer.init(name: "Swift"))
+            players.append(try LadderPlayer.init(name: "Java"))
+            players.append(try LadderPlayer.init(name: "C++"))
+            players.append(try LadderPlayer.init(name: "HTML"))
+            ladder = Ladder.init(height: heightOfLadder, numOfSteps: players.count-1)
+        }catch{
+            
+        }
+    }
 
 }
