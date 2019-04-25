4f1cf78ee808215345185aea400b3c1e93331c38
junyng <pom0894@naver.com>
2019-04-25 11:58:57 +0900

    게임 참가자들의 문자열 리턴 함수 및 사다리 문자열 리턴 함수 출력 함수로 변경, 사다리 층을 가져오는 함수명 변경

--- LadderGame/LadderGame/ResultView.swift
+++ LadderGame/LadderGame/ResultView.swift
@@ -14,33 +14,33 @@ struct ResultView {
     ///
     /// - Parameter ladderGame: 사다리 게임 구조체
     func draw(_ ladderGame: LadderGame) {
-        let ladder = getLadder(ladderGame.start())
-        let players = getNames(ladderGame.players)
-        
-        print("\(ladder)\n\(players)")
+        printLadder(ladderGame.start())
+        printNames(ladderGame.players)
     }
     
     /// 게임 참가자들의 이름을 가져옵니다.
     ///
     /// - Parameter players: 사다리 게임 참가자들
     /// - Returns: 게임 참가자들의 이름 문자열
-    private func getNames(_ players: [LadderPlayer]) -> String {
-        return players.map { $0.name.padding(toLength: 6, withPad: " ", startingAt: 0) }.joined()
+    private func printNames(_ players: [LadderPlayer]) {
+        let names = players.map { $0.name.padding(toLength: 6, withPad: " ", startingAt: 0) }.joined()
+        print(names, terminator: "")
     }
     
     /// 사다리를 나타내는 문자열을 가져옵니다.
     ///
     /// - Parameter layers: bool 타입 배열을 갖는 2차원 배열
     /// - Returns: 사다리를 나타내는 문자열
-    private func getLadder(_ layers: [[Bool]]) -> String {
-        return layers.map { getLayer($0) }.joined(separator: "\n")
+    private func printLadder(_ layers: [[Bool]]) {
+        let ladder = layers.map { layer($0) }.joined(separator: "\n")
+        print(ladder)
     }
     
     /// 사다리 층을 나타내는 문자열을 가져옵니다.
     ///
     /// - Parameter steps: bool 타입 배열
     /// - Returns: 사다리 층을 나타내는 문자열
-    private func getLayer(_ steps: [Bool]) -> String {
+    private func layer(_ steps: [Bool]) -> String {
         let rung = "|"
         let joined = steps.map { LadderStep($0).type.rawValue.description }.joined(separator: rung)
         let layer = "\(rung)\(joined)\(rung)"
