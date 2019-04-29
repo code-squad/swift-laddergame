f5b2ac86cb1a3696ce9ad2da3b3324587eea8ad0
chan <clown4014@gmail.com>
2019-04-29 14:52:24 +0900

    하나의 함수가 하나의 역할만을 하게 수정 반복은 상위 함수에서 하는 것으로 수정

--- LadderGame/LadderGame/userInput.swift
+++ LadderGame/LadderGame/userInput.swift
@@ -34,8 +34,6 @@ struct UserInput{
     }
 
     mutating func isString()->Bool{
-        let optionalString = requestNumber
-        convertedString = removeOptional(optionalString)
         if convertedString == "removeOptional fail" {
             return false
         }
@@ -43,35 +41,10 @@ struct UserInput{
     }
     
     mutating func isInt ()->Bool{
-        convertedInt = convertInt(convertedString)
         if convertedInt == 0 {
             return false
         }
         return true
-    }
-    
-    mutating func userInputString() {
-        var isString = self.isString()
-        while !(isString) {
-            print("값을 다시 확인하고 입력하세요")
-            isString = self.isString()
-        }
-    }
-    
-    mutating func checkUserInputInt() -> Bool {
-        userInputString()
-        guard isInt() else {
-            return false
-        }
-        return true
-    }
-    
-    mutating func userInputInt() {
-        var checkUserInputInt = self.checkUserInputInt()
-        while checkUserInputInt == false {
-            print("입력한 값을 다시 확인하세요, 정수를 입력해야 합니다")
-            checkUserInputInt = self.checkUserInputInt()
-        }
     }
     
     func isOverOne()->Bool{
@@ -81,36 +54,37 @@ struct UserInput{
         return true
     }
     
-    
     let playerNumberRequestMassage = "플레이어 수"
     let ladderHeightRequestMassage = "사다리 높이"
     
-    mutating func checkNumber(_ RequestMassage : String) -> Bool {
-        print(RequestMassage + "를 입력하세요")
-        userInputInt()
-        guard isOverOne() else{
-            print("최소" + RequestMassage + "는 2입니다," + RequestMassage + "를 다시 입력해주세요")
-            return false
-        }
-        return true
+    func printRquestMassage(_ RequestMassage:String) -> (String,String) {
+        let RequestMassage = RequestMassage + "를 입력하세요"
+        let ReRequestMassage = "최소" + RequestMassage + "는 2입니다," + RequestMassage + "를 다시 입력해주세요"
+        return (RequestMassage,ReRequestMassage)
     }
-    
-    mutating func Number(_ RequestMassage : String) {
-        var number : Bool
-        repeat {
-            number = self.checkNumber(RequestMassage)
+
+    mutating func ladderBoardInitNumber(_ RequestMassage:String){
+        while isOverOne() == false {
+            let printRquestMassage = self.printRquestMassage(RequestMassage)
+            print(printRquestMassage.0)
+            convertedString = removeOptional()
+            if isString() == false {
+                print("값을 다시 확인하고 입력하세요")
+                continue
+            }
+            convertedInt = convertInt(convertedString)
+            if isInt() == false {
+                print("입력한 값을 다시 확인하세요, 정수를 입력해야 합니다")
+                continue
+            }
+            if isOverOne() == false {
+                print(printRquestMassage.1)
+                continue
+            }
         }
-            while number == false
-    }
-    
-    mutating func DrawInitMember()->(Int,Int){
-        Number(playerNumberRequestMassage)
-        let playerNumber = convertedInt
-        Number(ladderHeightRequestMassage)
-        let ladderHeight = convertedInt
-        return (playerNumber,ladderHeight)
     }
 }
+
 
 
 
