6879ac589dcd52152bddc651904c6077d5990a1c
chan <clown4014@gmail.com>
2019-04-30 03:51:54 +0900

    ladderBoardInitNumber 함수 분리

--- LadderGame/LadderGame/userInput.swift
+++ LadderGame/LadderGame/userInput.swift
@@ -57,37 +57,40 @@ struct UserInput{
     let playerNumberRequestMassage = "플레이어 수"
     let ladderHeightRequestMassage = "사다리 높이"
     
-    func printRquestMassage(_ RequestMassage:String) -> (String,String) {
-        let RequestMassage = RequestMassage + "를 입력하세요"
-        let ReRequestMassage = "최소" + RequestMassage + "는 2입니다," + RequestMassage + "를 다시 입력해주세요"
-        return (RequestMassage,ReRequestMassage)
+    func RquestMassage(_ RequestMassage:String) -> (String,String,String,String) {
+        let LadderBoardInitNumber = RequestMassage + "를 입력하세요"
+        let IsStringFalse = "알수없는 값을 입력했습니다"
+        let IsIntFalse = "정수를 입력해야 합니다"
+        let IsOverOneFalse = "최소" + RequestMassage + "는 2입니다,"
+        return (LadderBoardInitNumber,IsStringFalse,IsIntFalse,IsOverOneFalse)
     }
     
-    mutating func printErrorMassage(_ Error:Bool) {
-        if Error == false {
-            print("값을 다시 확인하고 입력하세요")
+    mutating func printErrorMassage(_ isError:Bool, _ ErrorMassage:String) {
+        if isError == false {
+            print(ErrorMassage)
         }
+    }
+    
+    mutating func requestCorrectNumber(_ RequestMassage:String)->Bool {
+        convertedInt = 0
+        let (ladderBoardInitNumber,isStringFalse,isIntFalse,isOverOneFalse) = self.RquestMassage(RequestMassage)
+        print(ladderBoardInitNumber)
+        convertedString = removeOptional()
+        let isString = self.isString()
+        printErrorMassage(isString, isStringFalse)
+        convertedInt = convertInt()
+        let isInt = self.isInt()
+        printErrorMassage(isInt, isIntFalse)
+        let isOverOne = self.isOverOne()
+        printErrorMassage(isOverOne, isOverOneFalse)
+        return (isString && isInt && isOverOne)
     }
 
     mutating func ladderBoardInitNumber(_ RequestMassage:String){
-        var isString: Bool
-        var isInt : Bool
-        var isOverOne : Bool
-        let printRquestMassage = self.printRquestMassage(RequestMassage)
+        var isCorrectNumber:Bool
         repeat {
-            convertedInt = 0
-            print(printRquestMassage.0)
-            convertedString = removeOptional()
-            isString = self.isString()
-            printErrorMassage(isString)
-            
-            convertedInt = convertInt()
-            isInt = self.isInt()
-            printErrorMassage(isInt)
-            
-            isOverOne = self.isOverOne()
-            printErrorMassage(isOverOne)
-        } while (isString && isInt && isOverOne) == false
+           isCorrectNumber = requestCorrectNumber(RequestMassage)
+        } while isCorrectNumber == false
     }
 }
 
