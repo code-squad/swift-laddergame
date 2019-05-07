4c2dad08a3e7032cd3843f61f319eec2ee02067c
박준희 <junibibi04@gmail.com>
2019-05-07 14:31:50 +0900

    단위테스트 분리
    테스트함수가 한가지 기능 단위를 테스트하도록 분리.
    ResultView단위 테스트 삭제.

--- LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
+++ LadderGame/UnitTestLadderGame/UnitTestLadderGame.swift
@@ -10,21 +10,19 @@ import XCTest
 
 class UnitTestLadderGame: XCTestCase {
     
-    //플레이어이름 오류체크(한사람일때 / 6글자넘었을때 오류발생)
-    func testShowPlayerError() {
-        XCTAssertThrowsError(try showPlayerError(chNames: ["judy"]))
-        XCTAssertThrowsError(try showPlayerError(chNames: ["ChrisHemsworth"]))
-        XCTAssertThrowsError(try showPlayerError(chNames: ["judy","ChrisHemsworth"]))
-        XCTAssertThrowsError(try showPlayerError(chNames: ["judy","young","hyunsoo"]))
-        XCTAssertThrowsError(try showPlayerError(chNames: ["judy","ChrisHemsworth","min","su"]))
-        XCTAssertThrowsError(try showPlayerError(chNames: [""]))
-        XCTAssertThrowsError(try showPlayerError(chNames: [" "]))
-        XCTAssertNoThrow(try showPlayerError(chNames: ["judy","song"]))
-        XCTAssertNoThrow(try showPlayerError(chNames: ["judy","song","min","dong","ku"]))
+    func testPlayerNameCount() {
+        XCTAssertThrowsError(try showPlayerError(chNames: ["judy","ChrisH"]))
+        XCTAssertNoThrow(try showPlayerError(chNames: ["judy","Chris"]))
     }
     
-    //사다리높이 오류체크(문자입력했을때 / 빈칸으로입력했을때->ladderNum이 0일때 오류발생)
-    func testShowLabberError() {
+    func testPlayerCount() {
+        XCTAssertThrowsError(try showPlayerError(chNames: ["judy"]))
+        XCTAssertThrowsError(try showPlayerError(chNames: [" "]))
+        XCTAssertNoThrow(try showPlayerError(chNames: ["judy","song"]))
+        XCTAssertNoThrow(try showPlayerError(chNames: ["judy","song","min"]))
+    }
+    
+    func testLadderHeigh() {
         XCTAssertThrowsError(try showLabberError(chladderNum: 0))
         XCTAssertNoThrow(try showLabberError(chladderNum: 1))
         XCTAssertNoThrow(try showLabberError(chladderNum: 6))
@@ -35,21 +33,4 @@ class UnitTestLadderGame: XCTestCase {
         XCTAssertEqual(LadderStep.makeStep(target: rawData, changeTargetIndex: 0), "-----", "step생성 실패")
         XCTAssertEqual(LadderStep.makeStep(target: rawData, changeTargetIndex: 1), "     ", "step생성 실패")
     }
-    
-    func testMakeHorizontalLadderString() {
-        var resultView = ResultView()
-        let oneLadderFloor = "|-----|     |     |-----|"
-        let showLadders = [["-----","     ","     ","-----"]]
-        resultView.makeHorizontalLadderString(ladderFloorCount: 0, showLadders: showLadders)
-        XCTAssertEqual(resultView.oneLadderFloor,oneLadderFloor,"성공")
-    }
-
-    func testShowLadders() {
-        var resultView = ResultView()
-        let ladders = [["-----","     ","     ","-----"],["     ","     ","-----","     "],["-----","     ","     ","     "]]
-        let ladderFloors = ["|-----|     |     |-----|","|     |     |-----|     |","|-----|     |     |     |"]
-        //resultView.showLadders(ladders: ladders)
-        XCTAssertEqual(resultView.showLadders(ladders: ladders),ladderFloors,"성공")
-    }
-    
 }
