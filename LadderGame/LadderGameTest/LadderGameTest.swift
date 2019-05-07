93923b73d33164b7e184adc7b8820519e69890a6
junyng <pom0894@naver.com>
2019-05-07 15:23:58 +0900

    사다리 게임 단위 테스트 추가

---
+++ LadderGame/LadderGameTest/LadderGameTest.swift
@@ -0,0 +1,13 @@
+//
+//  LadderGameTest.swift
+//  LadderGameTest
+//
+//  Created by BLU on 2019. 5. 7..
+//  Copyright © 2019년 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class LadderGameTest: XCTestCase {
+
+}
