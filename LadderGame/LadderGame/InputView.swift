3182b56281f4933f172aa413fed5dc64c23739c9
박준희 <junibibi04@gmail.com>
2019-05-08 20:30:20 +0900

    InputView 파일수정
    출력메세지와 오류정정의를 InputView파일 하나로 묶었습니다.
    main파일에서 오류캐치하고 다시 게임이 실행되도록 수정하였습니다.

--- LadderGame/LadderGame/InputView.swift
+++ LadderGame/LadderGame/InputView.swift
@@ -8,32 +8,76 @@
 
 import Foundation
 
+
+enum Question: String {
+    case people = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"
+    case ladderHight = "최대 사다리 높이는 몇 개인가요?"
+}
+
+
 struct InputView {
     
-    //메세지를 출력하는 함수
-    private func showMessage(getType: Message) {
-        switch getType {
-        case .peopleNum:
-            print(Message.peopleNum.inputMessage)
-        case .ladderNum:
-            print(Message.ladderNum.inputMessage)
+    private func userInput(question: Question) throws -> String {
+        print(question.rawValue)
+        guard let answer = readLine() else {
+            throw UserInputError.emptyValue
         }
+        return answer
     }
     
-    func inputUserPlayerName(messageType: Message) -> Array<String> {
-        showMessage(getType: messageType)
-        let inputName = LadderPlayer(name: readLine()!)
-        let names = inputName.name.components(separatedBy: ",")
+    
+    func playerName() throws -> Array<String> {
+        let answer = try userInput(question: Question.people)
+        let names = answer.components(separatedBy: ",")
+        
+        guard answer != "" else {
+            throw UserInputError.emptyValue
+        }
+        
+        guard names.count != 1 else {
+            throw UserInputError.incorrectPlayerData
+        }
+        
+        var largeNameCount = 0
+        for index in 0..<names.count {
+            if largeNameCount < names[index].count {
+                largeNameCount = names[index].count
+                guard largeNameCount < 6 else {
+                    throw UserInputError.overNameCount
+                }
+            }
+        }
         return names
     }
     
-    //사라리높이 입력받는 함수
-    func inputUserLadderCount(messageType: Message) -> Int {
-        showMessage(getType: messageType)
-        let inputCount = readLine()!
-        guard let ladderNum = Int(inputCount) else {
-            return 0
+    
+    func ladderHight() throws -> Int {
+        let answer = try userInput(question: Question.ladderHight)
+        
+        guard answer != "" else {
+            throw UserInputError.emptyValue
         }
-        return ladderNum
+        
+        guard let ladderHight = Int(answer) else {
+            throw UserInputError.incorrectLadderData
+        }
+        return ladderHight
+    }
+}
+
+
+enum UserInputError: Error {
+    case emptyValue
+    case overNameCount
+    case incorrectPlayerData
+    case incorrectLadderData
+    
+    var description: String {
+        switch self {
+        case .emptyValue: return "값이 없습니다"
+        case .overNameCount: return "플레이어이름은 5글자까지 가능합니다"
+        case .incorrectPlayerData: return "플레이어를 두사람이상 입력해주세요."
+        case .incorrectLadderData: return "사다리의 높이는 숫자로 입력해주세요"
+        }
     }
 }
--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,50 +8,36 @@
 
 import Foundation
 
-//게임 실행하는 함수
+
 func playGame() {
-    
     let inputView = InputView()
+    var names = [String]()
+    var ladderHigh: Int = 0
     var ladderGame = LadderGame()
     var resultView = ResultView()
     
     
-    var playerNames = Array<String>()
-    let checkPlayer = inputView.inputUserPlayerName(messageType: Message.peopleNum)
-        
-    do {
-        try showPlayerError(chNames: checkPlayer)
+    while true {
+        do {
+            names = try inputView.playerName()
+            ladderHigh = try inputView.ladderHight()
+            break
             
-    } catch UserInputError.overNameCount {
-        exit(0)
-    } catch UserInputError.incorrectPlayerData {
-        exit(0)
-    } catch {
-        print("모든상황에러")
+        } catch UserInputError.emptyValue{
+            print(UserInputError.emptyValue.description)
+        } catch UserInputError.overNameCount{
+            print(UserInputError.overNameCount.description)
+        } catch UserInputError.incorrectPlayerData{
+            print(UserInputError.incorrectPlayerData.description)
+        } catch UserInputError.incorrectLadderData{
+            print(UserInputError.incorrectLadderData.description)
+        } catch {
+            print("알 수 없는 에러")
+        }
     }
-    playerNames = checkPlayer
-   
-    //참여자 인원수
-    var playCount = Int()
     
-    //입력 예외처리
-    let checkLadder = inputView.inputUserLadderCount(messageType: Message.ladderNum)
-    do {
-        try showLabberError(chladderNum: checkLadder)
-            
-    } catch UserInputError.incorrectLadderData {
-        exit(0)
-    } catch {
-        print("모든상황에러")
-    }
-    playCount = checkLadder
+  
     
-    //사다리 만들기
-    let horizontal = ladderGame.processingGame(playerNames: playerNames, playCount: playCount)
-    //사다리 출력 (세로 사다리 "|" 붙여서)
-    resultView.showLadders(ladders: horizontal)
-    //참여자 이름 출력 (사다리 아래에 이름 붙이기)
-    resultView.showPlayerName(playerName: playerNames)
 }
 
 playGame()
