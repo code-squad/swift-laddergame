71de5900af3b9df7e2cdbe49d857a0e7462a0b05
박준희 <junibibi04@gmail.com>
2019-05-08 21:07:47 +0900

    LadderGame 메소드와 프로퍼티명 수정
    LadderGame의 메소드와 프로퍼티명 수정했습니다.

--- LadderGame/LadderGame/LadderGame.swift
+++ LadderGame/LadderGame/LadderGame.swift
@@ -8,19 +8,20 @@
 
 import Foundation
 
+
 struct LadderGame {
     
     var ladder = Ladder()
     var outputForm = OutputForm()
-
+    
     mutating func processingGame(playerNames: [String], playCount: Int) -> [[String]] {
-        //사다리 가로 Bool값 배열 만들기
-        ladder.makeLadderData(peopleNum: playerNames.count, ladderNum: playCount)
-        let data = ladder.horizontalData
         
-        //사다리 Bool -> String
-        outputForm.makeHorizontalLadders(makeData: data)
-        let horizontal = outputForm.horizontalLadders
-        return horizontal
+        ladder.makeLadderElements(playerCount: playerNames.count, ladderCount: playCount)
+        let elements = ladder.ladderElements
+        
+        outputForm.transFormLadders(transData: elements)
+        let ladders = outputForm.ladders
+        
+        return ladders
     }
 }
