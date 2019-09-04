6886143bed8577a622a8b6bbf500cd4dfcc02fe4
kyler.jeon <kyler.jeon@kinemaster.com>
2019-09-04 17:21:08 +0900

    사다리게임 개선하기 #1

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,4 +8,77 @@
 
 import Foundation
 
-print("hello world")
+struct Player {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct LadderGame {
+    var players: [Player]
+    var heightOfLadders: Int
+    
+    var ladders: Array<Array<Bool>> = []
+    
+    init(players: [Player]) {
+        self.players = players
+        self.heightOfLadders = players.count
+    }
+    
+    mutating func makeLadders() {
+        for _ in 1...heightOfLadders {
+            var line : Array<Bool> = []
+            for _ in players {
+                let random = Int.random(in: 0...1)
+                line.append(random == 1)
+            }
+            ladders.append(line)
+        }
+    }
+    
+    func printLadders() {
+        for line in ladders {
+            print("     |", terminator: "")
+            for step in line {
+                if step {
+                    print(" ----- ", terminator: "")
+                } else {
+                    print("       ", terminator: "")
+                }
+                print("|", terminator: "")
+            }
+            print("")
+        }
+        for player in players {
+            print(" \(player.name) ", terminator: "")
+        }
+        print("")
+    }
+}
+
+func seperatePlayerName(names: String?) -> [Player] {
+    let nameToSplit = (names!.count > 3) ? names! : "a,b,c,d,e"
+    let names = nameToSplit.split(separator: ",")
+    
+    var players: [Player] = []
+    for name in names {
+        players.append(Player(name:String(name)))
+    }
+    return players
+}
+    
+print("참여할 사람의 이름을 쉼표화 함께 입력해주세요")
+let inputNames = readLine()
+//let players = Int(inputNames ?? "a, b, c, d, e") ?? 0
+let players = seperatePlayerName(names: inputNames)
+
+
+var game = LadderGame(players: players)
+game.makeLadders()
+game.printLadders()
+
+
+
+
---
+++ LadderGame/LadderGameTest/LadderGameTest.swift
@@ -0,0 +1,44 @@
+//
+/******************************************************************************
+ * File Name        : LadderGameTest.swift
+ * Description      : LadderGame
+ *******************************************************************************
+ * Copyright (c) 2002-2019 KineMaster Corp. All rights reserved.
+ * https://www.kinemastercorp.com/
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ ******************************************************************************/
+
+import XCTest
+
+class LadderGameTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+        //given
+        //when
+        //then
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
