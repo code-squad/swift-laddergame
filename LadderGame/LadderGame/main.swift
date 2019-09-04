8238ed876348962ec930b4c49c7b00eae1e2ea4e
sean-jeong <sean.jeong@kinemaster.com>
2019-09-04 17:21:01 +0900

    step2

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,5 +8,69 @@
 
 import Foundation
 
-print("Hello World")
-print("try one more")
+struct LadderGame {
+    var numberOfPlayers : Int
+    var heightOfLadders : Int
+    var ladders : Array<Array<Bool>> = []
+    var playerNames : Array<Substring> = []
+    
+    init(names: String?, height: Int) {
+        
+        if names != nil {
+            playerNames = names!.split(separator: ",")
+            numberOfPlayers = playerNames.count
+        } else {
+            numberOfPlayers = 0
+        }
+        
+       heightOfLadders = height
+        
+    }
+    
+    mutating func makeLadders() {
+        if numberOfPlayers == 0 || heightOfLadders == 0 {
+            return
+        }
+        for _ in 1...heightOfLadders {
+            var line : Array<Bool> = []
+            for _ in 1..<numberOfPlayers {
+                let random = Int.random(in: 0...1)
+                line.append(random == 1)
+            }
+            ladders.append(line)
+        }
+    }
+    
+    func printLadders() {
+        
+        for line in ladders {
+            print("|", terminator: "")
+            for step in line {
+                if step {
+                    print("-----", terminator: "")
+                } else {
+                    print("     ", terminator: "")
+                }
+                print("|", terminator: "")
+            }
+            print("")
+        }
+        
+        for name in playerNames {
+            print("\(name)", terminator: "\t")
+        }
+        print("")
+
+    }
+}
+
+print("Input names of players?")
+let names = readLine()
+
+print("Input height?")
+let height = readLine()
+let heightOfLadders = Int(height ?? "0")
+
+var game = LadderGame(names: names, height: heightOfLadders ?? 0)
+game.makeLadders()
+game.printLadders()
