4f73e5e65b5113fcbdf550df3efda1a993bbc45e
ALEX <rnfkehdrlf@gmail.com>
2019-09-04 17:23:21 +0900

    LadderGame Step-2

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,4 +8,59 @@
 
 import Foundation
 
-print("Hello world")
+struct LadderGame {
+    var playernames : Array<Substring>
+    var heightOfLadders : Int
+    
+    var ladders : Array<Array<Bool>> = []
+    
+    init(name: Array<Substring>, height:Int) {
+        playernames = name
+        heightOfLadders = height
+    }
+    
+    mutating func makeLadders(){
+        for _ in 1...heightOfLadders {
+            var line : Array<Bool> = []
+            for _ in 1...playernames.count-1{
+                let random = Int.random(in: 0...1)
+                line.append(random == 1)
+            }
+            ladders.append(line)
+        }
+    }
+    
+    func printLadders(){
+        for line in ladders{
+            print("|", terminator: "")
+            for step in line {
+                if step {
+                    print("-----", terminator: "")
+                }else{
+                    print("     ", terminator: "")
+                }
+                print("|", terminator: "")
+            }
+            print("")
+        }
+        for name in playernames {
+//            if name.count > 5{
+//                let index = name.index(name.startIndex, offsetBy: 5)
+//                name.substring(to: index)
+//            }
+            print("\(name) ", terminator: "")
+        }
+        print("")
+    }
+}
+print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)")
+let players = readLine()
+let names = players?.split(separator: ",") ?? Array<Substring>()
+
+print("최대 사다리 높이는 몇 개인가요?")
+let heights = readLine()
+let heightOfLadders = Int(heights ?? "0") ?? 0
+
+var game = LadderGame(name: names, height: heightOfLadders)
+game.makeLadders()
+game.printLadders()
---
+++ LadderGame/LadderGameTest/LadderGameTest.swift
@@ -0,0 +1,37 @@
+//
+//  LadderGameTest.swift
+//  LadderGameTest
+//
+//  Created by ALEX2 on 04/09/2019.
+//  Copyright © 2019 Codesquad Inc. All rights reserved.
+//
+
+import XCTest
+
+class LadderGameTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //given
+        //when
+        //then
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
