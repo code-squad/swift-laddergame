0f756451e2971b3c5ca6c37706680f147d42028a
jypthemiracle <hophfg@yahoo.co.kr>
2019-10-20 15:42:39 +0900

    commit

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -1,10 +1,32 @@
-//
-//  main.swift
-//  LadderGame
-//
-//  Created by JK on 09/10/2017.
-//  Copyright © 2017 Codesquad Inc. All rights reserved.
-//
-
 import Foundation
 
+func baseData() -> (Int, Int) {
+    print("참여할 사람은 모두 몇 명인가")
+    let pplNum = Int(readLine() ?? "0")!
+
+    print("최대 사다리 높이는 모두 몇 개인가")
+    let ladderheight = Int(readLine() ?? "0")!
+    
+    return (pplNum, ladderheight)
+}
+
+func makeLadders(_ participants: Int, _ height: Int){
+    var ladder = Array<Array<Bool>>()
+    for _ in 0..<height {
+        var ladderRow = Array<Bool>()
+        for _ in 0..<participants {
+            ladderRow.append(Bool.random())
+        }
+        ladder.append(ladderRow)
+    }
+
+    for ladderRow in ladder {
+        for step in ladderRow {
+            print(step ? "-|" : " |", terminator: "")
+        }
+        print("")
+    }
+}
+
+let (pplNum, ladderheight) = baseData()
+makeLadders(pplNum, ladderheight)
