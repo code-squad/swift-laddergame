de938b693305f1cda9d94c77d00ba01204e3d76a
xowdev <xowdev@gmail.com>
2019-10-20 14:04:40 +0900

    laddergame mission

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,28 @@
 
 import Foundation
 
+
+print("참여할 사람은 몇 명 인가요?")
+let peopleNum = Int(readLine() ?? "") ?? 0
+
+print("최대 사다리 높이는 몇 개인가요?")
+let ladderNum = Int(readLine() ?? "") ?? 0
+
+var ladderAry = Array<Array<Bool>>()
+
+for _ in 1...ladderNum {
+    var ladderRowAry = Array<Bool>()
+    ladderRowAry.append(false)
+    for _ in 1..<peopleNum {
+        ladderRowAry.append(Bool.random())
+    }
+    ladderAry.append(ladderRowAry)
+}
+
+for ladderRowAry in ladderAry
+{
+    for step in ladderRowAry {
+        print(step ? "-|" : " |", terminator: "")
+    }
+    print("")
+}
