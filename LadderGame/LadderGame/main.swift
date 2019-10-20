98f41a194313ff02c9c50051dd92026891972baf
자올 Summer <jaowlahn@gmail.com>
2019-10-20 14:17:29 +0900

    Ladder game ver 1

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,48 @@
 
 import Foundation
 
+class LadderGames{
+    private var steps = 2
+    private var players = 2
+    private var ladders = Array<Array<Bool>>()
+    func start(){
+        print("사다리는 몇 층으로 할까요?")
+        steps = Int(readLine() ?? "") ?? 1
+        if(steps < 2){
+            print("Error : 사다리는 2층 이상이어야 합니다.")
+            steps = 2
+            start()
+            return
+        }
+        print("몇 명이 참여하나요?")
+        players = Int(readLine() ?? "") ?? 2
+        if(players < 2){
+            print("Error : 최소 2명 이상 참가해주셔야 합니다.")
+            players = 2
+            start()
+            return
+        }
+        make()
+    }
+    func make(){
+        ladders.removeAll()
+        for _ in 0 ..< steps {
+            var row = Array<Bool>()
+            for _ in 0 ..< players{
+                row.append(arc4random_uniform(200) % 2 == 0)
+            }
+            ladders.append(row)
+        }
+        render()
+    }
+    func render(){
+        ladders.forEach { row in
+            print("|", terminator:"")
+            row.forEach { (v) in
+                print( v ? "-|" : " |", terminator:"")
+            }
+            print("")
+        }
+    }
+}
+LadderGames().start()
