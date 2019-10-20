2fab884a63db1c9a1ded29b2a9bc8c84b120df97
YukJiSoo <wltn3231@naver.com>
2019-10-20 13:51:35 +0900

    Feat 사다리게임
    사다리 높이와 인원 수를 입력 받아 랜덤 사다리를 출력해주는 게임.

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,3 +8,35 @@
 
 import Foundation
 
+func writeData() -> (Int, Int) {
+    print("참여할 사람은 몇 명 인가요?")
+    let numberOfPeople = Int(readLine() ?? "0") ?? 0
+
+    print("최대 사다리 높이는 몇 개인가요?")
+    let numberOfLadders = Int(readLine() ?? "0") ?? 0
+
+    return (numberOfPeople, numberOfLadders)
+}
+
+func printLadders(_ people: Int, _ height: Int) {
+    var ladder = Array<Array<Bool>>()
+
+    for _ in 1..<height {
+        var ladderRow = Array<Bool>()
+        for _ in 1..<people {
+            ladderRow.append(Bool.random())
+        }
+        ladder.append(ladderRow)
+    }
+
+    for ladderRow in ladder {
+        print("|", terminator: "")
+        for step in ladderRow {
+            print(step ? "-|" : " |", terminator: "")
+        }
+        print("")
+    }
+}
+
+let (numberOfPeople, numberOfLadders) = writeData()
+printLadders(numberOfPeople, numberOfLadders)
